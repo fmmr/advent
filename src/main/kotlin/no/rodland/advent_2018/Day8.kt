eb921141344a1fc8ff635693d9aadf9dba29d69a
frerodla <fmr@finn.no>
2018-12-08 23:33:00 +0100

    day 8 part 2

--- src/main/kotlin/no/rodland/advent_2018/Day8.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day8.kt
@@ -5,15 +5,37 @@ object Day8 {
         return allNodes.map { it.metaSum }.sum()
     }
 
-    fun partTwo(coordinates: List<Pair<Int, Int>>, limit: Int): Int {
-        return 2
+    fun partTwo(coordinates: List<Int>): Int {
+        return createSubTreeTwo(coordinates.toMutableList()).metaSum
     }
 
-    fun flatMapNodes(nodes: List<Node>): List<Node> {
+    private fun createSubTreeTwo(list: MutableList<Int>): Node {
+        val numNodes = list.removeAt(0)
+        val numMeta = list.removeAt(0)
+        val subNodes = (1..numNodes).map { createSubTreeTwo(list) }
+        val meta = if (subNodes.isNotEmpty()) {
+            (1..numMeta)
+                    .map { list.removeAt(0) }
+                    .map {
+                        if ((it - 1) < subNodes.size) {
+                            subNodes[it - 1].metaSum
+                        } else {
+                            0
+                        }
+                    }
+                    .sum()
+        } else {
+            (1..numMeta).map { list.removeAt(0) }.sum()
+        }
+        return Node(meta, subNodes)
+    }
+
+
+    private fun flatMapNodes(nodes: List<Node>): List<Node> {
         return listOf(nodes, nodes.flatMap { flatMapNodes(it.subNodes) }).flatten()
     }
 
-    fun createSubTree(list: MutableList<Int>): Node {
+    private fun createSubTree(list: MutableList<Int>): Node {
         val numNodes = list.removeAt(0)
         val numMeta = list.removeAt(0)
         val subNodes = (1..numNodes).map { createSubTree(list) }
