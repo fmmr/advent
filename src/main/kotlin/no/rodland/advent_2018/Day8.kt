53ae356327b2ba0dcaa15d5fc8409137427e3490
Fredrik Rodland <git@rodland.no>
2018-12-08 23:57:10 +0100

    day 8 part 2 rewrote to use the same tree-builder in both parts.  metadata-mapper is now a param.

--- src/main/kotlin/no/rodland/advent_2018/Day8.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day8.kt
@@ -1,47 +1,39 @@
 object Day8 {
     fun partOne(coordinates: List<Int>): Int {
-        val node: Node = createSubTree(coordinates.toMutableList())
-        val allNodes = flatMapNodes(listOf(node))
-        return allNodes.map { it.metaSum }.sum()
+        return createSubTreeTwo(coordinates.toMutableList()) { _, i -> i }
+                .flatten()
+                .map { it.metaSum }
+                .sum()
     }
 
     fun partTwo(coordinates: List<Int>): Int {
-        return createSubTreeTwo(coordinates.toMutableList()).metaSum
+        return createSubTreeTwo(coordinates.toMutableList()) { s, i ->
+            when {
+                s.isEmpty() -> i
+                (i - 1) < s.size -> s[i - 1].metaSum
+                else -> 0
+            }
+        }.metaSum
     }
 
-    private fun createSubTreeTwo(list: MutableList<Int>): Node {
+    private fun createSubTreeTwo(list: MutableList<Int>, metaDataMapper: (List<Node>, Int) -> Int): Node {
         val numNodes = list.removeAt(0)
         val numMeta = list.removeAt(0)
-        val subNodes = (1..numNodes).map { createSubTreeTwo(list) }
-        val meta = if (subNodes.isNotEmpty()) {
-            (1..numMeta)
-                    .map { list.removeAt(0) }
-                    .map {
-                        if ((it - 1) < subNodes.size) {
-                            subNodes[it - 1].metaSum
-                        } else {
-                            0
-                        }
-                    }
-                    .sum()
-        } else {
-            (1..numMeta).map { list.removeAt(0) }.sum()
-        }
+        val subNodes = (1..numNodes).map { createSubTreeTwo(list, metaDataMapper) }
+        val meta = (1..numMeta)
+                .map { list.removeAt(0) }
+                .map { metaDataMapper(subNodes, it) }
+                .sum()
         return Node(meta, subNodes)
     }
-
 
     private fun flatMapNodes(nodes: List<Node>): List<Node> {
         return listOf(nodes, nodes.flatMap { flatMapNodes(it.subNodes) }).flatten()
     }
 
-    private fun createSubTree(list: MutableList<Int>): Node {
-        val numNodes = list.removeAt(0)
-        val numMeta = list.removeAt(0)
-        val subNodes = (1..numNodes).map { createSubTree(list) }
-        val meta = (1..numMeta).map { list.removeAt(0) }.sum()
-        return Node(meta, subNodes)
+    data class Node(val metaSum: Int, val subNodes: List<Node>) {
+        fun flatten(): List<Node> {
+            return flatMapNodes(listOf(this))
+        }
     }
-
-    data class Node(val metaSum: Int, val subNodes: List<Node>)
 }
