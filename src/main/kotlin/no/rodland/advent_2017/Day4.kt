639775b84de2ad96bf11e7ac382a5aa0416ec2ed
Fredrik Rodland <git@rodland.no>
2018-12-10 16:21:18 +0100

    2017 Say 4 (just added skel)

---
+++ src/main/kotlin/no/rodland/advent_2017/Day4.kt
@@ -0,0 +1,15 @@
+package no.rodland.advent_2017
+
+object Day4 {
+
+    fun partOne(num: Int): Int {
+        return 2
+    }
+
+    fun partTwo(limit: Int): Int {
+        // size 11 is enough to solve number in test
+        return 3
+    }
+
+}
+
---
+++ src/test/kotlin/no/rodland/advent_2017/Day4Test.kt
@@ -0,0 +1,23 @@
+package no.rodland.advent_2017
+
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class Day4Test {
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `4,1,test`() {
+            report {
+                Day4.partOne(1) to 2
+            }
+        }
+
+    }
+
+    @Nested
+    inner class `Part 2` {
+    }
+}
