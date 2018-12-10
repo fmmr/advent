18cd6abe0f9b10190979081f33cf4f550d67058a
Fredrik Rodland <git@rodland.no>
2018-12-10 16:57:55 +0100

    2017 Day 4

--- src/main/kotlin/no/rodland/advent_2017/Day4.kt
+++ src/main/kotlin/no/rodland/advent_2017/Day4.kt
@@ -2,14 +2,18 @@ package no.rodland.advent_2017
 
 object Day4 {
 
-    fun partOne(num: Int): Int {
-        return 2
+    fun partOne(list: List<String>): Int {
+        return list.count { isValidOne(it) { s1, s2 -> s1 == s2 } }
     }
 
-    fun partTwo(limit: Int): Int {
-        // size 11 is enough to solve number in test
-        return 3
+    fun partTwo(list: List<String>): Int {
+        return list.count { isValidOne(it) { s1, s2 -> s1.toList().sorted() == s2.toList().sorted() } }
     }
+
+    private fun isValidOne(str: String, predicate: (String, String) -> Boolean): Boolean {
+        val list1 = str.split(" ")
+        return list1.map { tmp -> list1.count { predicate.invoke(it, tmp) } > 1 }.none { it }
+    }
+
 
 }
-
--- src/test/kotlin/no/rodland/advent_2017/Day4Test.kt
+++ src/test/kotlin/no/rodland/advent_2017/Day4Test.kt
@@ -3,15 +3,39 @@ package no.rodland.advent_2017
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import readFile
 
 internal class Day4Test {
+    val data4 = "2017/input_4.txt".readFile()
+    val test4 = listOf(2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2)
 
     @Nested
     inner class `Part 1` {
         @Test
         fun `4,1,test`() {
             report {
-                Day4.partOne(1) to 2
+                Day4.partOne(listOf("aa bb cc dd ee")) to 1
+            }
+        }
+
+        @Test
+        fun `4,1,test,2`() {
+            report {
+                Day4.partOne(listOf("aa bb cc dd aa")) to 0
+            }
+        }
+
+        @Test
+        fun `4,1,test,3`() {
+            report {
+                Day4.partOne(listOf("aa bb cc dd aaa")) to 1
+            }
+        }
+
+        @Test
+        fun `4,1,live`() {
+            report {
+                Day4.partOne(data4) to 337
             }
         }
 
@@ -19,5 +43,53 @@ internal class Day4Test {
 
     @Nested
     inner class `Part 2` {
+        @Test
+        fun `4,2,test`() {
+            report {
+                Day4.partTwo(listOf("abcde fghij")) to 1
+            }
+        }
+
+        @Test
+        fun `4,2,test,2`() {
+            report {
+                Day4.partTwo(listOf("abcde xyz ecdab")) to 0
+            }
+        }
+
+        @Test
+        fun `4,2,test,3`() {
+            report {
+                Day4.partTwo(listOf("a ab abc abd abf abj")) to 1
+            }
+        }
+
+        @Test
+        fun `4,2,test,4`() {
+            report {
+                Day4.partTwo(listOf("iiii oiii ooii oooi oooo")) to 1
+            }
+        }
+
+        @Test
+        fun `4,2,test,5`() {
+            report {
+                Day4.partTwo(listOf("oiii ioii iioi iiio")) to 0
+            }
+        }
+
+        @Test
+        fun `4,2,test,6`() {
+            report {
+                Day4.partTwo(listOf("aa bb cc dd aa")) to 0
+            }
+        }
+
+        @Test
+        fun `4,2,live`() {
+            report {
+                Day4.partTwo(data4) to 231
+            }
+        }
     }
 }
