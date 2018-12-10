43c170c87bf4a6cc2b90803dc69dfed98ecb3db5
Fredrik Rodland <git@rodland.no>
2018-12-10 09:37:19 +0100

    Day10 added filer (no code yet)

---
+++ src/main/kotlin/no/rodland/advent_2018/Day10.kt
@@ -0,0 +1,11 @@
+object Day10 {
+    fun partOne(players: List<String>): Int {
+        return 2
+    }
+
+    fun partTwo(players: List<String>): Int {
+        // part 2 copied from: https://todd.ginsberg.com/post/advent-of-code/2018/day9/
+        return 2
+    }
+}
+
