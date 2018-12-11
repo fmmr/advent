b7af5f0f81d32bcf5c86cc045fea9733d63fbbb7
Fredrik Rodland <git@rodland.no>
2018-12-11 13:11:14 +0100

    2018 Day 11

--- src/main/kotlin/no/rodland/advent_2018/Day11.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day11.kt
@@ -1,8 +1,55 @@
 object Day11 {
-
-    fun partOne(input: Int): Pair<Int, Int> {
-        return 1 to 1
+    fun partOne(serial: Int): Pair<Int, Int> {
+        val grid = 3
+        val max = (1..(300 - grid)).flatMap { x ->
+            (1..(300 - grid)).map { y ->
+                getAggregatedValues(x, y, serial, grid)
+            }
+        }.maxBy { it.second }
+        println("call and value: $max")
+        return max!!.first
     }
 
-}
+    fun partTwo(serial: Int, minRange: Int, maxRange: Int): Pair<Pair<Int, Int>, Int>? {
+        var lastVal = 0
+        val max = (minRange..maxRange).map { grid ->
+            val maxBy = (1..(300 - grid)).flatMap { x ->
+                (1..(300 - grid)).map { y ->
+                    getAggregatedValues(x, y, serial, grid) to grid
+                }
+            }.maxBy { it.first.second }
+            println("grid: $grid: $maxBy")
+            maxBy
+        }.maxBy { it!!.first.second }
+        println("max $max")
+        return (max!!.first.first.first to max.first.first.second) to (max.second)
+    }
 
+    private fun getAggregatedValues(xX: Int, yY: Int, serial: Int, gridsize: Int): Pair<Pair<Int, Int>, Int> {
+        val pair = (xX to yY) to (0..(gridsize - 1)).flatMap { x ->
+            (0..(gridsize - 1)).map { y ->
+                val x1 = x + xX
+                getHundred(((x1 + 10) * (y + yY) + serial) * (x1 + 10)) - 5
+            }
+        }.sum()
+        return pair
+    }
+
+    private fun getValues(xX: Int, yY: Int, serial: Int): Pair<Pair<Int, Int>, Int> {
+        val pair = (xX to yY) to (0..2).flatMap { x ->
+            (0..2).map { y ->
+                val x1 = x + xX
+                getHundred(((x1 + 10) * (y + yY) + serial) * (x1 + 10)) - 5
+            }
+        }.sum()
+        return pair
+    }
+
+    fun getHundred(i: Int): Int {
+        if (i < 100) {
+            return 0
+        }
+        val toString = (i / 100).toString()
+        return toString.get(toString.length - 1).toString().toInt()
+    }
+}
--- src/test/kotlin/no/rodland/advent_2018/Day11Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day11Test.kt
@@ -1,7 +1,9 @@
 package no.rodland.advent_2018
 
 import Day11
+import Day11.getHundred
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -14,15 +16,123 @@ internal class Day11Test {
         @Test
         fun `11,1,test`() {
             report {
-                Day11.partOne(42) to (1 to 1)
+                Day11.partOne(42) to (21 to 61)
             }
         }
 
+        @Test
+        fun `11,1,test,2`() {
+            report {
+                Day11.partOne(18) to (33 to 45)
+            }
+        }
+
+        @Test
+        fun `11,1,live`() {
+            report {
+                Day11.partOne(9798) to (44 to 37)
+            }
+        }
+
+        @Test
+        fun `11,1,getHundred`() {
+            report {
+                Day11.getHundred(42) to 0
+            }
+        }
+
+        @Test
+        fun `11,1,getHundred,2`() {
+            report {
+                Day11.getHundred(12342) to 3
+            }
+        }
+
+        @Test
+        fun `11,1,getHundred,3`() {
+            report {
+                Day11.getHundred(100) to 1
+            }
+        }
+
+        @Test
+        fun `11,1,getHundred,4`() {
+            report {
+                Day11.getHundred(100000) to 0
+            }
+        }
+
+        @Test
+        fun `11,1,getLevel`() {
+            report {
+                getHundred(((3 + 10) * 5 + 8) * (3 + 10)) - 5 to 4
+            }
+        }
+
+        @Test
+        fun `11,1,getLevel,2`() {
+            report {
+                getHundred(((217 + 10) * 196 + 39) * (217 + 10)) - 5 to 0
+            }
+        }
+
+        @Test
+        fun `11,1,getLevel,3`() {
+            report {
+                getHundred(((101 + 10) * 153 + 71) * (101 + 10)) - 5 to 4
+            }
+        }
+
+        @Test
+        fun `11,1,getLevel,4`() {
+            report {
+                getHundred(((122 + 10) * 79 + 57) * (122 + 10)) - 5 to -5
+            }
+        }
 
     }
 
     @Nested
     inner class `Part 2` {
+        @Test
+        @Slow(3100)
+        fun `11,2,test`() {
+            report {
+                Day11.partTwo(18, 15, 17) to ((90 to 269) to 16)
+            }
+        }
+
+        @Test
+        @Slow(10000)
+        fun `11,2,test,2`() {
+            report {
+                Day11.partTwo(18, 1, 20) to ((90 to 269) to 16)
+            }
+        }
+
+        @Test
+        @Slow(999999)
+        fun `11,2,test,3`() {
+            report {
+                Day11.partTwo(9798, 1, 300) to ((90 to 269) to 16)
+            }
+        }
+
+        @Test
+        @Slow(2400)
+        fun `11,2,live`() {
+            report {
+                Day11.partTwo(9798, 12, 14) to ((235 to 87) to 13)
+            }
+        }
+
+        @Test
+        @Slow(27800)
+        fun `11,2,live,2`() {
+            report {
+                Day11.partTwo(9798, 1, 30) to ((235 to 87) to 13)
+            }
+        }
     }
 }
 
