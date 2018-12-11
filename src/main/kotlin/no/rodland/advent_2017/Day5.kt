4c26c8c0df31bb7929fece9ec01712f90ba4a9bf
Fredrik Rodland <git@rodland.no>
2018-12-11 16:40:10 +0100

    2017 Day 5

--- src/main/kotlin/no/rodland/advent_2017/Day5.kt
+++ src/main/kotlin/no/rodland/advent_2017/Day5.kt
@@ -2,9 +2,37 @@ package no.rodland.advent_2017
 
 object Day5 {
 
-    fun partOne(list: List<Int>): Int {
-        return 2
+    fun partOne(list: MutableList<Int>): Int {
+        return walkThroughIt(list, increase(list))
     }
 
+    fun partTwo(list: MutableList<Int>): Int {
+        return walkThroughIt(list, decrease(list))
+    }
+
+    private fun increase(list: MutableList<Int>): (Int) -> Int {
+        return { i: Int -> list[i]++ }
+    }
+
+    private fun decrease(list: MutableList<Int>): (Int) -> Int {
+        return { i: Int ->
+            if (list[i] >= 3) {
+                list[i]--
+            } else {
+                list[i]++
+            }
+        }
+    }
+
+    private fun walkThroughIt(list: MutableList<Int>, func: (Int) -> Int): Int {
+        var i = 0
+        var numSteps = 0
+        while (!(i >= list.size || i < 0)) {
+            val stepsToMove = func(i)
+            i += stepsToMove
+            numSteps++
+        }
+        return numSteps
+    }
 
 }
--- src/test/kotlin/no/rodland/advent_2017/Day5Test.kt
+++ src/test/kotlin/no/rodland/advent_2017/Day5Test.kt
@@ -3,10 +3,10 @@ package no.rodland.advent_2017
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import readFile
+import readFileAsInt
 
 internal class Day5Test {
-    val data5 = "2017/input_5.txt".readFile()
+    val data5 = "2017/input_5.txt".readFileAsInt()
     val test5 = listOf(0, 3, 0, 1, -3)
 
     @Nested
@@ -14,12 +14,32 @@ internal class Day5Test {
         @Test
         fun `5,1,test`() {
             report {
-                Day5.partOne(test5) to 2
+                Day5.partOne(test5.toMutableList()) to 5
+            }
+        }
+
+        @Test
+        fun `5,1,live`() {
+            report {
+                Day5.partOne(data5.toMutableList()) to 318883
             }
         }
     }
 
     @Nested
     inner class `Part 2` {
+        @Test
+        fun `5,2,test`() {
+            report {
+                Day5.partTwo(test5.toMutableList()) to 10
+            }
+        }
+
+        @Test
+        fun `5,2,live`() {
+            report {
+                Day5.partTwo(data5.toMutableList()) to 23948711
+            }
+        }
     }
 }
