0d4d428ac4f8b3894bc8dbdfc6b8c14a4d377bea
Fredrik Rodland <git@rodland.no>
2018-12-15 13:55:35 +0100

    2018 day 15 started

--- src/main/kotlin/no/rodland/advent_2018/Day15.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day15.kt
@@ -1,11 +1,74 @@
 package no.rodland.advent_2018
 
+import java.util.*
+
 object Day15 {
+    const val WALL: Char = '#'
+    const val OPEN: Char = '.'
+
+
     fun partOne(list: List<String>): Int {
+        val (creatures, map) = init(list)
         return 2
+    }
+
+    fun init(list: List<String>): Pair<TreeSet<Creature>, List<MutableList<Char>>> {
+        val creatures = sortedSetOf<Creature>()
+        val map: List<MutableList<Char>> = list.mapIndexed { y, line ->
+            creatures.addAll(line
+                    .mapIndexed { x, c -> c to (x to y) }
+                    .filter { it.first.isCreature() }
+                    .map {
+                        Creature(
+                                it.first.toType(),
+                                "${it.first}_${it.second.first}X${it.second.second}",
+                                it.second.first,
+                                it.second.second
+                        )
+                    })
+            line.map { if (it.isCreature()) '.' else it }.toMutableList()
+        }
+        return Pair(creatures, map)
+    }
+
+    data class Creature(val type: Type, val name: String, var x: Int, var y: Int, val power: Int = 3, var hitPoints: Int = 200) : Comparable<Creature> {
+        override fun compareTo(other: Creature): Int {
+            val yComp = y.compareTo(other.y)
+            return if (yComp == 0) {
+                x.compareTo(other.x)
+            } else yComp
+        }
+
+        fun dead() = hitPoints <= 0
+
+        fun hit(other: Creature) {
+            other.hitPoints -= power
+        }
+    }
+
+    enum class Type {
+        ELF, GOBLIN;
+
+        fun other(): Type {
+            return when (this) {
+                ELF -> GOBLIN
+                GOBLIN -> ELF
+            }
+        }
     }
 
     fun partTwo(list: List<String>): Int {
         return 2
     }
 }
+
+fun Char.isCreature(): Boolean = this in "GE"
+
+fun Char.toType(): Day15.Type {
+    return when (this) {
+        'G' -> Day15.Type.GOBLIN
+        'E' -> Day15.Type.ELF
+        else -> error("not a vald creature type $this")
+    }
+}
+
--- src/test/kotlin/no/rodland/advent_2018/Day15Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day15Test.kt
@@ -2,23 +2,134 @@ package no.rodland.advent_2018
 
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
+import no.rodland.advent_2018.Day15.Creature
+import no.rodland.advent_2018.Day15.Type.ELF
+import no.rodland.advent_2018.Day15.Type.GOBLIN
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import readFile
+import kotlin.test.assertEquals
 
 @DisableSlow
 internal class Day15Test {
     val data15 = "2018/input_15.txt".readFile()
-    val test15 = listOf("1", "2")
+    val test15_mini = listOf(
+            "#######",
+            "#.E...#",
+            "#.....#",
+            "#...G.#",
+            "#######"
+    )
+    val test15_small = listOf(
+            "#########",
+            "#G..G..G#",
+            "#.......#",
+            "#.......#",
+            "#G..E..G#",
+            "#.......#",
+            "#.......#",
+            "#G..G..G#",
+            "#########"
+    )
+
+    @Nested
+    inner class Creatures {
+        @Test
+        fun `15,1,creatures,hit`() {
+            report {
+                val elf = Creature(ELF, "test", 0, 0)
+                val goblin = Creature(ELF, "test", 0, 0)
+                goblin.hit(elf)
+                repeat(10) { elf.hit(goblin) }
+                assertEquals(goblin.dead(), false)
+                goblin.hitPoints to (200 - 30)
+            }
+        }
+
+        @Test
+        fun `15,1,creatures,hit_until_dead`() {
+            report {
+                val elf = Creature(ELF, "test", 0, 0)
+                val goblin = Creature(ELF, "test", 0, 0)
+                goblin.hit(elf)
+                repeat(200 / 3) { elf.hit(goblin) }
+                assertEquals(goblin.dead(), false)
+                elf.hit(goblin)
+                assertEquals(goblin.dead(), true)
+                goblin.hitPoints to -1
+            }
+        }
+
+        @Test
+        fun `15,1,creatures,elf_other`() {
+            report {
+                ELF.other() to GOBLIN
+            }
+        }
+
+        @Test
+        fun `15,1,creatures,goblin_other`() {
+            report {
+                GOBLIN.other() to ELF
+            }
+        }
+
+        @Test
+        fun `15,1,creatures,g`() {
+            report {
+                'G'.toType() to GOBLIN
+            }
+        }
+
+        @Test
+        fun `15,1,creatures,e`() {
+            report {
+                'E'.toType() to ELF
+            }
+        }
+
+        @Test
+        fun `15,1,creatures,1`() {
+            report {
+                assertThrows<IllegalStateException> { '.'.toType() }
+                "exeption ok" to "exeption ok"
+            }
+        }
+    }
+
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `15,1,init,small`() {
+            report {
+                val (creatures, map) = Day15.init(test15_small)
+                (2..7).forEach { assertEquals(map[1], map[it]) }
+                assertEquals(map[0], map[8]) // left and right walls
+                assertEquals(map[0].size, 9) // left and right walls
+                assertEquals(creatures.count { it.type == GOBLIN }, 8)
+                assertEquals(creatures.count { it.type == ELF }, 1)
+                assertEquals(creatures.map { it.type.other() }.count { it == ELF }, 8)
+                creatures.size to 9
+            }
+        }
+
+        @Test
+        fun `15,1,init,mini`() {
+            report {
+                val (creatures, map) = Day15.init(test15_mini)
+                assertEquals(map[1].size, 7)
+                assertEquals(map[1], map[2])
+                assertEquals(map[1], map[3])
+                assertEquals(map[0], map[4]) // left and right walls
+                creatures.size to 2
+            }
+        }
+    }
 
     @Nested
     inner class `Part 1` {
-        @Test
-        fun `15,1,test`() {
-            report {
-                Day15.partOne(test15) to 2
-            }
-        }
 
         @Test
         fun `15,1,live`() {
@@ -33,7 +144,7 @@ internal class Day15Test {
         @Test
         fun `15,2,test`() {
             report {
-                Day15.partTwo(test15) to 2
+                Day15.partTwo(test15_mini) to 2
             }
         }
 
