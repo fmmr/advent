c2333865143d04c7fb3bf8cb55b3b59fcda74580
Fredrik Rodland <git@rodland.no>
2018-12-17 09:47:50 +0100

    2018 day 17 parse and minmax y

--- src/main/kotlin/no/rodland/advent_2018/Day17.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day17.kt
@@ -2,7 +2,28 @@ package no.rodland.advent_2018
 
 object Day17 {
     fun partOne(list: List<String>): Int {
+        val coordinates: List<Pair<IntRange, IntRange>> = parse(list)
+        getYMinMax(coordinates)
         return 2
+    }
+
+    fun getYMinMax(coordinates: List<Pair<IntRange, IntRange>>): Pair<Int, Int> {
+        val ymin = coordinates.map { it.second }.map { it.first }.min()!!
+        val ymax = coordinates.map { it.second }.map { it.last }.max()!!
+        return ymin to ymax
+    }
+
+    fun parse(list: List<String>): List<Pair<IntRange, IntRange>> {
+        return list.map { str ->
+            val part = (0..1).map { idx ->
+                val (c, range) = str.split(", ")[idx].split("=")
+                val rSplit = range.split("..")
+                val r1 = rSplit[0].toInt()
+                val r2 = if (rSplit.size == 2) rSplit[1].toInt() else r1
+                c to r1..r2
+            }.toMap()
+            part["x"]!! to part["y"]!!
+        }
     }
 
     fun partTwo(list: List<String>): Int {
--- src/test/kotlin/no/rodland/advent_2018/Day17Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day17Test.kt
@@ -9,7 +9,32 @@ import readFile
 @DisableSlow
 internal class Day17Test {
     val data17 = "2018/input_17.txt".readFile()
-    val test17 = listOf("1", "2")
+    val test17 = listOf("x=495, y=2..7", "y=7, x=495..501", "x=501, y=3..7", "x=498, y=2..4", "x=506, y=1..2", "x=498, y=10..13", "x=504, y=10..13", "y=13, x=498..504")
+
+    @Nested
+    inner class Parse {
+        @Test
+        fun `17,1,parse`() {
+            report {
+                Day17.parse(test17) to listOf(Pair(495..495, 2..7),
+                        Pair(495..501, 7..7),
+                        Pair(501..501, 3..7),
+                        Pair(498..498, 2..4),
+                        Pair(506..506, 1..2),
+                        Pair(498..498, 10..13),
+                        Pair(504..504, 10..13),
+                        Pair(498..504, 13..13))
+            }
+        }
+
+        @Test
+        fun `17,1,minmax`() {
+            report {
+                val parse = Day17.parse(test17)
+                Day17.getYMinMax(parse) to (1 to 13)
+            }
+        }
+    }
 
     @Nested
     inner class `Part 1` {
