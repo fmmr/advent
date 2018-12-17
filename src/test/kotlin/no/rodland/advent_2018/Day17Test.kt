99f5137b8613f43f3eed1b7406a44c9338b71178
Fredrik Rodland <git@rodland.no>
2018-12-17 09:09:03 +0100

    2018 day 17 skel

---
+++ src/test/kotlin/no/rodland/advent_2018/Day17Test.kt
@@ -0,0 +1,49 @@
+package no.rodland.advent_2018
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import readFile
+
+@DisableSlow
+internal class Day17Test {
+    val data17 = "2018/input_17.txt".readFile()
+    val test17 = listOf("1", "2")
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `17,1,test`() {
+            report {
+                Day17.partOne(test17) to 2
+            }
+        }
+
+        @Test
+        fun `17,1,live`() {
+            report {
+                Day17.partOne(data17) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `17,2,test`() {
+            report {
+                Day17.partTwo(test17) to 2
+            }
+        }
+
+        @Test
+        fun `17,2,live`() {
+            report {
+                Day17.partTwo(data17) to 2
+            }
+        }
+    }
+}
+
+
