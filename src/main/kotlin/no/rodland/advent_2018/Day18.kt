127ce94240dc9236cc9d0465c0ae4288dff44b85
Fredrik Rodland <git@rodland.no>
2018-12-18 19:43:09 +0100

    2018 day 18 part 2 (after talking to chriswk)

--- src/main/kotlin/no/rodland/advent_2018/Day18.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day18.kt
@@ -23,7 +23,60 @@ object Day18 {
         return num[TREE]!! * num[LUMBERYARD]!!
     }
 
+
+    // thanks to chriswk
     fun partTwo(list: List<String>): Int {
+        var cave: Caves = init(list)
+        var i = 0
+        val map = mutableMapOf<String, Int>()
+        map[cave.getSignature()] = 0
+        while (true) {
+            i++
+            cave = iteration(cave)
+            val sig = cave.getSignature()
+            if (map.containsKey(sig)) {
+                val previous = map[sig]!!
+                val period = i - previous
+                val rem = 1_000_000_000 - i
+                val target = rem % period + previous
+                val fasit = map.filterValues { v -> v == target }.keys.first()
+                val num = fasit.groupingBy { it }.eachCount()
+                println("found sig at round $previous with period $period $num")
+
+                return (num[TREE] ?: 0) * (num[LUMBERYARD] ?: 0)
+            } else {
+                map[sig] = i
+            }
+        }
+    }
+
+    private fun iteration(cave: Caves): Caves {
+        return cave.mapIndexed { y, row ->
+            row.mapIndexed { x, c -> getNewValue(c, Pos(x, y), cave) }.toCharArray()
+        }.toTypedArray()
+    }
+
+    private fun getNewValue(c: Char, pos: Pos, cave: Caves): Char {
+        val num = pos.neighboorCellsAllEight().filter { it in cave }.map { cave[it] }.groupingBy { it }.eachCount()
+        val trees = num[TREE] ?: 0
+        val lumberyard = num[LUMBERYARD] ?: 0
+        return with(c) {
+            if (isOpen()) {
+                if (trees >= 3) TREE else this
+            } else if (isTree()) {
+                if (lumberyard >= 3) LUMBERYARD else this
+            } else {
+                if (lumberyard >= 1 && trees >= 1) LUMBERYARD else OPEN
+            }
+        }
+    }
+
+    fun init(list: List<String>): Caves = list.map { it.toCharArray() }.toTypedArray()
+
+    private fun Char.isOpen(): Boolean = this == OPEN
+    private fun Char.isTree(): Boolean = this == TREE
+
+    fun partTwoTrialAndError(list: List<String>): Int {
         var cave: Caves = init(list)
         // 508 - 536
         val recurringList = (1..536).map {
@@ -38,53 +91,23 @@ object Day18 {
 
         val seq = sequence {
             var i = 0
-            var started = false
             while (true) {
-                if (started || i >= 536) {
-                    if (i >= recurringList.size) {
-                        i = 0
-                    }
-                    yield(recurringList[i])
-                    started = true
-                } else {
-                    yield(0)
+                if (i >= recurringList.size) {
+                    i = 0
                 }
+                yield(recurringList[i])
                 i++
             }
         }
-        val message = seq.drop(1000000000 - 1).take(1).first()
+
+        val offset = 4
+        val message = seq.drop(1000000000 - 1 - offset).take(1).first()
 
         return message
     }
 
-
-    private fun iteration(cave: Caves): Caves {
-        return cave.mapIndexed { y, row ->
-            row.mapIndexed { x, c -> getNewValue(c, Pos(x, y), cave) }.toCharArray()
-        }.toTypedArray()
-    }
-
-    private fun getNewValue(c: Char, pos: Pos, cave: Caves): Char {
-        val num = pos.neighboorCellsAllEight().filter { it in cave }.map { cave[it] }.groupingBy { it }.eachCount()
-        val open = num[OPEN] ?: 0
-        val trees = num[TREE] ?: 0
-        val lumberyard = num[LUMBERYARD] ?: 0
-        val me = cave[pos]
-        return if (me.isOpen()) {
-            if (trees >= 3) TREE else me
-        } else if (me.isTree()) {
-            if (lumberyard >= 3) LUMBERYARD else me
-        } else {
-            if (lumberyard >= 1 && trees >= 1) LUMBERYARD else OPEN
-        }
-    }
-
-
-    fun init(list: List<String>): Caves = list.map { it.toCharArray() }.toTypedArray()
-
-    private fun Char.isOpen(): Boolean = this == OPEN
-    private fun Char.isTree(): Boolean = this == TREE
-    private fun Char.isLumberyard(): Boolean = this == LUMBERYARD
 }
+
+private fun Array<CharArray>.getSignature(): String = map { row -> row.contentToString() }.joinToString("")
 
 
--- src/test/kotlin/no/rodland/advent_2018/Day18Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day18Test.kt
@@ -1,7 +1,6 @@
 package no.rodland.advent_2018
 
 import no.rodland.advent.DisableSlow
-import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -43,7 +42,13 @@ internal class Day18Test {
     inner class `Part 2` {
 
         @Test
-        @Slow(10000)
+        fun `18,2,test`() {
+            report {
+                Day18.partTwo(test18) to 0
+            }
+        }
+
+        @Test
         fun `18,2,live`() {
             report {
                 Day18.partTwo(data18) to 197276
