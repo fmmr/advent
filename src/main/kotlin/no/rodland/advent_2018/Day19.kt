54ea0ec1a242b0482c72dbaf6402047e27138489
Fredrik Rodland <git@rodland.no>
2018-12-20 19:28:15 +0100

    2018 Day 19

--- src/main/kotlin/no/rodland/advent_2018/Day19.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day19.kt
@@ -1,11 +1,45 @@
 package no.rodland.advent_2018
 
+
 object Day19 {
-    fun partOne(list: List<String>): Int {
-        return 2
+    fun partOne(list: List<String>, initialReg0: Int = 0): Int {
+        val instructionPointer = list[initialReg0].split(" ")[1].toInt()
+        val program: Array<Instruction> = parse(list.subList(1, list.size))
+        val register = execute(instructionPointer, program, Register(initialReg0, 0, 0, 0, 0, 0))
+        println("Ran ${program.size} line program with end result: $register")
+        return register[initialReg0]
     }
 
-    fun partTwo(list: List<String>): Int {
-        return 2
+    private fun execute(pointer: Int, program: Array<Instruction>, r: Register): Register {
+        var register = r
+        var ip = 0
+        while (ip >= 0 && ip < program.size) {
+            register[pointer] = ip
+
+
+            val instruction = program[ip]
+            val opCode = OpCode[instruction.i]
+
+//            print("ip=$ip $register $opCode ${instruction.a} ${instruction.b} ${instruction.c} ")
+            register = opCode.run(instruction, register)
+//            println(" $register ")
+
+            ip = register[pointer] + 1
+        }
+        println("program exited, ip: $ip, register: $register")
+        return register
+    }
+
+    private fun parse(subList: List<String>): Array<Instruction> {
+        return subList.map { it.split(" ") }
+                .map { Instruction(OpCode.valueOf(it[0]).id, it[1].toInt(), it[2].toInt(), it[3].toInt()) }
+                .toTypedArray()
+    }
+
+    fun partTwo(): Int {
+        // https://github.com/chriswk/aoc/blob/master/src/main/kotlin/com/chriswk/aoc2018/Day19.kt#L13
+        return 10551267.fatcorize().sum()
     }
 }
+
+private fun Int.fatcorize(): List<Int> = (1..(this / 2)).filter { this % it == 0 } + this
--- src/test/kotlin/no/rodland/advent_2018/Day19Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day19Test.kt
@@ -1,6 +1,7 @@
 package no.rodland.advent_2018
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -9,21 +10,30 @@ import readFile
 @DisableSlow
 internal class Day19Test {
     val data19 = "2018/input_19.txt".readFile()
-    val test19 = listOf("1", "2")
+    val test19 = listOf(
+            "#ip 0",
+            "seti 5 0 1",
+            "seti 6 0 2",
+            "addi 0 1 0",
+            "addr 1 2 3",
+            "setr 1 0 0",
+            "seti 8 0 4",
+            "seti 9 0 5"
+    )
 
     @Nested
     inner class `Part 1` {
         @Test
         fun `19,1,test`() {
             report {
-                Day19.partOne(test19) to 2
+                Day19.partOne(test19, 0) to 6
             }
         }
 
         @Test
         fun `19,1,live`() {
             report {
-                Day19.partOne(data19) to 2
+                Day19.partOne(data19, 0) to 1228
             }
         }
     }
@@ -31,16 +41,17 @@ internal class Day19Test {
     @Nested
     inner class `Part 2` {
         @Test
-        fun `19,2,test`() {
+        fun `19,2,live`() {
             report {
-                Day19.partTwo(test19) to 2
+                Day19.partTwo() to 15285504
             }
         }
 
         @Test
-        fun `19,2,live`() {
+        @Slow(2000000000)
+        fun `19,2,run`() {
             report {
-                Day19.partTwo(data19) to 2
+                Day19.partOne(data19, 1) to 15285504
             }
         }
     }
