4407c61561b8154529c407753fceaa95bc685041
Fredrik Rodland <git@rodland.no>
2018-12-21 21:16:17 +0100

    2017 Day 10 part 1 and 2

--- src/main/kotlin/no/rodland/advent_2017/Day10.kt
+++ src/main/kotlin/no/rodland/advent_2017/Day10.kt
@@ -2,10 +2,60 @@ package no.rodland.advent_2017
 
 object Day10 {
     fun partOne(input: List<Int>, range: IntRange = 0..255): Int {
-        return 2
+        val ar = IntArray(range.last + 1) { it }
+        println("starting: ${ar.contentToString()}")
+        println("input   : $input")
+        fix(ar, input)
+        println("finished: ${ar.contentToString()}")
+        return ar[0] * ar[1]
     }
 
-    fun partTwo(input: List<Int>): Int {
-        return 2
+    fun partTwo(input: String, range: IntRange = 0..255): String {
+        val inputs = input.map { it.toInt() }.toMutableList()
+        val ar = IntArray(range.last + 1) { it }
+
+        inputs.addAll(listOf(17, 31, 73, 47, 23))
+        var pointer = 0
+        var skipSize = 0
+
+        repeat(64) {
+            val p = fix(ar, inputs, pointer, skipSize)
+            pointer = p.first
+            skipSize = p.second
+        }
+        return ar
+                .toList()
+                .chunked(16)
+                .map { it.fold(0) { agg, n -> agg xor n } }
+                .map { if (it < 16) "0${it.toString(16)}" else it.toString(16) }
+                .joinToString("")
     }
+
+    fun fix(ar: IntArray, input: List<Int>, pointer: Int = 0, skipSize: Int = 0): Pair<Int, Int> {
+        var p = pointer
+        var s = skipSize
+        input.forEach { length ->
+            getSeq(ar)
+                    .drop(p)
+                    .take(length)
+                    .toList()
+                    .reversed()
+                    .forEachIndexed { idx, newNum ->
+                        ar[(p + idx) % ar.size] = newNum
+                    }
+            p += (s + length) % ar.size
+            s++
+        }
+        return p to s
+    }
+
+    private fun getSeq(ar: IntArray): Sequence<Int> {
+        return sequence {
+            while (true) {
+                yieldAll(ar.toList())
+            }
+        }
+    }
+
+
 }
--- src/test/kotlin/no/rodland/advent_2017/Day10Test.kt
+++ src/test/kotlin/no/rodland/advent_2017/Day10Test.kt
@@ -1,6 +1,7 @@
 package no.rodland.advent_2017
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -16,14 +17,14 @@ internal class Day10Test {
         @Test
         fun `10,1,test`() {
             report {
-                Day10.partOne(listOf(3, 4, 1, 5), 0..4) to 2
+                Day10.partOne(listOf(3, 4, 1, 5), 0..4) to 12
             }
         }
 
         @Test
         fun `10,1,live`() {
             report {
-                Day10.partOne(data10) to 2
+                Day10.partOne(data10) to 826
             }
         }
     }
@@ -31,16 +32,41 @@ internal class Day10Test {
     @Nested
     inner class `Part 2` {
         @Test
-        fun `10,2,test`() {
+        @Slow(380)
+        fun `10,2,test,1`() {
             report {
-                Day10.partTwo(listOf(3, 4, 1, 5)) to 2
+                Day10.partTwo("1,2,3") to "3efbe78a8d82f29979031a4aa0b16a9d"
             }
         }
 
         @Test
+        @Slow(560)
+        fun `10,2,test,2`() {
+            report {
+                Day10.partTwo("AoC 2017") to "33efeb34ea91902bb2f59c9920caa6cd"
+            }
+        }
+
+        @Test
+        fun `10,2,test,3`() {
+            report {
+                Day10.partTwo("") to "a2582a3a0e66e6e86e3812dcb672a272"
+            }
+        }
+
+        @Test
+        @Slow(323)
+        fun `10,2,test,4`() {
+            report {
+                Day10.partTwo("1,2,4") to "63960835bcdc130f0b66d7ff4f6a5a8e"
+            }
+        }
+
+        @Test
+        @Slow(9400)
         fun `10,2,live`() {
             report {
-                Day10.partTwo(data10) to 2
+                Day10.partTwo("2017/input_10.txt".readFile()[0]) to "d067d3f14d07e09c2e7308c3926605c4"
             }
         }
     }
