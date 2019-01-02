0e51da26a88ec76b9d2ee439e9083fc98c72c9aa
Fredrik Rodland <git@rodland.no>
2019-01-02 18:31:41 +0100

    2018 Day 23 naive part 2 just to get started

--- src/main/kotlin/no/rodland/advent_2018/Day23.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day23.kt
@@ -10,12 +10,34 @@ object Day23 {
     }
 
     fun partTwo(list: List<String>): Int {
+        val bots = list.map { NanoBot(it) }
         return 2
+    }
+
+
+    fun naivePartTwo(list: List<String>): NanoBot {
+        val bots = list.map { NanoBot(it) }
+        val maxX = bots.maxBy { it.x }!!.x
+        val minX = bots.minBy { it.x }!!.x
+        val maxY = bots.maxBy { it.y }!!.y
+        val minY = bots.minBy { it.y }!!.y
+        val maxZ = bots.maxBy { it.z }!!.z
+        val minZ = bots.minBy { it.z }!!.z
+        val points = (minX..maxX).flatMap { x ->
+            (minY..maxY).flatMap { y ->
+                (minZ..maxZ).map { z ->
+                    val point = NanoBot(x, y, z, 0)
+                    val num = bots.count { it.inRange(point) }
+                    point to num
+                }
+            }
+        }.maxBy { it.second }
+        return points!!.first
     }
 
     val re = """pos=<([-\d]+),([-\d]+),([-\d]+)>, r=([-\d]+)""".toRegex()
 
-    class NanoBot(val x: Int, val y: Int, val z: Int, val sigalRadius: Int) {
+    data class NanoBot(val x: Int, val y: Int, val z: Int, val sigalRadius: Int) {
         constructor(str: String) : this(re.get(str), re.get(str, 2), re.get(str, 3), re.get(str, 4))
 
         fun inRange(otherBot: NanoBot): Boolean {
@@ -25,8 +47,5 @@ object Day23 {
         fun distanceTo(other: NanoBot): Int {
             return Math.abs(other.x - x) + Math.abs(other.y - y) + Math.abs(other.z - z)
         }
-
     }
-
-
 }
