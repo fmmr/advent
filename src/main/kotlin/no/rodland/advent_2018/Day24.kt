180ea79247d9f6ac1424fda30989747a5ea18b77
Fredrik Rodland <git@rodland.no>
2019-01-03 13:58:29 +0100

    2018 Day 24 Started to parse

--- src/main/kotlin/no/rodland/advent_2018/Day24.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day24.kt
@@ -1,7 +1,17 @@
 package no.rodland.advent_2018
 
+import get
+import getString
+
 object Day24 {
-    fun partOne(list: List<String>): Int {
+
+
+    // 989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+    val re = """(\d+) units each with (\d+) hit points \(?(.*?)\)? ?with an attack that does (\d+) (.*) damage at initiative (\d+)""".toRegex()
+
+    fun partOne(imuneList: List<String>, infectionList: List<String>): Int {
+        val immunes = imuneList.map { Group(Team.IMMUNE, it) }
+        val infections = infectionList.map { Group(Team.INFECTION, it) }
         return 2
     }
 
@@ -9,7 +19,9 @@ object Day24 {
         return 2
     }
 
-    data class Group(val team: Team, var num: Int, val damage: Int, val attack: String, val initiative: Int, val weakness: List<String>, val immunity: List<String>) {
+    data class Group(val team: Team, var num: Int, var hitPoints: Int, val damage: Int, val attack: String, val initiative: Int, val str: String?) {
+        constructor(team: Team, str: String) : this(team, re.get(str), re.get(str, 2), re.get(str, 4), re.getString(str, 5)!!, re.get(str, 6), re.getString(str, 3))
+
         val effective = num * damage
     }
 
--- src/test/kotlin/no/rodland/advent_2018/Day24Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day24Test.kt
@@ -12,20 +12,53 @@ internal class Day24Test {
     val test24 = listOf("1", "2")
 
     @Nested
-    inner class `Part 1` {
+    inner class Parsing {
         @Test
-        fun `24,1,test`() {
+        fun `24,1,parse`() {
             report {
-                Day24.partOne(test24) to 2
+                Day24.Group(Day24.Team.IMMUNE, "17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2") to
+                        Day24.Group(Day24.Team.IMMUNE, 17, 5390, 4507, "fire", 2, "weak to radiation, bludgeoning")
             }
         }
 
         @Test
-        fun `24,1,live`() {
+        fun `24,1,parse, 2`() {
             report {
-                Day24.partOne(data24) to 2
+                Day24.Group(Day24.Team.INFECTION, "17 units each with 5390 hit points with an attack that does 4507 fire damage at initiative 2") to
+                        Day24.Group(Day24.Team.INFECTION, 17, 5390, 4507, "fire", 2, "")
             }
         }
+
+        @Test
+        fun `24,1,parse, 3`() {
+            report {
+                Day24.Group(Day24.Team.INFECTION, "989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3") to
+                        Day24.Group(Day24.Team.INFECTION, 989, 1274, 25, "slashing", 3, "immune to fire; weak to bludgeoning, slashing")
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `24,1,test`() {
+            report {
+                Day24.partOne(listOf(
+                        "17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2",
+                        "989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3"
+                ), listOf(
+                        "801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1",
+                        "4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4"
+                )) to 2
+            }
+        }
+
+//        @Test
+//        fun `24,1,live`() {
+//            report {
+//                Day24.partOne(data24, emptyList()) to 2
+//            }
+//        }
     }
 
     @Nested
