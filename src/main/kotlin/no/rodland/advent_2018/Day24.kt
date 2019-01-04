c87954c8146a881c1c0e9710903e4c85c0c1c44a
Fredrik Rodland <git@rodland.no>
2019-01-04 10:57:05 +0100

    2018 Day 24 Parsing seems ok

--- src/main/kotlin/no/rodland/advent_2018/Day24.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day24.kt
@@ -8,10 +8,12 @@ object Day24 {
 
     // 989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
     val re = """(\d+) units each with (\d+) hit points \(?(.*?)\)? ?with an attack that does (\d+) (.*) damage at initiative (\d+)""".toRegex()
+    val reWeak = """.*weak to ([^;]*).*""".toRegex()
+    val reImmune = """.*immune to ([^;]*).*""".toRegex()
 
     fun partOne(imuneList: List<String>, infectionList: List<String>): Int {
-        val immunes = imuneList.map { Group(Team.IMMUNE, it) }
-        val infections = infectionList.map { Group(Team.INFECTION, it) }
+        val immunes = imuneList.map { Group.of(Team.IMMUNE, it) }
+        val infections = infectionList.map { Group.of(Team.INFECTION, it) }
         return 2
     }
 
@@ -19,10 +21,18 @@ object Day24 {
         return 2
     }
 
-    data class Group(val team: Team, var num: Int, var hitPoints: Int, val damage: Int, val attack: String, val initiative: Int, val str: String?) {
-        constructor(team: Team, str: String) : this(team, re.get(str), re.get(str, 2), re.get(str, 4), re.getString(str, 5)!!, re.get(str, 6), re.getString(str, 3))
+    data class Group(val team: Team, var num: Int, val hitPoints: Int, val damage: Int, val attack: String, val initiative: Int, val weaks: List<String>, val immune: List<String>) {
 
         val effective = num * damage
+
+        companion object {
+            fun of(team: Team, str: String): Group {
+                val info = re.getString(str, 3) ?: ""
+                val weakList = reWeak.getString(info)?.split(",")?.map { it.trim() } ?: emptyList()
+                val immuneList = reImmune.getString(info)?.split(", ")?.map { it.trim() } ?: emptyList()
+                return Group(team, re.get(str), re.get(str, 2), re.get(str, 4), re.getString(str, 5)!!, re.get(str, 6), weakList, immuneList)
+            }
+        }
     }
 
     enum class Team {
--- src/test/kotlin/no/rodland/advent_2018/Day24Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day24Test.kt
@@ -12,33 +12,6 @@ internal class Day24Test {
     val test24 = listOf("1", "2")
 
     @Nested
-    inner class Parsing {
-        @Test
-        fun `24,1,parse`() {
-            report {
-                Day24.Group(Day24.Team.IMMUNE, "17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2") to
-                        Day24.Group(Day24.Team.IMMUNE, 17, 5390, 4507, "fire", 2, "weak to radiation, bludgeoning")
-            }
-        }
-
-        @Test
-        fun `24,1,parse, 2`() {
-            report {
-                Day24.Group(Day24.Team.INFECTION, "17 units each with 5390 hit points with an attack that does 4507 fire damage at initiative 2") to
-                        Day24.Group(Day24.Team.INFECTION, 17, 5390, 4507, "fire", 2, "")
-            }
-        }
-
-        @Test
-        fun `24,1,parse, 3`() {
-            report {
-                Day24.Group(Day24.Team.INFECTION, "989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3") to
-                        Day24.Group(Day24.Team.INFECTION, 989, 1274, 25, "slashing", 3, "immune to fire; weak to bludgeoning, slashing")
-            }
-        }
-    }
-
-    @Nested
     inner class `Part 1` {
         @Test
         fun `24,1,test`() {
@@ -74,6 +47,49 @@ internal class Day24Test {
         fun `24,2,live`() {
             report {
                 Day24.partTwo(data24) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class Parsing {
+        @Test
+        fun `24,1,parse,1`() {
+            report {
+                Day24.Group.of(Day24.Team.IMMUNE, "17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2") to
+                        Day24.Group(Day24.Team.IMMUNE, 17, 5390, 4507, "fire", 2, listOf("radiation", "bludgeoning"), emptyList())
+            }
+        }
+
+        @Test
+        fun `24,1,parse,2`() {
+            report {
+                Day24.Group.of(Day24.Team.INFECTION, "17 units each with 5390 hit points with an attack that does 4507 fire damage at initiative 2") to
+                        Day24.Group(Day24.Team.INFECTION, 17, 5390, 4507, "fire", 2, emptyList(), emptyList())
+            }
+        }
+
+        @Test
+        fun `24,1,parse,3`() {
+            report {
+                Day24.Group.of(Day24.Team.INFECTION, "989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3") to
+                        Day24.Group(Day24.Team.INFECTION, 989, 1274, 25, "slashing", 3, listOf("bludgeoning", "slashing"), listOf("fire"))
+            }
+        }
+
+        @Test
+        fun `24,1,parse,4`() {
+            report {
+                Day24.Group.of(Day24.Team.INFECTION, "989 units each with 1274 hit points (weak to bludgeoning, slashing; immune to fire) with an attack that does 25 slashing damage at initiative 3") to
+                        Day24.Group(Day24.Team.INFECTION, 989, 1274, 25, "slashing", 3, listOf("bludgeoning", "slashing"), listOf("fire"))
+            }
+        }
+
+        @Test
+        fun `24,1,parse,5`() {
+            report {
+                Day24.Group.of(Day24.Team.INFECTION, "989 units each with 1274 hit points (immune to fire, slashing) with an attack that does 25 slashing damage at initiative 3") to
+                        Day24.Group(Day24.Team.INFECTION, 989, 1274, 25, "slashing", 3, emptyList(), listOf("fire", "slashing"))
             }
         }
     }
