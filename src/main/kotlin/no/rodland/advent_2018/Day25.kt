bbb813a25dbafe07157d8b7adc270b42d8dadde6
Fredrik Rodland <git@rodland.no>
2019-01-05 17:35:46 +0100

    2018 Day 25

--- src/main/kotlin/no/rodland/advent_2018/Day25.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day25.kt
@@ -2,10 +2,70 @@ package no.rodland.advent_2018
 
 object Day25 {
     fun partOne(list: List<String>): Int {
-        return 2
+        val points = list.map { Point4D(it) }
+        val neighbors = points.map { point -> point to points.filterNot { point == it }.filter { point.isCloseTo(it) } }
+        val constellations: MutableMap<Int, Set<Point4D>> = mutableMapOf()
+        var id = 0
+
+        neighbors.forEach { (k, v) ->
+            if (constellations.doesNotContainPoint(k)) {
+                constellations[id++] = setOf(k) + v
+            } else {
+                val currentId = constellations.getConstelltion(k)!!
+                constellations[currentId] = constellations[currentId]!! + setOf(k) + v
+            }
+        }
+
+        var numberOfConstellationEntries = constellations.map { (_, v) -> v.size }.sum()
+        while (numberOfConstellationEntries > points.size) {
+            merge(points, constellations)
+            numberOfConstellationEntries = constellations.map { (_, v) -> v.size }.sum()
+        }
+        println("number points: ${list.size}, number of constellations: ${constellations.size}, number of points $numberOfConstellationEntries")
+        return constellations.filterValues { it.size > 0 }.size
     }
 
-    fun partTwo(list: List<String>): Int {
-        return 2
+    private fun merge(points: List<Point4D>, constellations: MutableMap<Int, Set<Point4D>>) {
+        points.map { it to constellations.getConstelltions(it) }
+                .filter { it.second.size > 1 }
+                .forEach { (_, ints) ->
+                    val mergedConstellation = ints.flatMap { constellations[it]?.toList() ?: emptyList() }
+                    ints.forEach { constellations.remove(it) }
+                    constellations[constellations.maxId() + 1] = mergedConstellation.toSet()
+                }
+    }
+
+    data class Point4D(val x: Int, val y: Int, val z: Int, val t: Int) {
+        constructor(str: String) : this(str.split(",")[0].toInt(), str.split(",")[1].toInt(), str.split(",")[2].toInt(), str.split(",")[3].toInt())
+
+        fun distanceTo(other: Point4D): Int {
+            return Math.abs(other.x - x) + Math.abs(other.y - y) + Math.abs(other.z - z) + Math.abs(other.t - t)
+        }
+
+        fun isCloseTo(other: Point4D): Boolean {
+            return distanceTo(other) <= 3
+        }
     }
 }
+
+private fun Map<Int, Set<Day25.Point4D>>.doesNotContainPoint(p: Day25.Point4D): Boolean {
+    return !containsPoint(p)
+}
+
+private fun Map<Int, Set<Day25.Point4D>>.containsPoint(p: Day25.Point4D): Boolean {
+    return getConstelltion(p) != null
+}
+
+private fun Map<Int, Set<Day25.Point4D>>.getConstelltion(p: Day25.Point4D): Int? {
+    return this.filterValues { it.contains(p) }.keys.firstOrNull()
+}
+
+private fun Map<Int, Set<Day25.Point4D>>.getConstelltions(p: Day25.Point4D): Set<Int> {
+    return this.filterValues { it.contains(p) }.keys
+}
+
+private fun Map<Int, Set<Day25.Point4D>>.maxId(): Int {
+    return this.keys.max() ?: 0
+}
+
+
--- src/test/kotlin/no/rodland/advent_2018/Day25Test.kt
+++ src/test/kotlin/no/rodland/advent_2018/Day25Test.kt
@@ -1,47 +1,90 @@
 package no.rodland.advent_2018
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import readFile
 
 @DisableSlow
 internal class Day25Test {
     val data25 = "2018/input_25.txt".readFile()
-    val test25 = listOf("1", "2")
 
-    @Nested
-    inner class `Part 1` {
-        @Test
-        fun `25,1,test`() {
-            report {
-                Day25.partOne(test25) to 2
-            }
-        }
-
-        @Test
-        fun `25,1,live`() {
-            report {
-                Day25.partOne(data25) to 2
-            }
+    @Test
+    fun `25,1,test,1`() {
+        report {
+            Day25.partOne(listOf(
+                    "0,0,0,0",
+                    "3,0,0,0",
+                    "0,3,0,0",
+                    "0,0,3,0",
+                    "0,0,0,3",
+                    "0,0,0,6",
+                    "9,0,0,0",
+                    "12,0,0,0"
+            )) to 2
         }
     }
 
-    @Nested
-    inner class `Part 2` {
-        @Test
-        fun `25,2,test`() {
-            report {
-                Day25.partTwo(test25) to 2
-            }
+    @Test
+    fun `25,1,test,2`() {
+        report {
+            Day25.partOne(listOf(
+                    "-1,2,2,0",
+                    "0,0,2,-2",
+                    "0,0,0,-2",
+                    "-1,2,0,0",
+                    "-2,-2,-2,2",
+                    "3,0,2,-1",
+                    "-1,3,2,2",
+                    "-1,0,-1,0",
+                    "0,2,1,-2",
+                    "3,0,0,0"
+            )) to 4
         }
+    }
 
-        @Test
-        fun `25,2,live`() {
-            report {
-                Day25.partTwo(data25) to 2
-            }
+    @Test
+    fun `25,1,test,3`() {
+        report {
+            Day25.partOne(listOf(
+                    "1,-1,0,1",
+                    "2,0,-1,0",
+                    "3,2,-1,0",
+                    "0,0,3,1",
+                    "0,0,-1,-1",
+                    "2,3,-2,0",
+                    "-2,2,0,0",
+                    "2,-2,0,-1",
+                    "1,-1,0,-1",
+                    "3,2,0,2"
+            )) to 3
+        }
+    }
+
+    @Test
+    fun `25,1,test,4`() {
+        report {
+            Day25.partOne(listOf(
+                    "1,-1,-1,-2",
+                    "-2,-2,0,1",
+                    "0,2,1,3",
+                    "-2,3,-2,1",
+                    "0,2,3,-2",
+                    "-1,-1,1,-2",
+                    "0,-2,-1,0",
+                    "-2,2,3,-1",
+                    "1,2,2,0",
+                    "-1,-2,0,-2"
+            )) to 8
+        }
+    }
+
+    @Test
+    @Slow(800)
+    fun `25,1,live`() {
+        report {
+            Day25.partOne(data25) to 394
         }
     }
 }
