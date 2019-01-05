5655f291b26e8eca0df6f861e3434fbf58d74adf
Fredrik Rodland <git@rodland.no>
2019-01-05 18:32:04 +0100

    2018 Day 25 cleaned (removed unused code)

--- src/main/kotlin/no/rodland/advent_2018/Day25.kt
+++ src/main/kotlin/no/rodland/advent_2018/Day25.kt
@@ -10,7 +10,7 @@ object Day25 {
         println("INIT: number points: ${list.size}, number of constellation entries: $numberOfConstellationEntries, number of constellations: ${constellations.size}")
         while (numberOfConstellationEntries > points.size) {
             merge(points, constellations)
-            constellations = constellations.filterValues { it.size > 0 }.toMutableMap()
+            constellations = constellations.filterValues { it.isNotEmpty() }.toMutableMap()
             numberOfConstellationEntries = constellations.map { (_, v) -> v.size }.sum()
             println("LOOP: number points: ${list.size}, number of constellation entries: $numberOfConstellationEntries, number of constellations: ${constellations.size}")
         }
@@ -18,7 +18,7 @@ object Day25 {
     }
 
     private fun merge(points: List<Point4D>, constellations: MutableMap<Int, Set<Point4D>>) {
-        points.map { it to constellations.getConstelltions(it) }
+        points.map { it to constellations.getConstellations(it) }
                 .filter { it.second.size > 1 }
                 .forEach { (_, ints) ->
                     val mergedConstellation = ints.flatMap { constellations[it]?.toList() ?: emptyList() }
@@ -30,7 +30,7 @@ object Day25 {
     data class Point4D(val x: Int, val y: Int, val z: Int, val t: Int) {
         constructor(str: String) : this(str.split(",")[0].toInt(), str.split(",")[1].toInt(), str.split(",")[2].toInt(), str.split(",")[3].toInt())
 
-        fun distanceTo(other: Point4D): Int {
+        private fun distanceTo(other: Point4D): Int {
             return Math.abs(other.x - x) + Math.abs(other.y - y) + Math.abs(other.z - z) + Math.abs(other.t - t)
         }
 
@@ -38,26 +38,9 @@ object Day25 {
             return distanceTo(other) <= 3
         }
     }
-}
 
-private fun Map<Int, Set<Day25.Point4D>>.doesNotContainPoint(p: Day25.Point4D): Boolean {
-    return !containsPoint(p)
+    private fun Map<Int, Set<Day25.Point4D>>.getConstellations(p: Day25.Point4D): Set<Int> {
+        return this.filterValues { it.contains(p) }.keys
+    }
 }
-
-private fun Map<Int, Set<Day25.Point4D>>.containsPoint(p: Day25.Point4D): Boolean {
-    return getConstelltion(p) != null
-}
-
-private fun Map<Int, Set<Day25.Point4D>>.getConstelltion(p: Day25.Point4D): Int? {
-    return this.filterValues { it.contains(p) }.keys.firstOrNull()
-}
-
-private fun Map<Int, Set<Day25.Point4D>>.getConstelltions(p: Day25.Point4D): Set<Int> {
-    return this.filterValues { it.contains(p) }.keys
-}
-
-private fun Map<Int, Set<Day25.Point4D>>.maxId(): Int {
-    return this.keys.max() ?: 0
-}
-
 
