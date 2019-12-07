cba13250bf5c7be23ca71d8ebcd5289ee75880f4
Fredrik Rodland <git@rodland.no>
2019-12-07 12:37:49 +0100

    2019 07 01

--- src/main/kotlin/no/rodland/advent_2019/Day07.kt
+++ src/main/kotlin/no/rodland/advent_2019/Day07.kt
@@ -1,11 +1,38 @@
 package no.rodland.advent_2019
 
 object Day07 {
-    fun partOne(list: List<String>): Int {
-        return 2
+    fun partOne(program: List<Int>, start: Int = 1): Int {
+        return phaseCombos().map { runAmplifiers(program, it) }.max()!!
     }
 
-    fun partTwo(list: List<String>): Int {
+    private fun phaseCombos(): List<List<Int>> {
+        return (0..4).flatMap { p0 ->
+            (0..4).flatMap { p1 ->
+                (0..4).flatMap { p2 ->
+                    (0..4).flatMap { p3 ->
+                        (0..4).map { p4 ->
+                            listOf(p0, p1, p2, p3, p4)
+                        }
+                    }
+                }
+            }
+        }
+                .distinct()
+                .filterNot { it.toSet().size != it.size }
+    }
+
+    fun runAmplifiers(program: List<Int>, phases: List<Int>): Int {
+        val resA = IntCodeComputer(program, mutableListOf(phases[0], 0)).runProgram()
+        val resB = IntCodeComputer(program, mutableListOf(phases[1], resA)).runProgram()
+        val resC = IntCodeComputer(program, mutableListOf(phases[2], resB)).runProgram()
+        val resD = IntCodeComputer(program, mutableListOf(phases[3], resC)).runProgram()
+        val resE = IntCodeComputer(program, mutableListOf(phases[4], resD)).runProgram()
+        return resE
+    }
+
+
+    fun partTwo(program: List<Int>): Int {
         return 2
     }
 }
+
--- src/test/kotlin/no/rodland/advent_2019/Day07Test.kt
+++ src/test/kotlin/no/rodland/advent_2019/Day07Test.kt
@@ -4,26 +4,38 @@ import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import readFile
 
 @DisableSlow
 internal class Day07Test {
-    val data07 = "2019/input_07.txt".readFile()
-    val test07 = listOf("1", "2")
+    val data07 = "2019/input_07.txt".readFirstLineInts()
 
     @Nested
     inner class `Part 1` {
         @Test
-        fun `07,1,test`() {
+        fun `07,1,test,1`() {
             report {
-                Day07.partOne(test07) to 2
+                Day07.partOne(listOf(3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0)) to 43210
+            }
+        }
+
+        @Test
+        fun `07,1,test,2`() {
+            report {
+                Day07.partOne(listOf(3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0)) to 54321
+            }
+        }
+
+        @Test
+        fun `07,1,test,3`() {
+            report {
+                Day07.partOne(listOf(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0)) to 65210
             }
         }
 
         @Test
         fun `07,1,live`() {
             report {
-                Day07.partOne(data07) to 2
+                Day07.partOne(data07) to 79723
             }
         }
     }
@@ -33,7 +45,7 @@ internal class Day07Test {
         @Test
         fun `07,2,test`() {
             report {
-                Day07.partTwo(test07) to 2
+                Day07.partTwo(listOf(1, 2)) to 2
             }
         }
 
