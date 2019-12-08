8abb86f90eb303ff665efdb60d1fcd1f4ac4e9d5
Fredrik Rodland <git@rodland.no>
2019-12-08 11:43:19 +0100

    2019 08 02 Space Image Format (decode picture)

--- src/main/kotlin/no/rodland/advent_2019/Day08.kt
+++ src/main/kotlin/no/rodland/advent_2019/Day08.kt
@@ -7,7 +7,27 @@ object Day08 {
         return minZerosLayer.count { it == 1 } * minZerosLayer.count { it == 2 }
     }
 
-    fun partTwo(list: List<Int>): Int {
-        return 2
+    fun partTwo(list: List<Int>, width: Int, height: Int): List<List<Int>> {
+        val layers = list.chunked(width * height).map { it.chunked(width) }
+        val finalImage = (0 until height).map { h ->
+            (0 until width).map { w ->
+                getPixel(layers, w, h)
+            }
+        }
+        finalImage.map { line ->
+            line.forEach { pixel -> print(makeReadable(pixel)) }
+            println()
+        }
+        return finalImage
+    }
+
+    private fun makeReadable(pixel: Int): Char = when (pixel) {
+        1 -> '*'
+        0 -> ' '
+        else -> error("unknown pixel value: $pixel")
+    }
+
+    private fun getPixel(layers: List<List<List<Int>>>, w: Int, h: Int): Int {
+        return layers.map { it[h][w] }.first { it != 2 }
     }
 }
