2a126af03cc7dfaee62c9b6af08514078e5f830b
Fredrik Rodland <git@rodland.no>
2019-12-12 09:28:50 +0100

    2019 12 01

---
+++ src/main/kotlin/no/rodland/advent_2019/Day12.kt
@@ -0,0 +1,67 @@
+package no.rodland.advent_2019
+
+object Day12 {
+    fun partOne(list: List<List<Int>>, steps: Int): Int {
+        var io = Moon(list[0])
+        var europa = Moon(list[1])
+        var ganymede = Moon(list[2])
+        var callisto = Moon(list[3])
+
+        (1..steps).map {
+            val newVelocities: List<Pos3D> = applyGravity(listOf(io, europa, ganymede, callisto))
+            io = io.applyVelocity(newVelocities[0])
+            europa = europa.applyVelocity(newVelocities[1])
+            ganymede = ganymede.applyVelocity(newVelocities[2])
+            callisto = callisto.applyVelocity(newVelocities[3])
+        }
+        return listOf(io, europa, ganymede, callisto).map { it.energy() }.sum()
+    }
+
+    private fun applyGravity(moons: List<Moon>): List<Pos3D> {
+        return moons.map { moon -> moon.applyGravity(moons.filterNot { it == moon }) }
+    }
+
+
+    fun partTwo(list: List<List<Int>>): Int {
+        return 2
+    }
+
+    data class Moon(val pos: Pos3D, val velocity: Pos3D) {
+        constructor(list: List<Int>) : this(Pos3D(list[0], list[1], list[2]), Pos3D(0, 0, 0))
+
+        fun energy() = pos.energy() * velocity.energy()
+        fun applyVelocity(newVelocity: Pos3D): Moon = Moon(Pos3D(pos.x + newVelocity.x, pos.y + newVelocity.y, pos.z + newVelocity.z), newVelocity)
+
+        fun applyGravity(moons: List<Moon>): Pos3D {
+            return moons
+                    .map { this.applyGravity(it) }
+                    .fold(velocity) { acc, it -> Pos3D(acc.x + it.x, acc.y + it.y, acc.z + it.z) }
+        }
+
+        fun applyGravity(other: Moon): Pos3D {
+            val x = when {
+                pos.x > other.pos.x -> -1
+                pos.x < other.pos.x -> 1
+                else -> 0
+            }
+            val y = when {
+                pos.y > other.pos.y -> -1
+                pos.y < other.pos.y -> 1
+                else -> 0
+            }
+            val z = when {
+                pos.z > other.pos.z -> -1
+                pos.z < other.pos.z -> 1
+                else -> 0
+            }
+            return Pos3D(x, y, z)
+        }
+
+
+    }
+
+    data class Pos3D(val x: Int, val y: Int, val z: Int) {
+        fun energy(): Int = Math.abs(x) + Math.abs(y) + Math.abs(z)
+    }
+
+}
---
+++ src/test/kotlin/no/rodland/advent_2019/Day12Test.kt
@@ -0,0 +1,58 @@
+package no.rodland.advent_2019
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisableSlow
+internal class Day12Test {
+    val data12 = listOf(
+            listOf(14, 15, -2),
+            listOf(17, -3, 4),
+            listOf(6, 12, -13),
+            listOf(-2, 10, -8)
+    )
+    val test12 = listOf(
+            listOf(-1, 0, 2),
+            listOf(2, -10, -7),
+            listOf(4, -8, 8),
+            listOf(3, 5, -1)
+    )
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `12,1,test`() {
+            report {
+                Day12.partOne(test12, 10) to 179
+            }
+        }
+
+        @Test
+        fun `12,1,live`() {
+            report {
+                Day12.partOne(data12, 1000) to 10189
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `12,2,test`() {
+            report {
+                Day12.partTwo(test12) to 2
+            }
+        }
+
+        @Test
+        fun `12,2,live`() {
+            report {
+                Day12.partTwo(data12) to 2
+            }
+        }
+    }
+}
+
+
