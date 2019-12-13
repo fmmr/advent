5dafe9d9bda604ed2a804051ac24b25ff5fa25fd
Fredrik Rodland <git@rodland.no>
2019-12-13 08:34:25 +0100

    2019 13 skel

---
+++ src/test/kotlin/no/rodland/advent_2019/Day13Test.kt
@@ -0,0 +1,48 @@
+package no.rodland.advent_2019
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisableSlow
+internal class Day13Test {
+    val data13 = "2019/input_13.txt".readFirstLineStrings()
+    val test13 = listOf("1", "2")
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `13,1,test`() {
+            report {
+                Day13.partOne(test13) to 2
+            }
+        }
+
+        @Test
+        fun `13,1,live`() {
+            report {
+                Day13.partOne(data13) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `13,2,test`() {
+            report {
+                Day13.partTwo(test13) to 2
+            }
+        }
+
+        @Test
+        fun `13,2,live`() {
+            report {
+                Day13.partTwo(data13) to 2
+            }
+        }
+    }
+}
+
+
