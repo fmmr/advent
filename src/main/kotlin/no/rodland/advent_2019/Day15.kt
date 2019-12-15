1de7e23d1045a79f4c9c042ed86b44113f6cbdc1
Fredrik Rodland <git@rodland.no>
2019-12-15 09:48:52 +0100

    2019 15 skel

---
+++ src/main/kotlin/no/rodland/advent_2019/Day15.kt
@@ -0,0 +1,11 @@
+package no.rodland.advent_2019
+
+object Day15 {
+    fun partOne(list: List<String>): Int {
+        return 2
+    }
+
+    fun partTwo(list: List<String>): Int {
+        return 2
+    }
+}
---
+++ src/test/kotlin/no/rodland/advent_2019/Day15Test.kt
@@ -0,0 +1,48 @@
+package no.rodland.advent_2019
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisableSlow
+internal class Day15Test {
+    val data15 = "2019/input_15.txt".readFirstLineStrings()
+    val test15 = listOf("1", "2")
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `15,1,test`() {
+            report {
+                Day15.partOne(test15) to 2
+            }
+        }
+
+        @Test
+        fun `15,1,live`() {
+            report {
+                Day15.partOne(data15) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `15,2,test`() {
+            report {
+                Day15.partTwo(test15) to 2
+            }
+        }
+
+        @Test
+        fun `15,2,live`() {
+            report {
+                Day15.partTwo(data15) to 2
+            }
+        }
+    }
+}
+
+
