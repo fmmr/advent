205730ca0f21dadc49b105bc85b0a673ee40bef3
Fredrik Rodland <git@rodland.no>
2019-12-16 11:16:09 +0100

    2019 16 01

--- src/main/kotlin/no/rodland/advent_2019/Day16.kt
+++ src/main/kotlin/no/rodland/advent_2019/Day16.kt
@@ -1,8 +1,36 @@
 package no.rodland.advent_2019
 
 object Day16 {
-    fun partOne(list: List<Int>): Int {
-        return 2
+
+
+    fun partOne(list: List<Int>, numPhases: Int = 100): String {
+        val result = getPhase(list, numPhases)
+        return result.subList(0, 8).joinToString("") { it.toString() }
+    }
+
+    private fun getPhase(list: List<Int>, phase: Int): List<Int> {
+        return if (phase == 1) {
+            list.mapIndexed { idx, el ->
+                val pattern = getPattern(idx + 1).take(list.size).toList()
+                Math.abs(list.zip(pattern) { num, p -> num * p }.sum() % 10)
+            }
+
+        } else {
+            getPhase(list.mapIndexed { idx, el ->
+                val pattern = getPattern(idx + 1).take(list.size).toList()
+                Math.abs(list.zip(pattern) { num, p -> num * p }.sum() % 10)
+            }, phase - 1)
+        }
+    }
+
+    fun getPattern(num: Int): Sequence<Int> {
+        val basePattern = listOf(0, 1, 0, -1).flatMap { e -> (1..num).map { e } }
+        val mod = basePattern.size
+        return sequence {
+            var count = 1
+            while (true)
+                yield(basePattern[count++ % basePattern.size])
+        }
     }
 
     fun partTwo(list: List<Int>): Int {
--- src/test/kotlin/no/rodland/advent_2019/Day16Test.kt
+++ src/test/kotlin/no/rodland/advent_2019/Day16Test.kt
@@ -8,21 +8,84 @@ import org.junit.jupiter.api.Test
 @DisableSlow
 internal class Day16Test {
     val data16 = "2019/input_16.txt".readFirstLineConvertToInts()
-    val test16 = listOf(1, 2)
+    val test16_1 = "12345678".map { it.toString().toInt() }
+    val test16_2 = "80871224585914546619083218645595".map { it.toString().toInt() }
+    val test16_3 = "19617804207202209144916044189917".map { it.toString().toInt() }
+    val test16_4 = "69317163492948606335995924319873".map { it.toString().toInt() }
+
+
+    @Nested
+    inner class PatternTest {
+        @Test
+        fun `16,1,pattern,1`() {
+            report {
+                Day16.getPattern(1).take(10).toList() to listOf(1, 0, -1, 0, 1, 0, -1, 0, 1, 0)
+            }
+        }
+
+        @Test
+        fun `16,1,pattern,2`() {
+            report {
+                Day16.getPattern(2).take(15).toList() to listOf(0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1, -1)
+            }
+        }
+
+        @Test
+        fun `16,1,pattern,8`() {
+            report {
+                Day16.getPattern(8).take(35).toList() to listOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0)
+            }
+        }
+    }
 
     @Nested
     inner class `Part 1` {
         @Test
-        fun `16,1,test`() {
+        fun `16,1,test,1_1`() {
             report {
-                Day16.partOne(test16) to 2
+                Day16.partOne(test16_1, 1) to "48226158"
+            }
+        }
+
+        @Test
+        fun `16,1,test,1_2`() {
+            report {
+                Day16.partOne(test16_1, 2) to "34040438"
+            }
+        }
+
+        @Test
+        fun `16,1,test,1_4`() {
+            report {
+                Day16.partOne(test16_1, 4) to "01029498"
+            }
+        }
+
+        @Test
+        fun `16,1,test,2`() {
+            report {
+                Day16.partOne(test16_2) to "24176176"
+            }
+        }
+
+        @Test
+        fun `16,1,test,3`() {
+            report {
+                Day16.partOne(test16_3) to "73745418"
+            }
+        }
+
+        @Test
+        fun `16,1,test,4`() {
+            report {
+                Day16.partOne(test16_4) to "52432133"
             }
         }
 
         @Test
         fun `16,1,live`() {
             report {
-                Day16.partOne(data16) to 2
+                Day16.partOne(data16) to "23135243"
             }
         }
     }
@@ -32,7 +95,7 @@ internal class Day16Test {
         @Test
         fun `16,2,test`() {
             report {
-                Day16.partTwo(test16) to 2
+                Day16.partTwo(test16_2) to 2
             }
         }
 
