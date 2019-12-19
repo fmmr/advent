b103cc584c78261f4ab4c3ceaa1978d39b47c2bb
Fredrik Rodland <git@rodland.no>
2019-12-19 11:12:42 +0100

    2019 19 02

--- src/main/kotlin/no/rodland/advent_2019/Day19.kt
+++ src/main/kotlin/no/rodland/advent_2019/Day19.kt
@@ -5,11 +5,51 @@ import kotlinx.coroutines.runBlocking
 
 object Day19 {
     fun partOne(program: List<String>): Long {
-        return (0..49).flatMap { x ->
-            (0..49).map { y ->
-                getPos(program, x.toLong(), y.toLong())
+//        printMap(program, 100, 100)
+        var minX = 0L
+        return (0..49).flatMap { y ->
+            var foundAffected = false
+            var skipCheck = false
+            val numAffected = (minX..49).map { x ->
+                val affected = if (!skipCheck) getPos(program, x, y.toLong()) else 0L
+                if (affected == 1L) {
+                    if (!foundAffected) {
+                        minX = x + 1
+                    }
+                    foundAffected = true
+                } else {
+                    if (foundAffected) {
+                        skipCheck = true
+                    }
+                }
+                affected
             }
+            numAffected
         }.sum()
+    }
+
+    fun printMap(program: List<String>, minX: Int, maxX: Int, minY: Int, maxY: Int) {
+        return (minY..maxY).forEach { y ->
+            (minX..maxX).forEach { x ->
+                print(getPos(program, x.toLong(), y.toLong()))
+            }
+            println()
+        }
+    }
+
+    fun partTwo(program: List<String>): Long {
+        var found = false
+        // picked 2 reasonably high start values
+        var y = 800L
+        var x = 800L
+        while (!found) {
+            y++
+            while (getPos(program, x, y) != 1L) {
+                x++
+            }
+            found = getPos(program, x + 99, y - 99) == 1L
+        }
+        return x * 10000 + (y - 99)
     }
 
     private fun getPos(liprogramt: List<String>, x: Long, y: Long): Long {
@@ -25,8 +65,4 @@ object Day19 {
         }
     }
 
-
-    fun partTwo(list: List<String>): Int {
-        return 2
-    }
 }
--- src/test/kotlin/no/rodland/advent_2019/Day19Test.kt
+++ src/test/kotlin/no/rodland/advent_2019/Day19Test.kt
@@ -1,6 +1,7 @@
 package no.rodland.advent_2019
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -23,9 +24,17 @@ internal class Day19Test {
     @Nested
     inner class `Part 2` {
         @Test
+        @Slow(10000)
+        fun `19,2,printMap`() {
+            report {
+                Day19.printMap(data19, 900, 1100, 900, 1100) to 2L
+            }
+        }
+
+        @Test
         fun `19,2,live`() {
             report {
-                Day19.partTwo(data19) to 2
+                Day19.partTwo(data19) to 9290812L
             }
         }
     }
