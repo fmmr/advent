3b8a054914cd834aac04ee29fc79239ec247397b
Fredrik Rodland <git@rodland.no>
2019-12-23 16:13:19 +0100

    2019 23 still skel

--- src/main/kotlin/no/rodland/advent_2019/Day23.kt
+++ src/main/kotlin/no/rodland/advent_2019/Day23.kt
@@ -1,12 +1,45 @@
 package no.rodland.advent_2019
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 object Day23 {
     fun partOne(program: List<String>): Long {
+        val inputs = mutableListOf<Channel<Long>>()
+        val outputs = mutableListOf<Channel<Long>>()
+        val computers = mutableListOf<IntCodeComputer>()
+        val jobs = mutableListOf<Job>()
+
+        (0..49).forEach { networkAddress ->
+            val input = Channel<Long>(20)
+            val output = Channel<Long>(20)
+            inputs.add(input)
+            outputs.add(output)
+            val computer = IntCodeComputer()
+            computers.add(computer)
+            jobs.add(GlobalScope.launch {
+                computer.runSuspend(
+                        program,
+                        {
+                            if (input.isEmpty) {
+                                -1L
+                            } else {
+                                input.receive()
+                            }
+                        },
+                        {
+                            // first value is network address
+                            // second value is X
+                            // third value is Y
+                            output.send(it)
+                        }
+                )
+
+            })
+        }
         return 2
     }
 
--- src/test/kotlin/no/rodland/advent_2019/Day23Test.kt
+++ src/test/kotlin/no/rodland/advent_2019/Day23Test.kt
@@ -4,11 +4,10 @@ import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import readFile
 
 @DisableSlow
 internal class Day23Test {
-    val data23 = "2019/input_23.txt".readFile()
+    val data23 = "2019/input_23.txt".readFirstLineStrings()
     val test23 = listOf("1", "2")
 
     @Nested
@@ -16,14 +15,14 @@ internal class Day23Test {
         @Test
         fun `23,1,test`() {
             report {
-                Day23.partOne(test23) to 2
+                Day23.partOne(test23) to 2L
             }
         }
 
         @Test
         fun `23,1,live`() {
             report {
-                Day23.partOne(data23) to 2
+                Day23.partOne(data23) to 2L
             }
         }
     }
@@ -33,14 +32,14 @@ internal class Day23Test {
         @Test
         fun `23,2,test`() {
             report {
-                Day23.partTwo(test23) to 2
+                Day23.partTwo(test23) to 2L
             }
         }
 
         @Test
         fun `23,2,live`() {
             report {
-                Day23.partTwo(data23) to 2
+                Day23.partTwo(data23) to 2L
             }
         }
     }
