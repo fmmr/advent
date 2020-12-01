95c065895cc48a654278cbd6baba27d0cd17dc09
Fredrik Rodland <git@rodland.no>
2020-12-01 17:16:33 +0100

    Day01 cleaned up code a tad.

--- src/main/kotlin/no/rodland/advent_2020/Day01.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day01.kt
@@ -5,20 +5,14 @@ object Day01 {
 
     private fun findTarget(expences: List<Int>, target: Int): Int? {
         return expences
-            .firstOrNull { exp ->
-                expences.contains(target - exp)
-            }
+            .firstOrNull { expences.contains(target - it) }
             ?.let { (target - it) * it }
     }
 
-
     fun partTwo(expences: List<Int>): Int {
-        return expences.map { it to findTarget(expences, 2020 - it) }.first {
-            it.second != null
-        }.let { it.first * it.second!! }
+        return expences
+            .map { it to findTarget(expences, 2020 - it) }
+            .first { it.second != null }
+            .let { it.first * it.second!! }
     }
-
-//    fun findFuel2FromChriswk(weight: Int): Int {
-//        return generateSequence(partOne(weight)) { w -> partOne(w) }.takeWhile { it > 0 }.sum()
-//    }
 }
