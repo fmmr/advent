04e0ab1bbf6b9d6c7062b1e619c5049e6955a6a1
Fredrik Rodland <git@rodland.no>
2020-12-04 00:14:24 +0100

    Day 3 cleanup

--- src/main/kotlin/no/rodland/advent_2020/Day03.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day03.kt
@@ -9,7 +9,7 @@ object Day03 {
     fun partTwo(list: List<String>): Long {
         val maze = list.map { it.toCharArray() }.toTypedArray()
         val slopes = listOf(3 to 1, 1 to 1, 5 to 1, 7 to 1, 1 to 2)
-        return slopes.map { (dx, dy) -> countTrees(maze, dx, dy) }.fold(1) { acc, n -> acc * n }
+        return slopes.map { (dx, dy) -> countTrees(maze, dx, dy).toLong() }.reduce { n1, n2 -> n1 * n2 }
     }
 
     private fun countTrees(maze: Array<CharArray>, dx: Int, dy: Int): Int {
@@ -18,10 +18,12 @@ object Day03 {
         val count = generateSequence(Pos(0, 0)) { it.next(dx, dy, width) }
             .takeWhile { it.y < height }
             //.onEach { println("$it, dx: $dx, dy: $dy, width: $width, height: $height") }
-            .count { maze[it.y][it.x] == '#' }
+            .count { maze.isTree(it) }
         println("NUM TREES: $count  dx: $dx, dy: $dy, width: $width, height: $height")
         return count
     }
+
+    private fun Array<CharArray>.isTree(pos: Pos) = this[pos.y][pos.x] == '#'
 
     data class Pos(val x: Int, val y: Int) {
         fun next(dX: Int, dY: Int, maxX: Int) = Pos((x + dX) % maxX, y + dY)
