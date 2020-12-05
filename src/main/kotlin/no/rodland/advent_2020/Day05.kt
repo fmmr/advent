c66be1b6915a2386cb45654aeda50c08b88ce089
Fredrik Rodland <git@rodland.no>
2020-12-05 17:34:37 +0100

    Day 5 part 1 & 2 - inlined some variables & functions.

--- src/main/kotlin/no/rodland/advent_2020/Day05.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day05.kt
@@ -2,36 +2,33 @@ package no.rodland.advent_2020
 
 object Day05 {
     fun partOne(list: List<String>): Int {
-        return list.map { rowAndColum(it) }.map { seatId(it.first, it.second) }.maxByOrNull { it } ?: -1
+        return list.map { seatId(it) }.maxByOrNull { it }!!
     }
 
     fun partTwo(list: List<String>): Int {
-        val ids = list.map { rowAndColum(it) }.map { seatId(it.first, it.second) }
+        val ids = list.map { seatId(it) }
         val allSeats = (ids.min()!!..ids.max()!!).toList()
         return (allSeats - ids).first()
     }
 
-    private fun rowAndColum(boarding: String): Pair<Int, Int> {
-        val rowCommands = boarding.substring(0..6)
-        val colCommands = boarding.substring(7)
-        val row = find(0..127, rowCommands).first
-        val col = find(0..7, colCommands).first
-        return row to col
+    private fun seatId(boarding: String): Int {
+        val row = find(0..127, boarding.substring(0..6)).first
+        val col = find(0..7, boarding.substring(7)).first
+        return row * 8 + col
     }
 
-    private tailrec fun find(intRange: IntRange, rowCommands: String): IntRange {
-        return if (rowCommands.isEmpty()) {
+    private tailrec fun find(intRange: IntRange, commands: String): IntRange {
+        return if (commands.isEmpty()) {
             intRange
         } else {
-            val command = rowCommands[0]
-            val range = when (command) {
-                'F', 'L' -> intRange.first..((intRange.last + intRange.first) / 2)
-                'B', 'R' -> ((intRange.last + intRange.first) / 2 + 1)..intRange.last
-                else -> error("unknown command: $command")
-            }
-            find(range, rowCommands.substring(1))
+            find(
+                when (commands[0]) {
+                    'F', 'L' -> intRange.first..((intRange.last + intRange.first) / 2)
+                    'B', 'R' -> ((intRange.last + intRange.first) / 2 + 1)..intRange.last
+                    else -> error("unknown command: ${commands[0]}")
+                },
+                commands.substring(1)
+            )
         }
     }
-
-    fun seatId(row: Int, column: Int) = row * 8 + column
 }
