9055cd43db9b912406f8ed10310d5f2b4b3c47ef
Fredrik Rodland <git@rodland.no>
2020-12-06 01:09:26 +0100

    Day 5 just replace the commands with the binary

--- src/main/kotlin/no/rodland/advent_2020/Day05.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day05.kt
@@ -2,14 +2,22 @@ package no.rodland.advent_2020
 
 object Day05 {
     fun partOne(list: List<String>): Int {
-        return list.map { seatIdChriswk(it) }.maxByOrNull { it }!!
+        return list.map { seatIdSimple(it) }.maxByOrNull { it }!!
     }
 
     fun partTwo(list: List<String>): Int {
-        val ids = list.map { seatIdChriswk(it) }.toSet()
+        val ids = list.map { seatIdSimple(it) }.toSet()
         val min = ids.minOrNull()!!
         val max = ids.maxOrNull()!!
         return ((min..max) - ids).first()
+    }
+
+    @Suppress("unused")
+    private fun seatIdSimple(boarding: String): Int {
+        return boarding
+            .replace("[BR]".toRegex(), "1")
+            .replace("[FL]".toRegex(), "0")
+            .toInt(2)
     }
 
     @Suppress("unused")
