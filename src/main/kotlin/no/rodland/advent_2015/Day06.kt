c30f469bd67aeb3ade7f286095221a02529b14d6
Fredrik Rodland <git@rodland.no>
2020-12-06 23:26:04 +0100

    2015 Day 6 part 1 & 2 - christmas lights - logic in cmd

--- src/main/kotlin/no/rodland/advent_2015/Day06.kt
+++ src/main/kotlin/no/rodland/advent_2015/Day06.kt
@@ -3,29 +3,21 @@ package no.rodland.advent_2015
 object Day06 {
     fun partOne(instructions: List<String>): Int {
         val grid = Array(1000) { BooleanArray(1000) { false } }
-        instructions.map { Instruction.fromString(it) }.forEach {
-            grid.part1(it)
-        }
+        instructions.map { Instruction.fromString(it) }.forEach { grid.part1(it) }
         return grid.countLights()
     }
 
 
     fun partTwo(instructions: List<String>): Int {
         val grid = Array(1000) { IntArray(1000) { 0 } }
-        instructions.map { Instruction.fromString(it) }.forEach {
-            grid.part2(it)
-        }
+        instructions.map { Instruction.fromString(it) }.forEach { grid.part2(it) }
         return grid.countBrightness()
     }
 
     private fun Array<BooleanArray>.part1(instr: Instruction) {
         ((instr.from.first)..(instr.to.first)).map { x ->
             ((instr.from.second)..(instr.to.second)).map { y ->
-                this[x][y] = when (instr.command) {
-                    Cmd.ON -> true
-                    Cmd.OFF -> false
-                    Cmd.TOGGLE -> !this[x][y]
-                }
+                this[x][y] = instr.command.part1(this[x][y])
             }
         }
     }
@@ -33,11 +25,7 @@ object Day06 {
     private fun Array<IntArray>.part2(instr: Instruction) {
         ((instr.from.first)..(instr.to.first)).map { x ->
             ((instr.from.second)..(instr.to.second)).map { y ->
-                this[x][y] = when (instr.command) {
-                    Cmd.ON -> this[x][y] + 1
-                    Cmd.OFF -> maxOf(this[x][y] - 1, 0)
-                    Cmd.TOGGLE -> this[x][y] + 2
-                }
+                this[x][y] = instr.command.part2(this[x][y])
             }
         }
     }
@@ -49,23 +37,26 @@ object Day06 {
 
     data class Instruction(val command: Cmd, val from: Pair<Int, Int>, val to: Pair<Int, Int>) {
         companion object {
+            val regex = """(.*) (\d+),(\d+) through (\d+),(\d+)""".toRegex()
             fun fromString(str: String): Instruction {
-                val (cmdFrom, to) = str.split(" through ")
-                val lastSpace = cmdFrom.lastIndexOf(" ")
-                val from = cmdFrom.substring(lastSpace + 1)
                 val cmd = when {
                     str.contains("off") -> Cmd.OFF
                     str.contains("on") -> Cmd.ON
                     str.contains("toggle") -> Cmd.TOGGLE
                     else -> error("not able to parse command from $str")
                 }
-                return Instruction(cmd, from.toPair(), to.toPair())
+                val find = regex.find(str)!!
+                return Instruction(cmd, find.toInt(2) to find.toInt(3), find.toInt(4) to find.toInt(5))
             }
+
+            private fun MatchResult.toInt(i: Int) = groups[i]!!.value.toInt()
         }
     }
 
-    enum class Cmd {
-        ON, OFF, TOGGLE
+    enum class Cmd(val part1: (Boolean) -> Boolean, val part2: (Int) -> Int) {
+        ON({ true }, { i -> i + 1 }),
+        OFF({ false }, { i -> maxOf(i - 1, 0) }),
+        TOGGLE({ b -> !b }, { i -> i + 2 });
     }
 }
 
