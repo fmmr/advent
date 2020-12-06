b4a8c91ace6cc1ebfeb6b1fa75ea5da281ee7615
Fredrik Rodland <git@rodland.no>
2020-12-06 16:41:06 +0100

    2015 Day 1 part 1 and 2 - removed println()

--- src/main/kotlin/no/rodland/advent_2015/Day01.kt
+++ src/main/kotlin/no/rodland/advent_2015/Day01.kt
@@ -12,7 +12,7 @@ object Day01 {
     private fun traverse(list: String, stopOnBasement: Boolean = false) = list.foldIndexed(0) { idx, acc, c ->
         val newAcc = if (c == '(') acc + 1 else acc - 1
         if (stopOnBasement && newAcc < 0) {
-            println("A IDX: $idx, acc: $acc, c: $c")
+            // println("A IDX: $idx, acc: $acc, c: $c")
             return idx + 1
         }
         newAcc
