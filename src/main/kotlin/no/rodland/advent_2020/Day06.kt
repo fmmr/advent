45a4ceb9f03d6d89a4540cc66d2dda4da163d79b
Fredrik Rodland <git@rodland.no>
2020-12-06 11:37:07 +0100

    Day 6 part 1 & 2

--- src/main/kotlin/no/rodland/advent_2020/Day06.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day06.kt
@@ -1,11 +1,20 @@
 package no.rodland.advent_2020
 
 object Day06 {
-    fun partOne(list: List<String>): Int {
-        return 2
+    fun partOne(list: String): Int {
+        return list.split("\n\n")
+            .map { it.toCharArray().filterNot { it == '\n' }.distinct().count() }
+            .sum()
     }
 
-    fun partTwo(list: List<String>): Int {
-        return 2
+    fun partTwo(list: String): Int {
+        return list.split("\n\n").map { it.getGroupCount() }.sum()
+    }
+
+    private fun String.getGroupCount(): Int {
+        val unique = toCharArray().filterNot { it == '\n' }.distinct()
+        val persons = this.split("\n")
+        return unique.count { ch -> persons.all { ch in it } }
     }
 }
+
--- src/test/kotlin/no/rodland/advent_2020/Day06Test.kt
+++ src/test/kotlin/no/rodland/advent_2020/Day06Test.kt
@@ -4,27 +4,54 @@ import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import readFile
 
 @Suppress("ClassName")
 @DisableSlow
 internal class Day06Test {
-    val data06 = "2020/input_06.txt".readFile()
-    val test06 = listOf("1", "2")
+    val data06 = "2020/input_06.txt".readFileAsOneString()
+    val test06a = """
+        abcx
+        abcy
+        abcz
+        """.trimIndent()
+    val test06b = """
+        abc
+        
+        a
+        b
+        c
+        
+        ab
+        ac
+        
+        a
+        a
+        a
+        a
+        
+        b
+        """.trimIndent()
 
     @Nested
     inner class `Part 1` {
         @Test
-        fun `06,1,test`() {
+        fun `06,1,test,1`() {
             report {
-                Day06.partOne(test06) to 2
+                Day06.partOne(test06a) to 6
+            }
+        }
+
+        @Test
+        fun `06,1,test,2`() {
+            report {
+                Day06.partOne(test06b) to 11
             }
         }
 
         @Test
         fun `06,1,live`() {
             report {
-                Day06.partOne(data06) to 2
+                Day06.partOne(data06) to 7128
             }
         }
     }
@@ -34,14 +61,14 @@ internal class Day06Test {
         @Test
         fun `06,2,test`() {
             report {
-                Day06.partTwo(test06) to 2
+                Day06.partTwo(test06b) to 6
             }
         }
 
         @Test
         fun `06,2,live`() {
             report {
-                Day06.partTwo(data06) to 2
+                Day06.partTwo(data06) to 3640
             }
         }
     }
