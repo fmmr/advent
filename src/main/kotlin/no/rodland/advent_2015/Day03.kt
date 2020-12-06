9f6431bf87b848e9b963e464a3e9b729c706a747
Fredrik Rodland <git@rodland.no>
2020-12-06 17:50:08 +0100

    2015 Day 3 part 1 & 2 - cleaned

--- src/main/kotlin/no/rodland/advent_2015/Day03.kt
+++ src/main/kotlin/no/rodland/advent_2015/Day03.kt
@@ -4,20 +4,16 @@ import no.rodland.advent.Pos
 
 object Day03 {
     fun partOne(list: String): Int {
-        return countHouses(list)
-    }
-
-    private fun countHouses(instructions: String): Int {
-        return houses(instructions).distinct().count()
-    }
-
-    private fun houses(instructions: String): List<Pos> {
-        return instructions.runningFold(Pos(0, 0)) { acc, c -> acc.getNext(c) }
+        return houses(list).distinct().count()
     }
 
     fun partTwo(list: String): Int {
         val santa = list.filterIndexed { index, _ -> index % 2 == 0 }
         val roboSanta = list.filterIndexed { index, _ -> index % 2 == 1 }
         return (houses(santa) + houses(roboSanta)).distinct().count()
+    }
+
+    private fun houses(instructions: String): List<Pos> {
+        return instructions.runningFold(Pos(0, 0)) { acc, c -> acc.getNext(c) }
     }
 }
