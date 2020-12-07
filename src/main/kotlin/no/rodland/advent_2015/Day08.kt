aba0bfbbe8d7f907c554891eb4076f23ccf9ab90
Fredrik Rodland <git@rodland.no>
2020-12-07 20:38:28 +0100

    2015 Day 8 skel

---
+++ src/main/kotlin/no/rodland/advent_2015/Day08.kt
@@ -0,0 +1,11 @@
+package no.rodland.advent_2015
+
+object Day08 {
+    fun partOne(list: List<String>): Int {
+        return 2
+    }
+
+    fun partTwo(list: List<String>): Int {
+        return 2
+    }
+}
