cb77e7fd4fe7d6adf4f3988220fa89e90a35ce81
Fredrik Rodland <git@rodland.no>
2020-12-07 13:52:13 +0100

    2015 Day 7 part 1 - flatMap instead of map.flatten

--- src/main/kotlin/no/rodland/advent_2020/Day07.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day07.kt
@@ -7,38 +7,35 @@ object Day07 {
         var size = 0
         val seq = generateSequence(start) { newList ->
             (newList + bags.filter { fromTo -> newList.any { fromTo.second.contains(it) } }.map { it.first }).distinct()
+        }.takeWhile { newList ->
+            val prevSize = size
+            size = newList.size
+            newList.size > prevSize
         }
-            .takeWhile { newList ->
-                val prevSize = size
-                size = newList.size
-                newList.size > prevSize
-            }
         return seq.last().size
     }
 
     private fun fromTo(line: String): Pair<Bag, List<Bag>> {
         //    posh tomato bags contain 4 faded coral bags, 2 dull chartreuse bags, 4 shiny teal bags, 3 light beige bags.
         val splitted = line.split(" contain ", ", ", ".").filter { it.isNotEmpty() }
-        return Bag(splitted[0].replace(" bags", "")) to Bag.fromString(splitted.subList(1, splitted.size))
+        return Bag(splitted[0].replace(" bags", "")) to Bag.list(splitted.subList(1, splitted.size))
     }
 
     data class Bag(val color: String) {
         companion object {
             val regex = """(\d+) (.*) bags?""".toRegex()
             private fun MatchResult.toInt(i: Int) = groups[i]!!.value.toInt()
-            fun fromString(str: List<String>): List<Bag> {
-                return str
-                    .map {
-                        if ("no other bags" == it) {
-                            emptyList()
-                        } else {
-                            val find = regex.find(it)!!
-                            val number = find.toInt(1)
-                            val colour = find.groups[2]!!.value
-                            MutableList(number) { Bag(colour) }
-                        }
+            fun list(str: List<String>): List<Bag> {
+                return str.flatMap {
+                    if ("no other bags" == it) {
+                        emptyList()
+                    } else {
+                        val find = regex.find(it)!!
+                        val number = find.toInt(1)
+                        val colour = find.groups[2]!!.value
+                        MutableList(number) { Bag(colour) }
                     }
-                    .flatten()
+                }
             }
         }
     }
