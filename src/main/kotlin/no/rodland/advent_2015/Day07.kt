2ee8fb2c0aeceff5010d04c72d0dba8f247905af
Fredrik Rodland <git@rodland.no>
2020-12-07 20:23:37 +0100

    2015 Day 7 part 1

--- src/main/kotlin/no/rodland/advent_2015/Day07.kt
+++ src/main/kotlin/no/rodland/advent_2015/Day07.kt
@@ -1,11 +1,66 @@
 package no.rodland.advent_2015
 
+@Suppress("EXPERIMENTAL_API_USAGE")
 object Day07 {
-    fun partOne(list: List<String>): Int {
-        return 2
+
+    fun partOne(input: List<String>, letter: String): Int {
+        val curcuit = input.map { it.split(" -> ") }.map { it[1] to OpParam.fromString(it[0]) }.toMap().toMutableMap()
+        val node = curcuit[letter]!!
+        return calc(node, curcuit)
     }
 
     fun partTwo(list: List<String>): Int {
         return 2
+    }
+
+    private fun calc(g: OpParam, curcuit: MutableMap<String, OpParam>): Int {
+        val intParams = g.params.map {
+            if (it.toIntOrNull() != null) {
+                it.toIntOrNull()!!
+            } else {
+                val value = calc(curcuit[it]!!, curcuit)
+                curcuit[it] = OpParam(Operation.NUM, listOf(value.toString()))
+                value
+            }
+        }
+        val answer = g.operation.exec(intParams)
+        return if (answer < 0) answer.toUShort().toInt() else answer
+    }
+
+    data class OpParam(val operation: Operation, val params: List<String>) {
+        companion object {
+            val regex = """(.*) (.*) (.*)""".toRegex()
+
+            fun fromString(str: String): OpParam {
+                return when {
+                    !str.contains(" ") -> OpParam(Operation.NUM, listOf(str))
+                    str.contains("NOT ") -> OpParam(Operation.NOT, listOf(str.replace("NOT ", "")))
+                    else -> {
+                        val find = regex.find(str)!!
+                        OpParam(Operation.valueOf(find.groups[2]!!.value), listOf(find.groups[1]!!.value, find.groups[3]!!.value))
+                    }
+                }
+            }
+        }
+    }
+
+    enum class Operation {
+        NUM,
+        AND,
+        OR,
+        LSHIFT,
+        RSHIFT,
+        NOT;
+
+        fun exec(list: List<Int>): Int {
+            return when (this) {
+                NUM -> list[0]
+                AND -> list[0] and list[1]
+                OR -> list[0] or list[1]
+                LSHIFT -> list[0] shl list[1]
+                RSHIFT -> list[0] shr list[1]
+                NOT -> list[0].inv()
+            }
+        }
     }
 }
--- src/test/kotlin/no/rodland/advent_2015/Day07Test.kt
+++ src/test/kotlin/no/rodland/advent_2015/Day07Test.kt
@@ -9,14 +9,23 @@ import org.junit.jupiter.api.Test
 @DisableSlow
 internal class Day07Test {
     val data07 = "2015/input_07.txt".readFile()
-    val test07 = listOf("1", "2")
+    val test07 = listOf(
+        "123 -> x",
+        "456 -> y",
+        "x AND y -> d",
+        "x OR y -> e",
+        "x LSHIFT 2 -> f",
+        "y RSHIFT 2 -> g",
+        "NOT x -> h",
+        "NOT y -> i",
+    )
 
     @Nested
     inner class Init {
         @Test
         fun `07,1,live,init`() {
             report {
-                Day07.partOne(data07) to 2
+                Day07.partOne(data07, "a") to 16076
             }
         }
 
@@ -31,23 +40,73 @@ internal class Day07Test {
     @Nested
     inner class `Part 1` {
         @Test
-        fun `07,1,test`() {
+        fun `07,1,test,g`() {
             report {
-                Day07.partOne(test07) to 2
+                Day07.partOne(test07, "g") to 114
+            }
+        }
+
+        @Test
+        fun `07,1,test,d`() {
+            report {
+                Day07.partOne(test07, "d") to 72
+            }
+        }
+
+        @Test
+        fun `07,1,test,e`() {
+            report {
+                Day07.partOne(test07, "e") to 507
+            }
+        }
+
+        @Test
+        fun `07,1,test,f`() {
+            report {
+                Day07.partOne(test07, "f") to 492
+            }
+        }
+
+        @Test
+        fun `07,1,test,h`() {
+            report {
+                Day07.partOne(test07, "h") to 65412
+            }
+        }
+
+        @Test
+        fun `07,1,test,i`() {
+            report {
+                Day07.partOne(test07, "i") to 65079
+            }
+        }
+
+        @Test
+        fun `07,1,test,x`() {
+            report {
+
+                Day07.partOne(test07, "x") to 123
+            }
+        }
+
+        @Test
+        fun `07,1,test,y`() {
+            report {
+                Day07.partOne(test07, "y") to 456
             }
         }
 
         @Test
         fun `07,1,live,1`() {
             report {
-                Day07.partOne(data07) to 2
+                Day07.partOne(data07, "a") to 16076
             }
         }
 
         @Test
         fun `07,1,live,2`() {
             report {
-                Day07.partOne(data07) to 2
+                Day07.partOne(data07, "a") to 16076
             }
         }
     }
