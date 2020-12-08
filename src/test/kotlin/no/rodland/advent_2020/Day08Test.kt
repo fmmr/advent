3c8d389806cd858a45c6b998c8f346e819a04965
Fredrik Rodland <git@rodland.no>
2020-12-08 10:46:21 +0100

    2020 Day 8 skel

---
+++ src/test/kotlin/no/rodland/advent_2020/Day08Test.kt
@@ -0,0 +1,78 @@
+package no.rodland.advent_2020
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day08Test {
+    val data08 = "2020/input_08.txt".readFile()
+    val test08 = listOf("1", "2")
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `08,1,live,init`() {
+            report {
+                Day08.partOne(data08) to 2
+            }
+        }
+
+        @Test
+        fun `08,2,live,init`() {
+            report {
+                Day08.partTwo(data08) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `08,1,test`() {
+            report {
+                Day08.partOne(test08) to 2
+            }
+        }
+
+        @Test
+        fun `08,1,live,1`() {
+            report {
+                Day08.partOne(data08) to 2
+            }
+        }
+
+        @Test
+        fun `08,1,live,2`() {
+            report {
+                Day08.partOne(data08) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `08,2,test`() {
+            report {
+                Day08.partTwo(test08) to 2
+            }
+        }
+
+        @Test
+        fun `08,2,live,1`() {
+            report {
+                Day08.partTwo(data08) to 2
+            }
+        }
+
+        @Test
+        fun `08,2,live,2`() {
+            report {
+                Day08.partTwo(data08) to 2
+            }
+        }
+    }
+}
