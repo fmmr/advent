17ae48161e1df9d98a8bcdbfd048615f31a555fc
Fredrik Rodland <git@rodland.no>
2020-12-10 11:18:52 +0100

    2020 Day 10 - part 1 - cleaned

--- src/main/kotlin/no/rodland/advent_2020/Day10.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day10.kt
@@ -2,7 +2,14 @@ package no.rodland.advent_2020
 
 object Day10 {
     fun partOne(list: List<Int>): Int {
-        val diffs = (listOf(0) + list).sorted().windowed(2).map { it[1] - it[0] }.groupBy { it }.map { it.key to it.value.size }.toMap()
+        val diffs = (listOf(0) + list)
+            .asSequence()
+            .sorted()
+            .windowed(2)
+            .map { it[1] - it[0] }
+            .groupBy { it }
+            .map { it.key to it.value.size }
+            .toMap()
         return diffs[1]!! * (diffs[3]!! + 1)
     }
 
