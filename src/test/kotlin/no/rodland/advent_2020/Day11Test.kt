ea5ff6b60e5295254e44785b0cc04229d4c89613
Fredrik Rodland <git@rodland.no>
2020-12-11 10:04:16 +0100

    2020 Day11 skel

---
+++ src/test/kotlin/no/rodland/advent_2020/Day11Test.kt
@@ -0,0 +1,78 @@
+package no.rodland.advent_2020
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day11Test {
+    val data11 = "2020/input_11.txt".readFile()
+    val test11 = listOf("1", "2")
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `11,1,live,init`() {
+            report {
+                Day11.partOne(data11) to 2
+            }
+        }
+
+        @Test
+        fun `11,2,live,init`() {
+            report {
+                Day11.partTwo(data11) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `11,1,test`() {
+            report {
+                Day11.partOne(test11) to 2
+            }
+        }
+
+        @Test
+        fun `11,1,live,1`() {
+            report {
+                Day11.partOne(data11) to 2
+            }
+        }
+
+        @Test
+        fun `11,1,live,2`() {
+            report {
+                Day11.partOne(data11) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `11,2,test`() {
+            report {
+                Day11.partTwo(test11) to 2
+            }
+        }
+
+        @Test
+        fun `11,2,live,1`() {
+            report {
+                Day11.partTwo(data11) to 2
+            }
+        }
+
+        @Test
+        fun `11,2,live,2`() {
+            report {
+                Day11.partTwo(data11) to 2
+            }
+        }
+    }
+}
