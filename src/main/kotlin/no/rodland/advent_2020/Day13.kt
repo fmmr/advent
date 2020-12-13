d2513047b99de23f4f637c36d3aa89db0d6ea24a
Fredrik Rodland <git@rodland.no>
2020-12-13 11:22:10 +0100

    2020 Day 13 - part 1 - cleaned +/- 1

--- src/main/kotlin/no/rodland/advent_2020/Day13.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day13.kt
@@ -7,13 +7,15 @@ object Day13 {
         val earliestDeparture = list[0].toInt()
         val busses = list[1].split(",").filterNot { it == "x" }.map { it.toInt() }
         val int: Int? = null
-        val pair = generateSequence(earliestDeparture to int) { (nextInt, _) ->
-            (nextInt + 1) to busses.firstOrNull { nextInt % it == 0 }
-
-        }.takeWhileInclusive { it.second == null }.last()
-        val minute = pair.first - 1
-        val bus = pair.second!!
-        return (bus * (minute - earliestDeparture))
+        val minuteBus = generateSequence(earliestDeparture to int) { (i, _) ->
+            (i + 1).let { nextI ->
+                val bus = busses.firstOrNull { nextI % it == 0 }
+                nextI to bus
+            }
+        }
+            .takeWhileInclusive { it.second == null }
+            .last()
+        return (minuteBus.second!! * (minuteBus.first - earliestDeparture))
     }
 
     fun partTwo(list: List<String>): Int {
