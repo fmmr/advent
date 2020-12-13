5edfc5c90f5bd43a482b030d3d5b5af7d7752fac
Fredrik Rodland <git@rodland.no>
2020-12-13 09:52:22 +0100

    2020 Day 13 - skel

---
+++ src/test/kotlin/no/rodland/advent_2020/Day13Test.kt
@@ -0,0 +1,78 @@
+package no.rodland.advent_2020
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day13Test {
+    val data13 = "2020/input_13.txt".readFile()
+    val test13 = listOf("1", "2")
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `13,1,live,init`() {
+            report {
+                Day13.partOne(data13) to 2
+            }
+        }
+
+        @Test
+        fun `13,2,live,init`() {
+            report {
+                Day13.partTwo(data13) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `13,1,test`() {
+            report {
+                Day13.partOne(test13) to 2
+            }
+        }
+
+        @Test
+        fun `13,1,live,1`() {
+            report {
+                Day13.partOne(data13) to 2
+            }
+        }
+
+        @Test
+        fun `13,1,live,2`() {
+            report {
+                Day13.partOne(data13) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `13,2,test`() {
+            report {
+                Day13.partTwo(test13) to 2
+            }
+        }
+
+        @Test
+        fun `13,2,live,1`() {
+            report {
+                Day13.partTwo(data13) to 2
+            }
+        }
+
+        @Test
+        fun `13,2,live,2`() {
+            report {
+                Day13.partTwo(data13) to 2
+            }
+        }
+    }
+}
