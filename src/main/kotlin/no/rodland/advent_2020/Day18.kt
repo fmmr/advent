2f4b1a03f2b4dbeba1d82d5f080fdc9146b8d036
Fredrik Rodland <git@rodland.no>
2020-12-18 08:53:01 +0100

    2020 18 - skel

---
+++ src/main/kotlin/no/rodland/advent_2020/Day18.kt
@@ -0,0 +1,12 @@
+package no.rodland.advent_2020
+
+@Suppress("UNUSED_PARAMETER")
+object Day18 {
+    fun partOne(list: List<String>): Int {
+        return 2
+    }
+
+    fun partTwo(list: List<String>): Int {
+        return 2
+    }
+}
