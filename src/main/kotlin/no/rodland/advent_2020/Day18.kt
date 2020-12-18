58dce410613b757f410760528a56e03ef927b9e5
Fredrik Rodland <git@rodland.no>
2020-12-18 15:59:32 +0100

    2020 Day 18, cleaned

--- src/main/kotlin/no/rodland/advent_2020/Day18.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day18.kt
@@ -3,7 +3,7 @@ package no.rodland.advent_2020
 import java.lang.Character.getNumericValue
 import java.util.*
 
-@Suppress("UNUSED_PARAMETER")
+// https://en.wikipedia.org/wiki/Shunting-yard_algorithm
 object Day18 {
     fun partOne(list: List<String>): Long {
         return list.map { shuntingYard(it) }.map { rpn(it) }.sum()
@@ -14,22 +14,21 @@ object Day18 {
         return list.map { shuntingYard(it, precendence) }.map { rpn(it) }.sum()
     }
 
-    //123*+456+*+
     fun rpn(rpn: String): Long {
+        // example: 123*+456+*+
         val stack = Stack<Long>()
         var i = 0
         while (i < rpn.length) {
-            when (val next = rpn[i]) {
+            when (val next = rpn[i++]) {
                 '+', '*' -> stack.push(next.calc(stack.pop(), stack.pop()))
                 else -> stack.push(next.getNum())
             }
-            i++
         }
         return stack.pop()
     }
 
-    // 1 + (2 * 3) + (4 * (5 + 6))
     fun shuntingYard(input: String, precedence: Map<Char, Int> = mapOf('+' to 1, '*' to 1)): String {
+        // example: 1 + (2 * 3) + (4 * (5 + 6))
         val str = input.toCharArray().toList()
         val output = mutableListOf<Char>()
         val stack = Stack<Char>()
@@ -38,7 +37,7 @@ object Day18 {
                 ' ' -> {
                 }
                 '*', '+' -> {
-                    while (stack.isNotEmpty() && stack.peek() != '(' && stackHasHigherPrecedence(c, stack.peek(), precedence)) {
+                    while (popStack(stack, c, precedence)) {
                         output.add(stack.pop())
                     }
                     stack.push(c)
@@ -57,20 +56,19 @@ object Day18 {
         }
         output.addAll(stack.toList().reversed())
         return output.joinToString("")
-
     }
 
-    private fun stackHasHigherPrecedence(next: Char, topOfStack: Char, precedence: Map<Char, Int>): Boolean {
-        return precedence[topOfStack]!! >= precedence[next]!!
-    }
+    private fun popStack(stack: Stack<Char>, c: Char, precedence: Map<Char, Int>) = stack.isNotEmpty() && stack.peek() != '(' && stackHasHigherPrecedence(c, stack.peek(), precedence)
+
+    private fun stackHasHigherPrecedence(next: Char, topOfStack: Char, precedence: Map<Char, Int>) = precedence[topOfStack]!! >= precedence[next]!!
 
     private fun Char.getNum() = getNumericValue(this).toLong()
 
+    private fun Char.calc(pop: Long, pop1: Long): Long = when (this) {
+        '+' -> pop + pop1
+        '*' -> pop * pop1
+        else -> error("unable to calc $this with $pop and $pop1")
+    }
 }
 
-private fun Char.calc(pop: Long, pop1: Long): Long = when (this) {
-    '+' -> pop + pop1
-    '*' -> pop * pop1
-    else -> error("unable to calc $this with $pop and $pop1")
-}
 
