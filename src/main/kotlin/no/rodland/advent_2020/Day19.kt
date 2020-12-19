342c40d83f25b9654e284d4063ff469744678032
Fredrik Rodland <git@rodland.no>
2020-12-19 13:35:34 +0100

    2020 Day 19 - part 1 and 2

--- src/main/kotlin/no/rodland/advent_2020/Day19.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day19.kt
@@ -1,12 +1,48 @@
 package no.rodland.advent_2020
 
-@Suppress("UNUSED_PARAMETER")
 object Day19 {
     fun partOne(list: List<String>): Int {
-        return 2
+        val (ruleList, messages) = list.filterNot { it.isEmpty() }.partition { it.contains(": ") }
+        val rules = ruleList.map { it.split(": ") }.map { it.first().toInt() to it.last().replace("\"", "").trim() }.toMap()
+//        val shouldBeTrue = validate("ababbb", listOf(0), rules)
+//        val shouldBeFalse = validate("bababa", listOf(0), rules)
+        return messages.count {
+            val validated = validate(it, listOf(0), rules)
+//            println("validated $it $validated")
+            validated
+        }
     }
 
     fun partTwo(list: List<String>): Int {
-        return 2
+        val (ruleList, messages) = list.filterNot { it.isEmpty() }.partition { it.contains(": ") }
+        val rules = ruleList.map { it.split(": ") }.map { it.first().toInt() to it.last().replace("\"", "").trim() }.toMap().toMutableMap()
+        rules[8] = "42 | 42 8"
+        rules[11] = "42 31 | 42 11 31"
+        return messages.count {
+            validate(it, listOf(0), rules)
+        }
+
+    }
+
+    private fun validate(s: String, rulesToMatch: List<Int>, rules: Map<Int, String>): Boolean {
+        if (s.isEmpty()) {  // finished matching string - should be no more rules
+            return rulesToMatch.isEmpty()
+        }
+        if (rulesToMatch.isEmpty()) { // no more rules, but still got string to match - false
+            return false
+        }
+        val nextRuleToMatch = rules.getValue(rulesToMatch[0])
+//        println("matching $nextRuleToMatch on $s (rulesToMatch: $rulesToMatch)")
+        if (nextRuleToMatch[0] in 'a'..'b') {
+            return if ((s[0] == nextRuleToMatch[0])) { // next char matches char in rule
+                validate(s.drop(1), rulesToMatch.drop(1), rules)  // match rest if string to rest of rules
+            } else {
+                false  // nope - next char does not match
+            }
+        }
+        return nextRuleToMatch.split(" | ").any { rulePart ->
+            val newRules = rulePart.split(" ").map { splittedRule -> splittedRule.toInt() } + rulesToMatch.drop(1)
+            validate(s, newRules, rules)
+        }
     }
 }
--- src/test/kotlin/no/rodland/advent_2020/Day19Test.kt
+++ src/test/kotlin/no/rodland/advent_2020/Day19Test.kt
@@ -9,21 +9,34 @@ import org.junit.jupiter.api.Test
 @DisableSlow
 internal class Day19Test {
     val data19 = "2020/input_19.txt".readFile()
-    val test19 = listOf("1", "2")
+    val test19 = listOf(
+        "0: 4 1 5",
+        "1: 2 3 | 3 2",
+        "2: 4 4 | 5 5",
+        "3: 4 5 | 5 4",
+        "4: a",
+        "5: b",
+        "",
+        "ababbb",
+        "bababa",
+        "abbbab",
+        "aaabbb",
+        "aaaabbb",
+    )
 
     @Nested
     inner class Init {
         @Test
         fun `19,1,live,init`() {
             report {
-                Day19.partOne(data19) to 2
+                Day19.partOne(data19) to 171
             }
         }
 
         @Test
         fun `19,2,live,init`() {
             report {
-                Day19.partTwo(data19) to 2
+                Day19.partTwo(data19) to 369
             }
         }
     }
@@ -40,14 +53,14 @@ internal class Day19Test {
         @Test
         fun `19,1,live,1`() {
             report {
-                Day19.partOne(data19) to 2
+                Day19.partOne(data19) to 171
             }
         }
 
         @Test
         fun `19,1,live,2`() {
             report {
-                Day19.partOne(data19) to 2
+                Day19.partOne(data19) to 171
             }
         }
     }
@@ -55,23 +68,16 @@ internal class Day19Test {
     @Nested
     inner class `Part 2` {
         @Test
-        fun `19,2,test`() {
-            report {
-                Day19.partTwo(test19) to 2
-            }
-        }
-
-        @Test
         fun `19,2,live,1`() {
             report {
-                Day19.partTwo(data19) to 2
+                Day19.partTwo(data19) to 369
             }
         }
 
         @Test
         fun `19,2,live,2`() {
             report {
-                Day19.partTwo(data19) to 2
+                Day19.partTwo(data19) to 369
             }
         }
     }
