6bf0fb2c0618eb2fc369816625a9da76506307df
Fredrik Rodland <git@rodland.no>
2020-12-20 19:01:38 +0100

    2020 Day 20 - part 1 - refactored

--- src/main/kotlin/no/rodland/advent_2020/Day20.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day20.kt
@@ -1,69 +1,55 @@
 package no.rodland.advent_2020
 
 import product
+import kotlin.math.sqrt
 
-typealias Forrest = Array<CharArray>
+typealias Forrest = List<List<Char>>
 
 // --- Day 20: Jurassic Jigsaw ---
 @Suppress("UNUSED_PARAMETER")
 object Day20 {
     fun partOne(list: String): Long {
         val tiles = list.split("\n\n").map { Tile(it) }
-        val common = tiles.map { it to it.commonBorders(tiles) }.toMap()
-        return common.filter { it.value.size == 2 }.map { it.key.num.toLong() }.product()
+        val common = tiles.map { it.num to it.commonBorders(tiles) }.toMap()
+        return common.filter { it.value.size == 2 }.map { it.key.toLong() }.product()
     }
 
     fun partTwo(list: String): Int {
+        val tiles = list.split("\n\n").map { Tile(it) }
+        val common = tiles.map { it.num to it.commonBorders(tiles) }.toMap()
+        val aCorner = common.filter { it.value.size == 2 }.map { it.key }.first()
+        val length = sqrt(common.size.toDouble()).toInt()
+
+
+
         return 2
     }
 
     val regex = "Tile (\\d+):".toRegex()
 
+    fun Forrest.rotateR() = (0..this[0].size).map { colum(this[0].size - 1 - it) }
+    fun Forrest.rotateL() = rotateR().rotateR().rotateR()
+    fun Forrest.rotate2() = rotateR().rotateR()
+    fun Forrest.colum(col: Int) = this.map { it[col] }
+    fun Forrest.flipH() = map { it.reversed() }
+    fun Forrest.flipV() = reversed()
+
+    fun Forrest.commonBorders(forrest: Forrest): Set<List<Char>> = borders().intersect(forrest.borders())
+    fun Forrest.borders() = listOf(vBorders(), hBorders()).flatten()
+    fun Forrest.vBorders() = listOf(topBorder(), bottomBorder()).flatten()
+    fun Forrest.hBorders() = listOf(leftBorder(), rightBorder()).flatten()
+    fun Forrest.leftBorder() = map { it.first() }.let { listOf(it, it.reversed()) }
+    fun Forrest.rightBorder() = map { it.last() }.let { listOf(it, it.reversed()) }
+    fun Forrest.topBorder() = first().toList().let { listOf(it, it.reversed()) }
+    fun Forrest.bottomBorder() = last().toList().let { listOf(it, it.reversed()) }
+
     data class Tile(val num: Int, val forrest: Forrest) {
-        constructor(str: String, split: List<String> = str.split("\n")) : this(regex.find(split[0])?.groups?.get(1)?.value?.toInt() ?: -1, split.subList(1, split.size).map { it.toCharArray() }.toTypedArray())
+        constructor(str: String, split: List<String> = str.split("\n")) : this(regex.find(split[0])?.groups?.get(1)?.value?.toInt() ?: -1, split.subList(1, split.size).map { row -> row.map { it } })
+
+        fun commonBorders(tiles: List<Tile>): List<Tile> = tiles.filterNot { it == this }.filterNot { forrest.commonBorders(it.forrest).isEmpty() }
 
         override fun toString(): String {
             return "Tile: $num"
         }
-
-        fun commonBorders(tiles: List<Tile>): List<Tile> = tiles.filterNot { it == this }.filterNot { commonBorders(it).isEmpty() }
-
-        private fun commonBorders(tile: Tile): Set<List<Char>> = borders().intersect(tile.borders())
-
-        private fun borders() = listOf(vBorders(), hBorders()).flatten()
-        private fun vBorders() = listOf(topBorder(), bottomBorder()).flatten()
-        private fun hBorders() = listOf(leftBorder(), rightBorder()).flatten()
-
-
-        private fun leftBorder(): List<List<Char>> {
-            return try {
-                forrest.map { it.first() }.let { listOf(it, it.reversed()) }
-            } catch (e: Exception) {
-                error("no left border for $this")
-            }
-        }
-
-        private fun rightBorder() = forrest.map { it.last() }.let { listOf(it, it.reversed()) }
-        private fun topBorder() = forrest.first().toList().let { listOf(it, it.reversed()) }
-        private fun bottomBorder() = forrest.last().toList().let { listOf(it, it.reversed()) }
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Tile
-
-            if (num != other.num) return false
-            if (!forrest.contentDeepEquals(other.forrest)) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            var result = num
-            result = 31 * result + forrest.contentDeepHashCode()
-            return result
-        }
     }
 }
-
-
