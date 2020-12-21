1febb97182d3cc4091a7928013c26032f2eb64b3
Fredrik Rodland <git@rodland.no>
2020-12-21 15:26:26 +0100

    2020 Day 21 - part 1 (does not look good)

--- src/main/kotlin/no/rodland/advent_2020/Day21.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day21.kt
@@ -1,10 +1,57 @@
 package no.rodland.advent_2020
 
+typealias Ingredient = Map<String, List<String>>
+typealias Alergen = Map<String, List<String>>
+
 @Suppress("UNUSED_PARAMETER")
 object Day21 {
+    // rblnlnk spmsczc tlrpk nxs ... (contains soy, eggs, nuts)
+
+    // 85 That's not the right answer
+    // 4830 That's not the right answer
     fun partOne(list: List<String>): Int {
-        return 2
+        val splitted = list
+            .map { it.split("(contains ") }
+            .map { it.first().split(" ").filterNot { it.isEmpty() } to it.last().replace(")", "").split(", ").filterNot { it.isEmpty() } }
+
+        val allIngredients = splitted.flatMap { it.first }
+        val alergens: Alergen = splitted.flatMap { (i, a) ->
+            a.map { it to i }
+        }.toMap()
+        val ingredients: Ingredient = splitted.flatMap { (i, a) ->
+            i.map { it to a }
+        }.toMap()
+
+
+        val found = findFromAlergens(splitted)
+
+        return (allIngredients - found.values).size
     }
+
+    // uff....
+    private fun findFromAlergens(splitted: List<Pair<List<String>, List<String>>>): MutableMap<String, String> {
+        val mapValues = splitted
+            .flatMap { listList -> listList.second.map { allergen -> allergen to listList.first.filter { it.isNotEmpty() } } }
+            .groupBy { it.first }
+            .mapValues { (_, v) -> v.map { it.second } }
+        val parsed = mapValues
+            .mapValues { (_, v) -> v.map { it.toSet() } }
+            .map { (k, v) -> k to (v.reduce { acc, list -> acc.intersect(list) }) }
+            .toMap()
+        var mut = parsed.toMutableMap()
+        val found = mutableMapOf<String, String>()
+        while (mut.isNotEmpty()) {
+            val newFound = mut.filter { it.value.size == 1 }
+            newFound.forEach { k, ing ->
+                val ingredient = ing.first()
+                found[k] = ingredient
+                mut = mut.mapValues { (_, v) -> v.filterNot { it == ingredient }.toSet() }.toMutableMap()
+                mut.remove(k)
+            }
+        }
+        return found
+    }
+
 
     fun partTwo(list: List<String>): Int {
         return 2
--- src/test/kotlin/no/rodland/advent_2020/Day21Test.kt
+++ src/test/kotlin/no/rodland/advent_2020/Day21Test.kt
@@ -9,14 +9,19 @@ import org.junit.jupiter.api.Test
 @DisableSlow
 internal class Day21Test {
     val data21 = "2020/input_21.txt".readFile()
-    val test21 = listOf("1", "2")
+    val test21 = listOf(
+        "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+        "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+        "sqjhc fvjkl (contains soy)",
+        "sqjhc mxmxvkd sbzzf (contains fish)",
+    )
 
     @Nested
     inner class Init {
         @Test
         fun `21,1,live,init`() {
             report {
-                Day21.partOne(data21) to 2
+                Day21.partOne(data21) to 2569
             }
         }
 
@@ -33,21 +38,21 @@ internal class Day21Test {
         @Test
         fun `21,1,test`() {
             report {
-                Day21.partOne(test21) to 2
+                Day21.partOne(test21) to 5
             }
         }
 
         @Test
         fun `21,1,live,1`() {
             report {
-                Day21.partOne(data21) to 2
+                Day21.partOne(data21) to 2569
             }
         }
 
         @Test
         fun `21,1,live,2`() {
             report {
-                Day21.partOne(data21) to 2
+                Day21.partOne(data21) to 2569
             }
         }
     }
