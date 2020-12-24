06e9cdcdb748fa2b598a465078c91e31f3fb1522
Fredrik Rodland <git@rodland.no>
2020-12-24 02:12:14 +0100

    2020 day 23 Part 2 - cleaned

--- src/main/kotlin/no/rodland/advent_2020/Day23.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day23.kt
@@ -4,7 +4,7 @@ package no.rodland.advent_2020
 object Day23 {
     fun partOne(list: String, iterations: Int = 100): String {
         val original = list.map { c -> c.toString().toInt() }.map { LinkNext(it) }.toList()
-        val buffer = RingBuffer2(original)
+        val buffer = RingBuffer1(original)
         repeat(iterations) { buffer.play() }
         return buffer.resultPart1()
     }
@@ -12,27 +12,27 @@ object Day23 {
     fun partTwo(list: String, iterations: Int = 10000000): Long {
         val original = list.map { c -> c.toString().toInt() }
         val intList = (original + ((original.maxOrNull()!! + 1)..1000000)).map { LinkNext(it) }.toList()
-        val buffer = RingBuffer2(intList)
-        repeat(10000000) { buffer.play() }
-        val res = buffer.result()
+        val buffer = RingBuffer1(intList)
+        repeat(iterations) { buffer.play() }
+        val res = buffer.resultPart2()
         return res.second.num.toLong() * res.third.num.toLong()
     }
 
-    class RingBuffer2(data: List<LinkNext>) {
-
+    class RingBuffer1(data: List<LinkNext>) {
         private var dataArray = Array(data.size) { data[it] }
         var current = dataArray[0]
         val size = dataArray.size
         private var dataArrayFirst = Array(10) { idx -> dataArray.indexOfFirst { arrayElement -> arrayElement.num == idx } }
 
         init {
+            // TODO we can arrange the array (i.e by changing pointers, and skipping idx 0) so elements with num = i always reside in idx i - will make code in play simpler.
             dataArray.indices.take(dataArray.size - 1).forEach { idx -> data[idx].next = data[idx + 1] }
             dataArray.last().next = dataArray[0]
         }
 
         operator fun get(i: Int): LinkNext = dataArray.get(i)
 
-        fun result(): Triple<LinkNext, LinkNext, LinkNext> {
+        fun resultPart2(): Triple<LinkNext, LinkNext, LinkNext> {
             val indexOfOne = dataArray[dataArrayFirst[1]]
             return Triple(indexOfOne, indexOfOne.next, indexOfOne.next.next)
         }
@@ -47,7 +47,6 @@ object Day23 {
         }
 
         fun play() {
-
             val blackList1 = current.next
             val blackList2 = blackList1.next
             val blackList3 = blackList2.next
@@ -71,94 +70,8 @@ object Day23 {
         }
     }
 
-    @Suppress("unused")
-    private fun debug(i: Int, buffer: RingBuffer, num: Int, destination: Int, idx: Int) {
-        println("-- move $i --")
-        println("cups:  ${buffer.toString(num)}")
-        println("pick up: ${buffer.getBlackList()}")
-        println("destination: $destination")
-        println("idx_current: $idx")
-        println()
-    }
-
     class LinkNext(val num: Int) {
         lateinit var next: LinkNext
         override fun toString() = "link: $num, next: ${next.num}"
-    }
-
-    private fun playPart1(list: String, iterations: Int): RingBuffer {
-        val buffer = RingBuffer(list)
-        var idx = 0
-        for (i in 1..iterations) {
-            val num = buffer[idx]
-            buffer.blacklist(idx)
-            val destination = buffer.destination(num)
-            //  debug(i, buffer, num, destination, idx)
-            buffer.moveStuff(num, destination, i)
-            idx = buffer.indexAfter(num)
-        }
-        return buffer
-    }
-
-    class RingBuffer(data: List<Int>) : Iterable<Int> {
-        constructor(str: String) : this(str.map { c -> c.toString().toInt() })
-
-        private var dataArray = IntArray(data.size) { data[it] }
-        private var blackList = emptyList<Int>()
-
-        operator fun get(i: Int): Int = dataArray[i % dataArray.size]
-
-        fun destination(num: Int) = dataArray.filterNot { it in blackList }.filter { it < num }.maxOrNull() ?: dataArray.filterNot { it in blackList }.filterNot { it == num }.maxOrNull()!!
-
-        fun moveStuff(num: Int, destination: Int, iteration: Int) {
-            val idxNum = indexOf(num)
-
-            val blackListIndices = blackList.map { indexOf(it) }
-            val indices = (idxNum..(3 * dataArray.size)).filterNot { (it % dataArray.size) in blackListIndices }
-            val valuesWithOutBlackList = indices.map { get(it) }
-            val idxDest = valuesWithOutBlackList.indexOf(destination)
-
-            val data = (valuesWithOutBlackList.subList(0, idxDest + 1) + blackList + valuesWithOutBlackList.subList(idxDest + 1, valuesWithOutBlackList.size)).take(9)
-
-            dataArray = IntArray(dataArray.size) { data[it] }
-        }
-
-        fun getBlackList() = blackList
-
-        fun blacklist(idx: Int) {
-            blackList = get3After(idx)
-        }
-
-        fun get3After(i: Int): List<Int> = (1..3).map { dataArray[(it + i) % dataArray.size] }
-
-        fun indexOf(num: Int) = dataArray.indexOf(num)
-        fun indexAfter(num: Int) = dataArray.indexOf(num) + 1
-
-        fun sequence(start: Int = 0): Sequence<Int> {
-            var i = start
-            return sequence {
-                while (true) {
-                    yield(get(i++))
-                }
-            }
-        }
-
-        fun size(): Int = dataArray.size
-        fun result(): String {
-            val from = indexOf(1) + 1
-            return sequence(from).take(dataArray.size - 1).joinToString("")
-        }
-
-        override fun iterator(): Iterator<Int> = object : Iterator<Int> {
-            private var index: Int = 0
-            override fun hasNext(): Boolean = true
-            override fun next(): Int = get(index++)
-        }
-
-        fun toString(num: Int): String = toString().replace(num.toString(), "($num)")
-
-        override fun toString(): String {
-            return dataArray.joinToString(" ")
-        }
     }
 }
--- src/test/kotlin/no/rodland/advent_2020/Day23Test.kt
+++ src/test/kotlin/no/rodland/advent_2020/Day23Test.kt
@@ -3,10 +3,8 @@ package no.rodland.advent_2020
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.Slow
 import no.rodland.advent.report
-import no.rodland.advent_2020.Day23.RingBuffer
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 @Suppress("ClassName")
 @DisableSlow
@@ -41,72 +39,6 @@ internal class Day23Test {
         }
 
         @Test
-        fun `23,1,test,buffer_get`() {
-            report {
-                assertEquals(RingBuffer(listOf(1, 2, 3))[0], 1)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[1], 2)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[2], 3)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[3], 1)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[4], 2)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[5], 3)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[6], 1)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[7], 2)
-                assertEquals(RingBuffer(listOf(1, 2, 3))[8], 3)
-                1 to 1
-            }
-        }
-
-        @Test
-        fun `23,1,test,buffer_seq`() {
-            report {
-                val buffer = RingBuffer(listOf(1, 2, 3, 4, 5))
-                val seq = buffer.sequence().take(8).toList()
-                assertEquals(listOf(1, 2, 3, 4, 5, 1, 2, 3), seq)
-                1 to 1
-            }
-        }
-
-        @Test
-        fun `23,1,test,buffer_seq_start`() {
-            report {
-                val buffer = RingBuffer(listOf(1, 2, 3, 4, 5))
-                val seq = buffer.sequence(2).take(8).toList()
-                assertEquals(listOf(3, 4, 5, 1, 2, 3, 4, 5), seq)
-                1 to 1
-            }
-        }
-
-        @Test
-        fun `23,1,test,buffer_result`() {
-            report {
-                val buffer = RingBuffer(listOf(7, 8, 1, 3, 2, 4, 5))
-                assertEquals("324578", buffer.result())
-                1 to 1
-            }
-        }
-
-        @Test
-        fun `23,1,test,buffer_get3`() {
-            report {
-                val buffer = RingBuffer(listOf(7, 8, 1, 3, 2, 4, 5))
-                val idx = buffer.indexOf(4)
-                val three = buffer.get3After(idx)
-
-                assertEquals(listOf(5, 7, 8), three)
-                1 to 1
-            }
-        }
-
-        @Test
-        fun `23,1,test,buffer_indexof`() {
-            report {
-                val buffer = RingBuffer(listOf(1, 2, 3, 4, 5))
-                assertEquals(buffer.indexOf(4), 3)
-                1 to 1
-            }
-        }
-
-        @Test
         fun `23,1,live,1`() {
             report {
                 Day23.partOne(data23) to "78569234"
@@ -124,7 +56,7 @@ internal class Day23Test {
     @Nested
     inner class `Part 2` {
         @Test
-//        @Slow(5000)
+        @Slow(5000)
         fun `23,2,test`() {
             report {
                 Day23.partTwo(test23) to 149245887792
