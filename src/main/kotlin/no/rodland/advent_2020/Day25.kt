812cba11a822cd7025e2647f061da9915d697714
Fredrik Rodland <git@rodland.no>
2020-12-25 21:55:32 +0100

    2020 Day 25 - part 1 - with sequences instead of while loops.

--- src/main/kotlin/no/rodland/advent_2020/Day25.kt
+++ src/main/kotlin/no/rodland/advent_2020/Day25.kt
@@ -4,30 +4,12 @@ package no.rodland.advent_2020
 object Day25 {
     fun partOne(list: Pair<Long, Long>): Long {
         val (cardPK, doorPK) = list
-        val cardLoop = getLoop(cardPK)
-        return transform(doorPK, cardLoop)
+        return transform(doorPK, getLoop(cardPK))
     }
 
-    private fun getLoop(publicKey: Long): Int {
-        val subject = 7L
-        var current = subject
-        var loop = 1
-        while (current != publicKey) {
-            current = calc(current, subject)
-            loop++
-        }
-        return loop
-    }
+    private fun getLoop(publicKey: Long): Int = generateSequence(1L) { it.calc() }.indexOf(publicKey)
 
-    private fun transform(num: Long, loops: Int): Long {
-        var current = num
-        repeat(loops - 1) {
-            current = calc(current, num)
-        }
-        return current
-    }
+    private fun transform(num: Long, loops: Int) = generateSequence(1L) { it.calc(num) }.drop(loops).first()
 
-    private fun calc(current: Long, num: Long): Long {
-        return (current * num) % 20201227
-    }
+    private fun Long.calc(num: Long = 7L): Long = (this * num) % 20201227
 }
