32d2f2bee6ab41259b7b29ccb76f2584130f65df
Fredrik Rodland <git@rodland.no>
2021-01-10 23:48:13 +0100

    2015 Day 22 - skel

---
+++ src/main/kotlin/no/rodland/advent_2015/Day22.kt
@@ -0,0 +1,12 @@
+package no.rodland.advent_2015
+
+@Suppress("UNUSED_PARAMETER")
+object Day22 {
+    fun partOne(hitPoints: Int, damage: Int): Int {
+        return 2
+    }
+
+    fun partTwo(hitPoints: Int, damage: Int): Int {
+        return 2
+    }
+}
---
+++ src/test/kotlin/no/rodland/advent_2015/Day22Test.kt
@@ -0,0 +1,75 @@
+package no.rodland.advent_2015
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day22Test {
+    @Nested
+    inner class Init {
+        @Test
+        fun `22,1,live,init`() {
+            report {
+                Day22.partOne(51, 9) to 2
+            }
+        }
+
+        @Test
+        fun `22,2,live,init`() {
+            report {
+                Day22.partTwo(51, 9) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `22,1,test`() {
+            report {
+                Day22.partOne(51, 9) to 2
+            }
+        }
+
+        @Test
+        fun `22,1,live,1`() {
+            report {
+                Day22.partOne(51, 9) to 2
+            }
+        }
+
+        @Test
+        fun `22,1,live,2`() {
+            report {
+                Day22.partOne(51, 9) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `22,2,test`() {
+            report {
+                Day22.partTwo(51, 9) to 2
+            }
+        }
+
+        @Test
+        fun `22,2,live,1`() {
+            report {
+                Day22.partTwo(51, 9) to 2
+            }
+        }
+
+        @Test
+        fun `22,2,live,2`() {
+            report {
+                Day22.partTwo(51, 9) to 2
+            }
+        }
+    }
+}
