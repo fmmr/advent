f3e1c9575e8f91f5e2285486c8a7321243f99a0c
Fredrik Rodland <git@rodland.no>
2021-01-11 22:52:49 +0100

    2015 Day 24 - skel

---
+++ src/main/kotlin/no/rodland/advent_2015/Day24.kt
@@ -0,0 +1,12 @@
+package no.rodland.advent_2015
+
+@Suppress("UNUSED_PARAMETER")
+object Day24 {
+    fun partOne(list: List<Int>): Int {
+        return 2
+    }
+
+    fun partTwo(list: List<Int>): Int {
+        return 2
+    }
+}
