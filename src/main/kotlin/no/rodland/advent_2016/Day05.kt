326fbbee9f8c7641491df4f8c8f75eda35eb8e01
Fredrik Rodland <git@rodland.no>
2021-01-12 17:31:32 +0100

    2016 Day 05 - part 1 and 2

--- src/main/kotlin/no/rodland/advent_2016/Day05.kt
+++ src/main/kotlin/no/rodland/advent_2016/Day05.kt
@@ -1,12 +1,37 @@
 package no.rodland.advent_2016
 
-@Suppress("UNUSED_PARAMETER")
+import com.google.common.hash.Hashing.md5
+
+@Suppress("UNUSED_PARAMETER", "UnstableApiUsage", "DEPRECATION")
 object Day05 {
-    fun partOne(list: String): Int {
-        return 2
+
+    val md5 = md5()
+    fun partOne(doorId: String): String {
+        var i = 0
+        return generateSequence { md5.hashString((doorId + (i++)), Charsets.UTF_8) }
+                .map { it.toString() }
+                .filter { it.startsWith("00000") }
+                .map { it[5] }
+                .take(8)
+                .joinToString("")
     }
 
-    fun partTwo(list: String): Int {
-        return 2
+    fun partTwo(doorId: String): String {
+        var i = 0
+        val take = generateSequence { md5(doorId, i++) }
+                .filter { it.startsWith("00000") }
+                .filter { c -> c[5] in '0'..'7' }
+                .map { it[5] to it[6] }
+                .distinctBy { it.first }
+                .take(8)
+                .sortedBy { it.first }
+                .map { it.second }
+                .joinToString("")
+        println("iterations: $i")
+        return take
+    }
+
+    private fun md5(doorId: String, i: Int): String {
+        return md5.hashString((doorId + (i)), Charsets.UTF_8).toString()
     }
 }
--- src/test/kotlin/no/rodland/advent_2016/Day05Test.kt
+++ src/test/kotlin/no/rodland/advent_2016/Day05Test.kt
@@ -1,6 +1,7 @@
 package no.rodland.advent_2016
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -12,67 +13,40 @@ internal class Day05Test {
     val test05 = "abc"
 
     @Nested
-    inner class Init {
-        @Test
-        fun `05,1,live,init`() {
-            report {
-                Day05.partOne(data05) to 2
-            }
-        }
-
-        @Test
-        fun `05,2,live,init`() {
-            report {
-                Day05.partTwo(data05) to 2
-            }
-        }
-    }
-
-    @Nested
+    @Slow(10000)
     inner class `Part 1` {
         @Test
         fun `05,1,test`() {
             report {
-                Day05.partOne(test05) to 2
+                Day05.partOne(test05) to "18f47a30"
             }
         }
 
         @Test
         fun `05,1,live,1`() {
             report {
-                Day05.partOne(data05) to 2
+                Day05.partOne(data05) to "801b56a7"
             }
         }
 
-        @Test
-        fun `05,1,live,2`() {
-            report {
-                Day05.partOne(data05) to 2
-            }
-        }
     }
 
     @Nested
+//    @Slow(15000)
     inner class `Part 2` {
         @Test
         fun `05,2,test`() {
             report {
-                Day05.partTwo(test05) to 2
+                Day05.partTwo(test05) to "05ace8e3"
             }
         }
 
         @Test
         fun `05,2,live,1`() {
             report {
-                Day05.partTwo(data05) to 2
+                Day05.partTwo(data05) to "424a0197"
             }
         }
 
-        @Test
-        fun `05,2,live,2`() {
-            report {
-                Day05.partTwo(data05) to 2
-            }
-        }
     }
 }
