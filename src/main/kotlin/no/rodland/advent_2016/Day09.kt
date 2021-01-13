3d50485d0d43e0ee0e065a286e8ac67e1c93201a
Fredrik Rodland <git@rodland.no>
2021-01-13 23:30:11 +0100

    2016 Day 09 - part 1 and 2 - not pretty.

--- src/main/kotlin/no/rodland/advent_2016/Day09.kt
+++ src/main/kotlin/no/rodland/advent_2016/Day09.kt
@@ -2,11 +2,58 @@ package no.rodland.advent_2016
 
 @Suppress("UNUSED_PARAMETER")
 object Day09 {
-    fun partOne(list: List<String>): Int {
-        return 2
+    fun partOne(str: String): Int {
+        return decompressPart1(str).length
     }
 
-    fun partTwo(list: List<String>): Int {
-        return 2
+    fun partTwo(str: String): Long {
+        return getDecompressedLengthPart2(str)
     }
+
+    private fun getDecompressedLengthPart2(str: String): Long {
+        var counter = 0L
+        val sb = StringBuilder()
+        var buffer = sb
+        var i = 0
+        while (i < str.length) {
+            val c = str[i++]
+            if (c == '(') {
+                buffer = StringBuilder()
+            } else if (c == ')' && buffer != sb) {
+                val (num, times) = buffer.toString().split("x").map { it.toInt() }
+                val subStr = str.substring(i, i + num)
+                counter += times * getDecompressedLengthPart2(subStr)
+                i += num
+                buffer = sb
+            } else {
+                buffer.append(c)
+                if (buffer == sb) {
+                    counter++
+                }
+            }
+        }
+        return counter
+    }
+
+    fun decompressPart1(str: String): String {
+        val sb = StringBuilder()
+        var buffer = sb
+        var i = 0
+        while (i < str.length) {
+            val c = str[i++]
+            if (c == '(') {
+                buffer = StringBuilder()
+            } else if (c == ')' && buffer != sb) {
+                val (num, times) = buffer.toString().split("x").map { it.toInt() }
+                val subStr = str.substring(i, i + num).repeat(times)
+                sb.append(subStr)
+                i += num
+                buffer = sb
+            } else {
+                buffer.append(c)
+            }
+        }
+        return sb.toString()
+    }
+
 }
--- src/test/kotlin/no/rodland/advent_2016/Day09Test.kt
+++ src/test/kotlin/no/rodland/advent_2016/Day09Test.kt
@@ -8,25 +8,21 @@ import org.junit.jupiter.api.Test
 @Suppress("ClassName")
 @DisableSlow
 internal class Day09Test {
-    val data09 = "2016/input_09.txt".readFile()
-    val test09 = listOf(
-            "1",
-            "2",
-    )
+    val data09 = "2016/input_09.txt".readFileAsOneString()
 
     @Nested
     inner class Init {
         @Test
         fun `09,1,live,init`() {
             report {
-                Day09.partOne(data09) to 2
+                Day09.partOne(data09) to 123908
             }
         }
 
         @Test
         fun `09,2,live,init`() {
             report {
-                Day09.partTwo(data09) to 2
+                Day09.partTwo(data09) to 10755693147
             }
         }
     }
@@ -36,21 +32,63 @@ internal class Day09Test {
         @Test
         fun `09,1,test`() {
             report {
-                Day09.partOne(test09) to 2
+                Day09.partOne("A(1x5)BC") to 7
+            }
+        }
+
+        @Test
+        fun `09,1,test,1`() {
+            report {
+                Day09.decompressPart1("ADVENT") to "ADVENT"
+            }
+        }
+
+        @Test
+        fun `09,1,test,3`() {
+            report {
+                Day09.decompressPart1("A(1x5)BC") to "ABBBBBC"
+            }
+        }
+
+        @Test
+        fun `09,1,test,4`() {
+            report {
+                Day09.decompressPart1("(3x3)XYZ") to "XYZXYZXYZ"
+            }
+        }
+
+        @Test
+        fun `09,1,test,5`() {
+            report {
+                Day09.decompressPart1("A(2x2)BCD(2x2)EFG") to "ABCBCDEFEFG"
+            }
+        }
+
+        @Test
+        fun `09,1,test,6`() {
+            report {
+                Day09.decompressPart1("(6x1)(1x3)A") to "(1x3)A"
+            }
+        }
+
+        @Test
+        fun `09,1,test,7`() {
+            report {
+                Day09.decompressPart1("X(8x2)(3x3)ABCY") to "X(3x3)ABC(3x3)ABCY"
             }
         }
 
         @Test
         fun `09,1,live,1`() {
             report {
-                Day09.partOne(data09) to 2
+                Day09.partOne(data09) to 123908
             }
         }
 
         @Test
         fun `09,1,live,2`() {
             report {
-                Day09.partOne(data09) to 2
+                Day09.partOne(data09) to 123908
             }
         }
     }
@@ -58,23 +96,44 @@ internal class Day09Test {
     @Nested
     inner class `Part 2` {
         @Test
-        fun `09,2,test`() {
+        fun `09,2,test,1`() {
             report {
-                Day09.partTwo(test09) to 2
+                Day09.partTwo("(3x3)XYZ") to "XYZXYZXYZ".length.toLong()
+            }
+        }
+
+        @Test
+        fun `09,2,test,2`() {
+            report {
+                Day09.partTwo("X(8x2)(3x3)ABCY") to "XABCABCABCABCABCABCY".length.toLong()
+            }
+        }
+
+        @Test
+        fun `09,2,test,3`() {
+            report {
+                Day09.partTwo("(27x12)(20x12)(13x14)(7x10)(1x12)A") to 241920
+            }
+        }
+
+        @Test
+        fun `09,2,test,4`() {
+            report {
+                Day09.partTwo("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN") to 445
             }
         }
 
         @Test
         fun `09,2,live,1`() {
             report {
-                Day09.partTwo(data09) to 2
+                Day09.partTwo(data09) to 10755693147
             }
         }
 
         @Test
         fun `09,2,live,2`() {
             report {
-                Day09.partTwo(data09) to 2
+                Day09.partTwo(data09) to 10755693147
             }
         }
     }
