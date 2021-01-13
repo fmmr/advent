e93687a30922e2c588019057a9aa14454d3a3b18
Fredrik Rodland <git@rodland.no>
2021-01-13 13:05:23 +0100

    2016 Day 06 - cleaned

--- src/main/kotlin/no/rodland/advent_2016/Day06.kt
+++ src/main/kotlin/no/rodland/advent_2016/Day06.kt
@@ -5,22 +5,20 @@ import increment
 @Suppress("UNUSED_PARAMETER")
 object Day06 {
     fun partOne(list: List<String>): String {
-        val counters = (list[0].indices).map { mutableMapOf<Char, Int>() }
-        list.forEach { str ->
-            str.forEachIndexed { index, c ->
-                counters[index].increment(c)
-            }
-        }
-        return counters.map { map -> map.filterValues { value -> value == map.values.maxOrNull()!! }.keys.first() }.joinToString("")
+        return getString(list) { value -> value == values.maxOrNull()!! }
     }
 
     fun partTwo(list: List<String>): String {
+        return getString(list) { value -> value == values.minOrNull()!! }
+    }
+
+    private fun getString(list: List<String>, letterPick: Map<Char, Int>.(Int) -> Boolean): String {
         val counters = (list[0].indices).map { mutableMapOf<Char, Int>() }
         list.forEach { str ->
             str.forEachIndexed { index, c ->
                 counters[index].increment(c)
             }
         }
-        return counters.map { map -> map.filterValues { value -> value == map.values.minOrNull()!! }.keys.first() }.joinToString("")
+        return counters.map { map -> map.filterValues { map.letterPick(it) }.keys.first() }.joinToString("")
     }
 }
