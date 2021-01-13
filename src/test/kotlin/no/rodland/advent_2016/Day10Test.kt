f320272b367f4421a4450f553e41289b0b2486ac
Fredrik Rodland <git@rodland.no>
2021-01-13 23:31:13 +0100

    2016 Day 10 - skel

---
+++ src/test/kotlin/no/rodland/advent_2016/Day10Test.kt
@@ -0,0 +1,81 @@
+package no.rodland.advent_2016
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day10Test {
+    val data10 = "2016/input_10.txt".readFile()
+    val test10 = listOf(
+            "1",
+            "2",
+    )
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `10,1,live,init`() {
+            report {
+                Day10.partOne(data10) to 2
+            }
+        }
+
+        @Test
+        fun `10,2,live,init`() {
+            report {
+                Day10.partTwo(data10) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `10,1,test`() {
+            report {
+                Day10.partOne(test10) to 2
+            }
+        }
+
+        @Test
+        fun `10,1,live,1`() {
+            report {
+                Day10.partOne(data10) to 2
+            }
+        }
+
+        @Test
+        fun `10,1,live,2`() {
+            report {
+                Day10.partOne(data10) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `10,2,test`() {
+            report {
+                Day10.partTwo(test10) to 2
+            }
+        }
+
+        @Test
+        fun `10,2,live,1`() {
+            report {
+                Day10.partTwo(data10) to 2
+            }
+        }
+
+        @Test
+        fun `10,2,live,2`() {
+            report {
+                Day10.partTwo(data10) to 2
+            }
+        }
+    }
+}
