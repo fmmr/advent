47b66e031b18946e45cb95c00de7a5b846f76217
Fredrik Rodland <git@rodland.no>
2021-01-13 18:40:43 +0100

    2016 Day 08 - skel

---
+++ src/main/kotlin/no/rodland/advent_2016/Day08.kt
@@ -0,0 +1,12 @@
+package no.rodland.advent_2016
+
+@Suppress("UNUSED_PARAMETER")
+object Day08 {
+    fun partOne(list: List<String>): Int {
+        return 2
+    }
+
+    fun partTwo(list: List<String>): Int {
+        return 2
+    }
+}
