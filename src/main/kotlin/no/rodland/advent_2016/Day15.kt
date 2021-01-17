d22311783b455ed86ce2c44358f60d2aff0099f2
Fredrik Rodland <git@rodland.no>
2021-01-17 11:58:26 +0100

    2016 Day 15 - part1 & 2

--- src/main/kotlin/no/rodland/advent_2016/Day15.kt
+++ src/main/kotlin/no/rodland/advent_2016/Day15.kt
@@ -1,6 +1,5 @@
 package no.rodland.advent_2016
 
-@Suppress("UNUSED_PARAMETER")
 object Day15 {
 
     // "Disc #1 has 17 positions; at time=0, it is at position 1."
@@ -8,15 +7,12 @@ object Day15 {
 
     fun partOne(list: List<String>): Int {
         val discs = list.map { Disc(it) }
-        return 2
-    }
-
-    fun partTwo(list: List<String>): Int {
-        return 2
+        return generateSequence(0, Int::inc).filter { i -> discs.all { it.isAtZeroGivenTZero(i) } }.first()
     }
 
     data class Disc(val num: Int, val positions: Int, val start: Int) {
         constructor(str: String, r: MatchResult.Destructured = regex.find(str)!!.destructured) : this(r.component1().toInt(), r.component2().toInt(), r.component3().toInt())
 
+        fun isAtZeroGivenTZero(tZero: Int) = (num + tZero + start) % positions == 0
     }
 }
--- src/test/kotlin/no/rodland/advent_2016/Day15Test.kt
+++ src/test/kotlin/no/rodland/advent_2016/Day15Test.kt
@@ -16,6 +16,7 @@ internal class Day15Test {
             "Disc #5 has 3 positions; at time=0, it is at position 0.",
             "Disc #6 has 13 positions; at time=0, it is at position 5.",
     )
+    val data15_2 = data15 + "Disc #7 has 11 positions; at time=0, it is at position 0."
     val test15 = listOf(
             "Disc #1 has 5 positions; at time=0, it is at position 4.",
             "Disc #2 has 2 positions; at time=0, it is at position 1.",
@@ -26,14 +27,14 @@ internal class Day15Test {
         @Test
         fun `15,1,live,init`() {
             report {
-                Day15.partOne(data15) to 2
+                Day15.partOne(data15) to 317371
             }
         }
 
         @Test
         fun `15,2,live,init`() {
             report {
-                Day15.partTwo(data15) to 2
+                Day15.partOne(data15_2) to 2080951
             }
         }
     }
@@ -41,47 +42,73 @@ internal class Day15Test {
     @Nested
     inner class `Part 1` {
         @Test
+        fun `15,1,test,logic_0_0`() {
+            report {
+                val discs = test15.map { Day15.Disc(it) }
+                discs[0].isAtZeroGivenTZero(0) to true
+            }
+        }
+
+        @Test
+        fun `15,1,test,logic_0_5`() {
+            report {
+                val discs = test15.map { Day15.Disc(it) }
+                discs[0].isAtZeroGivenTZero(5) to true
+            }
+        }
+
+        @Test
+        fun `15,1,test,logic_1_0`() {
+            report {
+                val discs = test15.map { Day15.Disc(it) }
+                discs[1].isAtZeroGivenTZero(0) to false
+            }
+        }
+
+        @Test
+        fun `15,1,test,logic_1_5`() {
+            report {
+                val discs = test15.map { Day15.Disc(it) }
+                discs[1].isAtZeroGivenTZero(5) to true
+            }
+        }
+
+        @Test
         fun `15,1,test`() {
             report {
-                Day15.partOne(test15) to 2
+                Day15.partOne(test15) to 5
             }
         }
 
         @Test
         fun `15,1,live,1`() {
             report {
-                Day15.partOne(data15) to 2
+                Day15.partOne(data15) to 317371
             }
         }
 
         @Test
         fun `15,1,live,2`() {
             report {
-                Day15.partOne(data15) to 2
+                Day15.partOne(data15) to 317371
             }
         }
     }
 
     @Nested
     inner class `Part 2` {
-        @Test
-        fun `15,2,test`() {
-            report {
-                Day15.partTwo(test15) to 2
-            }
-        }
 
         @Test
         fun `15,2,live,1`() {
             report {
-                Day15.partTwo(data15) to 2
+                Day15.partOne(data15_2) to 2080951
             }
         }
 
         @Test
         fun `15,2,live,2`() {
             report {
-                Day15.partTwo(data15) to 2
+                Day15.partOne(data15_2) to 2080951
             }
         }
     }
