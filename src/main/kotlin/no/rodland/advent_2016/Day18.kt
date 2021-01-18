46d63d64b8b2425109d97bd8f89399407ebd9f61
Fredrik Rodland <git@rodland.no>
2021-01-18 23:42:08 +0100

    2016 Day 18 - skel

---
+++ src/main/kotlin/no/rodland/advent_2016/Day18.kt
@@ -0,0 +1,12 @@
+package no.rodland.advent_2016
+
+@Suppress("UNUSED_PARAMETER")
+object Day18 {
+    fun partOne(str: String): Int {
+        return 2
+    }
+
+    fun partTwo(str: String): Int {
+        return 2
+    }
+}
