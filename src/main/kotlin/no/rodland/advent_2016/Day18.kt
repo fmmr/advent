a282fe094ab8dee93cb3b079c182029f208d1893
Fredrik Rodland <git@rodland.no>
2021-01-19 00:03:29 +0100

    2016 Day 18 - part 1 & 2

--- src/main/kotlin/no/rodland/advent_2016/Day18.kt
+++ src/main/kotlin/no/rodland/advent_2016/Day18.kt
@@ -3,10 +3,25 @@ package no.rodland.advent_2016
 @Suppress("UNUSED_PARAMETER")
 object Day18 {
     fun partOne(str: String): Int {
-        return 2
+        return (1 until 40)
+                .runningFold(str) { acc, _ ->
+                    getNextRow(acc)
+                }
+//                .onEach { println(it.count { it == '.' }) }
+                .map { s -> s.count { it == '.' } }
+                .sum()
+    }
+
+    private fun getNextRow(str: String): String {
+        return (".$str.").windowed(3, 1, false).map { if (it[0] == it[2]) '.' else '^' }.joinToString("")
     }
 
     fun partTwo(str: String): Int {
-        return 2
+        return (1 until 400000)
+                .runningFold(str) { acc, _ ->
+                    getNextRow(acc)
+                }
+                .map { s -> s.count { it == '.' } }
+                .sum()
     }
 }
--- src/test/kotlin/no/rodland/advent_2016/Day18Test.kt
+++ src/test/kotlin/no/rodland/advent_2016/Day18Test.kt
@@ -1,6 +1,7 @@
 package no.rodland.advent_2016
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -16,62 +17,15 @@ internal class Day18Test {
         @Test
         fun `18,1,live,init`() {
             report {
-                Day18.partOne(data18) to 2
+                Day18.partOne(data18) to 1961
             }
         }
 
         @Test
+        @Slow(2700)
         fun `18,2,live,init`() {
             report {
-                Day18.partTwo(data18) to 2
-            }
-        }
-    }
-
-    @Nested
-    inner class `Part 1` {
-        @Test
-        fun `18,1,test`() {
-            report {
-                Day18.partOne(test18) to 2
-            }
-        }
-
-        @Test
-        fun `18,1,live,1`() {
-            report {
-                Day18.partOne(data18) to 2
-            }
-        }
-
-        @Test
-        fun `18,1,live,2`() {
-            report {
-                Day18.partOne(data18) to 2
-            }
-        }
-    }
-
-    @Nested
-    inner class `Part 2` {
-        @Test
-        fun `18,2,test`() {
-            report {
-                Day18.partTwo(test18) to 2
-            }
-        }
-
-        @Test
-        fun `18,2,live,1`() {
-            report {
-                Day18.partTwo(data18) to 2
-            }
-        }
-
-        @Test
-        fun `18,2,live,2`() {
-            report {
-                Day18.partTwo(data18) to 2
+                Day18.partTwo(data18) to 20000795
             }
         }
     }
