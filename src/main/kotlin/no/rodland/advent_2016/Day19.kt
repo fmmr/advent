452caf4415053d6dc3b5d2e765f2841b1d3bc8cc
Fredrik Rodland <git@rodland.no>
2021-01-19 01:33:54 +0100

    2016 Day 19 - part 2

--- src/main/kotlin/no/rodland/advent_2016/Day19.kt
+++ src/main/kotlin/no/rodland/advent_2016/Day19.kt
@@ -6,69 +6,51 @@ object Day19 {
     // The Josephus Problem - Numberphile https://www.youtube.com/watch?v=uCsD3ZGzMgE
 
     fun partOne(numElves: Int): Int {
-        return (generateSequence(1) { i -> 2 * i }.first { it > numElves } / 2).let { (numElves - it) * 2 + 1 }
+        val solutionA = (generateSequence(1) { i -> 2 * i }.first { it > numElves } / 2).let { (numElves - it) * 2 + 1 }
+        val solutionB = (numElves.toString(2).replaceFirst('1', '0') + '1').toInt(2)
+//        val solutionC = solvePart1(numElves)
+        return solutionB
     }
-
-    // 1508979 too low
 
     fun partTwo(numElves: Int): Int {
-        return 2
+        val powThreeLessTheNumElves = generateSequence(1) { i -> 3 * i }.first { it > numElves } / 3
+        val diff = numElves - powThreeLessTheNumElves
+        val solutionA = if (diff == 0) {
+            powThreeLessTheNumElves
+        } else if (numElves < (powThreeLessTheNumElves * 2)) {
+            diff
+        } else {
+            powThreeLessTheNumElves + ((numElves - 2 * powThreeLessTheNumElves) * 2)
+        }
+//        val solutionB = solvePart2(numElves)
+        return solutionA
     }
 
-    private fun naiveGamePart2(numElves: Int): Int {
-        val game = IntArray(numElves) { 1 }
+    // tginsberg
+    @Suppress("unused")
+    fun solvePart1(numElves: Int): Int {
+        val queue = ArrayDeque((1..numElves).toList())
+        while (queue.size > 1) {
+            queue.add(queue.removeFirst())
+            queue.removeFirst()
+        }
+        return queue.first()
+    }
 
-        var i = 0
-        var tmp: Int = 0
-        while (game.count { it > 0 } > 1) {
+    // tginsberg
+    @Suppress("unused")
+    fun solvePart2(numElves: Int): Int {
+        val left = ArrayDeque((1..numElves / 2).toList())
+        val right = ArrayDeque(((numElves / 2) + 1..numElves).toList())
 
-            // find player
-            while (tmp == 0) {
-                tmp = game[i++ % numElves]
-            }
-            val playerIdx = i - 1
+        while (left.size + right.size > 1) {
+            if (left.size > right.size) left.removeLast()
+            else right.removeFirst()
 
-            tmp = 0
-            // find victim
-            while (tmp == 0) {
-                tmp = game[i++ % numElves]
-            }
-            val victimIdx = i - 1
-
-            game[playerIdx % numElves] += game[victimIdx % numElves]
-            game[victimIdx % numElves] = 0
-            tmp = 0
+            right.addLast(left.removeFirst())
+            left.addLast(right.removeFirst())
         }
 
-        return game.indexOfFirst { it > 0 } + 1
+        return left.firstOrNull() ?: right.first()
     }
-
-    private fun naiveGamePart1(numElves: Int): Int {
-        val game = IntArray(numElves) { 1 }
-
-        var i = 0
-        var tmp: Int = 0
-        while (game.count { it > 0 } > 1) {
-
-            // find player
-            while (tmp == 0) {
-                tmp = game[i++ % numElves]
-            }
-            val playerIdx = i - 1
-
-            tmp = 0
-            // find victim
-            while (tmp == 0) {
-                tmp = game[i++ % numElves]
-            }
-            val victimIdx = i - 1
-
-            game[playerIdx % numElves] += game[victimIdx % numElves]
-            game[victimIdx % numElves] = 0
-            tmp = 0
-        }
-
-        return game.indexOfFirst { it > 0 } + 1
-    }
-
 }
--- src/test/kotlin/no/rodland/advent_2016/Day19Test.kt
+++ src/test/kotlin/no/rodland/advent_2016/Day19Test.kt
@@ -25,6 +25,14 @@ internal class Day19Test {
             }
         }
 
+
+        @Test
+        fun `19,2,test,many`() {
+            report {
+                listOf(242, 243, 244, 245, 161, 162, 163, 164).map { Day19.partTwo(it) } to listOf(241, 243, 1, 2, 80, 81, 83, 85)
+            }
+        }
+
         @Test
         fun `19,2,live,test`() {
             report {
@@ -35,7 +43,7 @@ internal class Day19Test {
         @Test
         fun `19,2,live,init`() {
             report {
-                Day19.partTwo(3017957) to 2
+                Day19.partTwo(3017957) to 1423634
             }
         }
     }
