5d08e448f6d407dbfc43e0d0873733333b320b3c
Fredrik Rodland <git@rodland.no>
2021-01-19 12:17:57 +0100

    2016 Day 21 - skel

---
+++ src/test/kotlin/no/rodland/advent_2016/Day21Test.kt
@@ -0,0 +1,81 @@
+package no.rodland.advent_2016
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day21Test {
+    val data21 = "2016/input_21.txt".readFile()
+    val test21 = listOf(
+            "1",
+            "2",
+    )
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `21,1,live,init`() {
+            report {
+                Day21.partOne(data21) to 2
+            }
+        }
+
+        @Test
+        fun `21,2,live,init`() {
+            report {
+                Day21.partTwo(data21) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `21,1,test`() {
+            report {
+                Day21.partOne(test21) to 2
+            }
+        }
+
+        @Test
+        fun `21,1,live,1`() {
+            report {
+                Day21.partOne(data21) to 2
+            }
+        }
+
+        @Test
+        fun `21,1,live,2`() {
+            report {
+                Day21.partOne(data21) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `21,2,test`() {
+            report {
+                Day21.partTwo(test21) to 2
+            }
+        }
+
+        @Test
+        fun `21,2,live,1`() {
+            report {
+                Day21.partTwo(data21) to 2
+            }
+        }
+
+        @Test
+        fun `21,2,live,2`() {
+            report {
+                Day21.partTwo(data21) to 2
+            }
+        }
+    }
+}
