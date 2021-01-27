f0205eb6c1a9b826441cd520b7026db768613874
Fredrik Rodland <git@rodland.no>
2021-01-27 10:37:52 +0100

    2017 Day 19 - skel

---
+++ src/main/kotlin/no/rodland/advent_2017/Day19.kt
@@ -0,0 +1,12 @@
+package no.rodland.advent_2017
+
+@Suppress("UNUSED_PARAMETER")
+object Day19 {
+    fun partOne(list: List<String>): Int {
+        return 2
+    }
+
+    fun partTwo(list: List<String>): Int {
+        return 2
+    }
+}
---
+++ src/test/kotlin/no/rodland/advent_2017/Day19Test.kt
@@ -0,0 +1,85 @@
+package no.rodland.advent_2017
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day19Test {
+    val data19 = "2017/input_19.txt".readFile()
+    val test19 = listOf(
+            "     |          ",
+            "     |  +--+    ",
+            "     A  |  C    ",
+            " F---|----E|--+ ",
+            "     |  |  |  D ",
+            "     +B-+  +--+ ",
+    )
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `19,1,live,init`() {
+            report {
+                Day19.partOne(data19) to 2
+            }
+        }
+
+        @Test
+        fun `19,2,live,init`() {
+            report {
+                Day19.partTwo(data19) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `19,1,test`() {
+            report {
+                Day19.partOne(test19) to 2
+            }
+        }
+
+        @Test
+        fun `19,1,live,1`() {
+            report {
+                Day19.partOne(data19) to 2
+            }
+        }
+
+        @Test
+        fun `19,1,live,2`() {
+            report {
+                Day19.partOne(data19) to 2
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `19,2,test`() {
+            report {
+                Day19.partTwo(test19) to 2
+            }
+        }
+
+        @Test
+        fun `19,2,live,1`() {
+            report {
+                Day19.partTwo(data19) to 2
+            }
+        }
+
+        @Test
+        fun `19,2,live,2`() {
+            report {
+                Day19.partTwo(data19) to 2
+            }
+        }
+    }
+}
