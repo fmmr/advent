784b09ca4c70883760dea9198b2d031258993a07
Fredrik Rodland <git@rodland.no>
2021-01-28 15:09:23 +0100

    2017 Day 21 - part 1

--- src/main/kotlin/no/rodland/advent_2017/Day21.kt
+++ src/main/kotlin/no/rodland/advent_2017/Day21.kt
@@ -2,18 +2,84 @@ package no.rodland.advent_2017
 
 @Suppress("UNUSED_PARAMETER")
 object Day21 {
-    fun partOne(list: List<String>): Int {
-        val rules = list.map { it.split(" => ") }.map { Grid(it[0]) to Grid(it[1]) }
-        return 2
+    fun partOne(list: List<String>, iterations: Int): Int {
+        val start = Grid(".#./..#/###")
+        val rules = list
+                .map { it.split(" => ") }
+                .map { Grid(it[0]) to Grid(it[1]) }
+                .flatMap { pair -> pair.first.allVariations().map { it to pair.second } }
+                .toMap()
+
+        return generateSequence(start) {
+            it.split().map { row ->
+                row.map { g ->
+                    val replacement = rules[g]
+                    replacement ?: error("no replacement found for:\n${g.toScreen()}")
+                }
+            }.concat()
+        }.drop(1)
+                .take(iterations)
+//                .onEach { println(it.toScreen()) }
+                .last()
+                .on
+    }
+
+    fun List<List<Grid>>.concat(): Grid {
+        return Grid(flatMap { row ->
+            row.reduce { acc, grid ->
+//                println("$acc    +   $grid")
+                acc + grid
+            }
+        })
     }
 
     data class Grid(val list: List<List<Char>>) : List<List<Char>> by list {
         constructor(s: String) : this(s.split("/").let { row -> row.map { it.toCharArray().toList() } })
 
+        val on by lazy { map { it.count { it == '#' } }.sum() }
+
+        operator fun plus(other: Grid): Grid = Grid(mapIndexed { index: Int, list: List<Char> -> list + other[index] })
+
+        fun split(): List<List<Grid>> {
+            return if (size <= 3) {
+                listOf(listOf(this))
+            } else if (size % 2 == 0) {
+                val rows = chunked(2)
+                val cols = rows.map { it[0].chunked(2).zip(it[1].chunked(2)) }
+                cols.map { it.map { g -> Grid(listOf(g.first, g.second)) } }
+            } else {
+                val rows = chunked(3)
+                return rows.map { gridRow ->
+                    val row1 = gridRow[0].chunked(3)
+                    val row2 = gridRow[1].chunked(3)
+                    val row3 = gridRow[2].chunked(3)
+                    row1.mapIndexed { index: Int, list: List<Char> -> Grid(listOf(row1[index], row2[index], row3[index])) }
+                }
+            }
+        }
+
+        fun allVariations() = sequence {
+            yield(this@Grid)
+            yield(flipV())
+            yield(flipH())
+            rotateR().let { rotated ->
+                yield(rotated)
+                yield(rotated.flipV())
+                yield(rotated.flipH())
+            }
+            yield(rotate2())
+            yield(rotateL())
+        }
+
+        fun rotateR() = Grid((0 until size).map { column(it).reversed() })
+        private fun rotateL() = rotateR().rotateR().rotateR()
+        private fun rotate2() = rotateR().rotateR()
+        private fun flipH() = Grid(map { it.reversed() })
+        private fun flipV() = Grid(reversed())
         private fun column(col: Int) = this.map { it[col] }
 
         override fun toString() = list.joinToString(",", prefix = "[", postfix = "]") { it.joinToString("") }
-        fun toScreen() = list.joinToString("\n") { it.joinToString("") }
+        fun toScreen() = list.joinToString("\n") { it.joinToString("") } + "\n"
     }
 
     fun partTwo(list: List<String>): Int {
--- src/test/kotlin/no/rodland/advent_2017/Day21Test.kt
+++ src/test/kotlin/no/rodland/advent_2017/Day21Test.kt
@@ -2,6 +2,7 @@ package no.rodland.advent_2017
 
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
+import no.rodland.advent_2017.Day21.concat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -19,7 +20,7 @@ internal class Day21Test {
         @Test
         fun `21,1,live,init`() {
             report {
-                Day21.partOne(data21) to 2
+                Day21.partOne(data21, 5) to 205
             }
         }
 
@@ -36,21 +37,124 @@ internal class Day21Test {
         @Test
         fun `21,1,test`() {
             report {
-                Day21.partOne(test21) to 2
+                Day21.partOne(test21, 2) to 12
+            }
+        }
+
+        @Test
+        fun `21,1,test,split 8`() {
+            report {
+                val splitted = Day21.Grid("abcdefgh/ijklmnop/qrstuvwx/yzæøå123/4567890a/bcdefghi/jklmnopq/rstuvwxy").split()
+                val test = listOf(
+                        splitted[0][0] == Day21.Grid("ab/ij"),
+                        splitted[2][3] == Day21.Grid("0a/hi"),
+                        splitted[3][3] == Day21.Grid("pq/xy"),
+                )
+                test to listOf(true, true, true)
+            }
+        }
+
+        @Test
+        fun `21,1,test,split 9`() {
+            report {
+                val splitted = Day21.Grid("abcdefghi/jklmnopqr/stuvwxyzæ/123456789/0abcdefgh/ijklmnopq/rstuvwxyz/æøå123456/789abcdef").split()
+                val test = listOf(
+                        splitted[0][0],
+                        splitted[1][1],
+                        splitted[2][2],
+                )
+                val fasit = listOf(
+                        Day21.Grid("abc/jkl/stu"),
+                        Day21.Grid("456/cde/lmn"),
+                        Day21.Grid("xyz/456/def"),
+                )
+                test.zip(fasit).all { it.first == it.second } to true
+            }
+        }
+
+        @Test
+        fun `21,1,test,split 6`() {
+            report {
+                val splitted = Day21.Grid("abcdef/ghijkl/mnopqr/stuvwx/123456/789abc").split()
+                val test = listOf(
+                        splitted[0][0] == Day21.Grid("ab/gh"),
+                        splitted[1][1] == Day21.Grid("op/uv"),
+                        splitted[1][2] == Day21.Grid("qr/wx"),
+                        splitted[2][2] == Day21.Grid("56/bc"),
+                )
+                test to listOf(true, true, true, true)
+            }
+        }
+
+        @Test
+        fun `21,1,test,concat`() {
+            report {
+                val test = listOf(
+                        listOf(Day21.Grid("ab/ef"), Day21.Grid("cd/gh")),
+                        listOf(Day21.Grid("12/56"), Day21.Grid("34/78"))
+                )
+                val concat = test.concat()
+                concat to Day21.Grid("abcd/efgh/1234/5678")
+            }
+        }
+
+        @Test
+        fun `21,1,test,split size 2`() {
+            report {
+                val grid = Day21.Grid("ab/cd")
+                grid.split() to listOf(listOf(grid))
+            }
+        }
+
+        @Test
+        fun `21,1,test,split size 3`() {
+            report {
+                val grid = Day21.Grid("abc/def/ghi")
+                grid.split() to listOf(listOf(grid))
+            }
+        }
+
+        @Test
+        fun `21,1,test,concat size 2`() {
+            report {
+                val grid = Day21.Grid("ab/cd")
+                val test = listOf(listOf(grid))
+                val concat = test.concat()
+                concat to grid
+            }
+        }
+
+        @Test
+        fun `21,1,test,concat size 3`() {
+            report {
+                val grid = Day21.Grid("abc/def/ghi")
+                val test = listOf(listOf(grid))
+                val concat = test.concat()
+                concat to grid
+            }
+        }
+
+
+        @Test
+        fun `21,1,test,split and concat`() {
+            report {
+                val grid = Day21.Grid("abcd/efgh/1234/5678")
+                val splitted = grid.split().concat()
+                splitted to grid
             }
         }
 
         @Test
         fun `21,1,live,1`() {
             report {
-                Day21.partOne(data21) to 2
+                Day21.partOne(data21, 5) to 205
             }
         }
 
         @Test
         fun `21,1,live,2`() {
             report {
-                Day21.partOne(data21) to 2
+                Day21.partOne(data21, 5) to 205
             }
         }
     }
