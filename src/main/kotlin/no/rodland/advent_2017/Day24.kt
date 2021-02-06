512dc60a78517ee550d01384234b4fcd164ee516
Fredrik Rodland <git@rodland.no>
2021-02-06 23:38:47 +0100

    2017 Day 24 - part 1

--- src/main/kotlin/no/rodland/advent_2017/Day24.kt
+++ src/main/kotlin/no/rodland/advent_2017/Day24.kt
@@ -3,10 +3,52 @@ package no.rodland.advent_2017
 @Suppress("UNUSED_PARAMETER")
 object Day24 {
     fun partOne(list: List<String>): Int {
-        return 2
+
+        val start = Port(0, 0, Used.FIRST)
+        val ports = list.map { it.split("/") }.map { Port(it.first().toInt(), it.last().toInt()) }.toSet()
+        val bridges = build(listOf(start), ports)
+        val strongestBridge = bridges.map { l -> l.sumBy { it.strength } }
+        return strongestBridge.maxOrNull()!!
     }
+
+    fun build(ports: List<Port> = emptyList(), available: Set<Port>): List<List<Port>> {
+        val last = ports.last()
+        val lastUnused = last.unused()
+        val nextPorts = available.filter { it.validFor(last) }
+
+        return if (nextPorts.size == 0) {
+            listOf(ports)
+        } else {
+            nextPorts.flatMap { nextPort ->
+                val portsWithNext = ports.dropLast(1) + Port(last.port1, last.port2, Used.BOTH) + Port(nextPort.port1, nextPort.port2, lastUnused)
+                val newAvailable = available - nextPort
+                build(portsWithNext, newAvailable)
+            }
+        }
+    }
+
 
     fun partTwo(list: List<String>): Int {
         return 2
     }
+
+    data class Port(val port1: Int, val port2: Int, val used: Used = Used.NONE) {
+        constructor(port1: Int, port2: Int, usedNum: Int) : this(port1, port2, if (usedNum == port1) Used.FIRST else Used.SECOND)
+
+        val strength = port1 + port2
+        fun validFor(p: Port) = validFor(p.unused())
+        fun validFor(i: Int) = port1 == i || port2 == i
+
+        fun unused() = when (used) {
+            Used.FIRST -> port2
+            Used.SECOND -> port1
+            Used.BOTH -> -1
+            else -> error("should not happen: $this $used")
+        }
+    }
+
+    enum class Used {
+        FIRST, SECOND, BOTH, NONE
+    }
+
 }
--- src/test/kotlin/no/rodland/advent_2017/Day24Test.kt
+++ src/test/kotlin/no/rodland/advent_2017/Day24Test.kt
@@ -1,6 +1,7 @@
 package no.rodland.advent_2017
 
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -10,19 +11,18 @@ import org.junit.jupiter.api.Test
 internal class Day24Test {
     val data24 = "2017/input_24.txt".readFile()
     val test24 = listOf(
-            "1",
-            "2",
+            "0/2",
+            "2/2",
+            "2/3",
+            "3/4",
+            "3/5",
+            "0/1",
+            "10/1",
+            "9/10",
     )
 
     @Nested
     inner class Init {
-        @Test
-        fun `24,1,live,init`() {
-            report {
-                Day24.partOne(data24) to 2
-            }
-        }
-
         @Test
         fun `24,2,live,init`() {
             report {
@@ -36,19 +36,13 @@ internal class Day24Test {
         @Test
         fun `24,1,test`() {
             report {
-                Day24.partOne(test24) to 2
+                Day24.partOne(test24) to 31
             }
         }
 
         @Test
+        @Slow(1800)
         fun `24,1,live,1`() {
-            report {
-                Day24.partOne(data24) to 2
-            }
-        }
-
-        @Test
-        fun `24,1,live,2`() {
             report {
                 Day24.partOne(data24) to 2
             }
