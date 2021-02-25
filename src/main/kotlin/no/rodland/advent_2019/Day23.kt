d7646916e9014ccb4a48756337023825072855d6
Fredrik Rodland <git@rodland.no>
2021-02-25 11:18:28 +0100

    2019 - Day 23 - part 1&2 Copied verbatim from Todd Ginsberg

--- src/main/kotlin/no/rodland/advent_2019/Day23.kt
+++ src/main/kotlin/no/rodland/advent_2019/Day23.kt
@@ -1,90 +1,112 @@
 package no.rodland.advent_2019
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
+import com.ginsberg.advent2019.IntCodeComputerMk2
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+// copy of https://todd.ginsberg.com/post/advent-of-code/2019/day23/
 object Day23 {
-    fun partOne(program: List<String>): Long {
-        val inputs = mutableListOf<Channel<Long>>()
-        val outputs = mutableListOf<Channel<Long>>()
-        val computers = mutableListOf<IntCodeComputer>()
-        val jobs = mutableListOf<Job>()
+    fun partOne(input: List<String>): Int {
+        val program = input.withIndex().associateTo(mutableMapOf()) { it.index.toLong() to it.value.toLong() }
+        return runComputers(program) { _, nat, answerChannel ->
+            answerChannel.send(nat.receive())
+        }
+    }
 
-        (0..49).forEach { networkAddress ->
-            val input = Channel<Long>(20)
-            val output = Channel<Long>(20)
-            inputs.add(input)
-            outputs.add(output)
-            val computer = IntCodeComputer()
-            computers.add(computer)
-            jobs.add(GlobalScope.launch {
-                computer.runSuspend(
-                        program,
-                        {
-                            if (input.isEmpty) {
-                                -1L
-                            } else {
-                                input.receive()
+    @ExperimentalCoroutinesApi
+    fun partTwo(input: List<String>): Int {
+        val program = input.withIndex().associateTo(mutableMapOf()) { it.index.toLong() to it.value.toLong() }
+        return runComputers(program) { computers, nat, answerChannel ->
+            coroutineScope {
+                var mostRecentNatValue: Pair<Long, Long> = nat.receive()
+                launch {
+                    while (true) {
+                        mostRecentNatValue = nat.receive()
+                    }
+                }
+                launch {
+                    var latestDelivery: Long? = null
+                    while (true) {
+                        if (computers.all { it.input.isEmpty }) {
+                            val toSend = mostRecentNatValue
+                            computers[0].input.send(toSend.first)
+                            computers[0].input.send(toSend.second)
+
+                            if (latestDelivery == toSend.second) {
+                                answerChannel.send(toSend)
                             }
-                        },
-                        {
-                            // first value is network address
-                            // second value is X
-                            // third value is Y
-                            output.send(it)
+                            latestDelivery = toSend.second
                         }
-                )
-
-            })
+                        delay(15) // Give the computers a chance to work
+                    }
+                }
+            }
         }
-        return 2
     }
 
-    fun partTwo(program: List<String>): Long {
-        return 2
+    private fun runComputers(program: MutableMap<Long, Long>, natMessageHandler: suspend (Array<IntCodeComputerMk2>, ReceiveChannel<Pair<Long, Long>>, SendChannel<Pair<Long, Long>>) -> Unit): Int = runBlocking {
+        val computers = Array(50) {
+            IntCodeComputerMk2(program = program.copyOf(), output = Channel(UNLIMITED))
+        }
+        val answer = Channel<Pair<Long, Long>>(CONFLATED)
+        val nat = Channel<Pair<Long, Long>>(UNLIMITED)
+        val router = router(computers, nat)
+
+        launch {
+            natMessageHandler(computers, nat, answer)
+        }
+
+        computers.forEachIndexed { id, computer ->
+            launch {
+                computer.runProgram()
+            }
+            // Initialize with network id.
+            computer.input.send(id.toLong())
+            computer.input.send(-1)
+
+            launch {
+                while (true) {
+                    router.send(
+                            Triple(
+                                    computer.output.receive(),
+                                    computer.output.receive(),
+                                    computer.output.receive()
+                            )
+                    )
+                }
+            }
+        }
+
+        answer.receive().second.toInt().also {
+            this.coroutineContext.cancelChildren()
+        }
     }
 
-    private fun runSpringScript(program: List<String>, cmds: List<String>): Long {
-        val input = Channel<Long>(20)
-        val output = Channel<Long>(20)
-        var count = 0L
-        var h = 0L
-        val job = IntCodeComputer().launch(program, input, output)
-        val out = GlobalScope.launch {
+    private fun CoroutineScope.router(computers: Array<IntCodeComputerMk2>, nat: SendChannel<Pair<Long, Long>>): SendChannel<Triple<Long, Long, Long>> {
+        val input: Channel<Triple<Long, Long, Long>> = Channel(UNLIMITED)
+        launch {
             while (true) {
-                try {
-                    h = output.receive()
-                    val hei = h.toChar()
-                    count++
-                    print("$hei")
-                } catch (e: Exception) {
-                    println("GOT EXCEPTION ")
-                    break
+                val (to, x, y) = input.receive()
+                if (to == 255L) {
+                    nat.send(Pair(x, y))
+                } else {
+                    computers[to.toInt()].input.send(x)
+                    computers[to.toInt()].input.send(y)
                 }
             }
         }
-        println()
-
-
-        runBlocking {
-            cmds.forEach { line ->
-                line.forEach {
-                    input.send(it.toLong())
-                }
-                input.send(10L)
-            }
-        }
-
-        runBlocking {
-            job.join()
-            out.join()
-        }
-
-        return h
+        return input
     }
 
-
+    private fun <T, R> MutableMap<T, R>.copyOf(): MutableMap<T, R> = mutableMapOf<T, R>().also { it.putAll(this) }
 }
--- src/test/kotlin/no/rodland/advent_2019/Day23Test.kt
+++ src/test/kotlin/no/rodland/advent_2019/Day23Test.kt
@@ -1,5 +1,6 @@
 package no.rodland.advent_2019
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
 import org.junit.jupiter.api.Nested
@@ -8,38 +9,24 @@ import org.junit.jupiter.api.Test
 @DisableSlow
 internal class Day23Test {
     val data23 = "2019/input_23.txt".readFirstLineStrings()
-    val test23 = listOf("1", "2")
 
     @Nested
     inner class `Part 1` {
         @Test
-        fun `23,1,test`() {
-            report {
-                Day23.partOne(test23) to 2L
-            }
-        }
-
-        @Test
         fun `23,1,live`() {
             report {
-                Day23.partOne(data23) to 2L
+                Day23.partOne(data23) to 23266
             }
         }
     }
 
     @Nested
     inner class `Part 2` {
-        @Test
-        fun `23,2,test`() {
-            report {
-                Day23.partTwo(test23) to 2L
-            }
-        }
-
+        @ExperimentalCoroutinesApi
         @Test
         fun `23,2,live`() {
             report {
-                Day23.partTwo(data23) to 2L
+                Day23.partTwo(data23) to 17493
             }
         }
     }
