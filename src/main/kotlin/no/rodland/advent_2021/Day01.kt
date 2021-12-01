a8e03962aa5f445ed8602f615f7d6fa625a2e5a1
Fredrik Rodland <git@rodland.no>
2021-12-01 19:33:43 +0000

    2021 - Day 01 - countIncreased impl as receiver

--- src/main/kotlin/no/rodland/advent_2021/Day01.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day01.kt
@@ -3,12 +3,12 @@ package no.rodland.advent_2021
 @Suppress("UNUSED_PARAMETER")
 object Day01 {
     fun partOne(list: List<Int>): Int {
-        return countIncreased(list)
+        return list.countIncreased()
     }
 
-    private fun countIncreased(list: List<Int>) = list.zipWithNext().count { it.second > it.first }
+    private fun List<Int>.countIncreased() = zipWithNext().count { it.second > it.first }
 
     fun partTwo(list: List<Int>): Int {
-        return countIncreased(list.windowed(3).map { it.sum() })
+        return list.windowed(3).map { it.sum() }.countIncreased()
     }
 }
