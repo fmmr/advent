a2258a5eb0a712a3beaa0a374ec9850325633152
Fredrik Rodland <git@rodland.no>
2021-12-03 12:54:49 +0000

    2021 - Day 03 - part 2 - cleaned code

--- src/main/kotlin/no/rodland/advent_2021/Day03.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day03.kt
@@ -2,37 +2,30 @@ package no.rodland.advent_2021
 
 object Day03 {
     fun partOne(list: List<String>): Int {
-        val length = list[0].length
-        val gamma = (0 until length).map { mostUsedBit(list, it) }.joinToString("")
+        val gamma = (0 until list[0].length).map { mostUsedBit(list, it) }.joinToString("")
         val epsilon = gamma.flipBits()
         return gamma.toInt(2) * epsilon.toInt(2)
     }
 
     fun partTwo(list: List<String>): Int {
-        val oxygen = find(list, ::mostUsedBit)
-        val co2 = find(list, ::leastUsedBit)
+        val oxygen = findRating(list, ::mostUsedBit)
+        val co2 = findRating(list, ::leastUsedBit)
         return oxygen.toInt(2) * co2.toInt(2)
     }
 
-    private fun find(list: List<String>, func: (List<String>, Int) -> Char) = list
+    private fun findRating(list: List<String>, bitCriteria: (List<String>, Int) -> Char) = list
         .indices
         .asSequence()
-        .runningFold(list) { l, idx ->
-            val bitCriteria = func(l, idx)
-            l.filter { it[idx] == bitCriteria }
-        }
+        .runningFold(list) { l, idx -> l.filter { it[idx] == bitCriteria(l, idx) } }
         .first { it.size == 1 }
         .first()
 
-    private fun String.flipBits() = map { if (it == '0') '1' else '0' }.joinToString("")
+    // could be implemented with xor FF - "101011" xor "111111" => "010100"
+    private fun String.flipBits() = map { it.flip() }.joinToString("")
 
-    private fun mostUsedBit(l: List<String>, idx: Int): Char {
-        val (zero, one) = l.map { it[idx] }.partition { it == '0' }
-        return if (zero.size > one.size) '0' else '1'
-    }
+    private fun Char.flip() = if (this == '0') '1' else '0'
 
-    private fun leastUsedBit(l: List<String>, idx: Int): Char {
-        val (zero, one) = l.map { it[idx] }.partition { it == '0' }
-        return if (one.size < zero.size) '1' else '0'
-    }
+    private fun mostUsedBit(l: List<String>, idx: Int): Char = if (2 * l.count { it[idx] == '1' } >= l.size) '1' else '0'
+
+    private fun leastUsedBit(l: List<String>, idx: Int) = mostUsedBit(l, idx).flip()
 }
