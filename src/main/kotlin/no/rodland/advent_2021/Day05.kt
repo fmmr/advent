6fd100922e153e85ae3c4fe378af1fc92fc7296f
Fredrik Rodland <git@rodland.no>
2021-12-05 21:12:20 +0100

    2021 - Day 05 - part 1 & 2 - extracted fun for solve - actually very similar for part 1 and part 2.

--- src/main/kotlin/no/rodland/advent_2021/Day05.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day05.kt
@@ -7,25 +7,21 @@ val regex = """(-?\d+),(-?\d+) -> (-?\d+),(-?\d+)""".toRegex()
 
 object Day05 {
     fun partOne(list: List<String>): Int {
-        return list
-            .map { Vent(it) }
-            .filterNot { it.isDiagonal() }
-            .flatMap { it.allPos() }
-            .groupingBy { it }
-            .eachCount()
-            .filterValues { it >= 2 }
-            .size
+        return solve(list) { it.isLine() }
     }
 
     fun partTwo(list: List<String>): Int {
-        return list
-            .map { Vent(it) }
-            .flatMap { it.allPos() }
-            .groupingBy { it }
-            .eachCount()
-            .filterValues { it >= 2 }
-            .size
+        return solve(list) { true }
     }
+
+    private fun solve(list: List<String>, filter: (Vent) -> Boolean) = list
+        .map { Vent(it) }
+        .filter { filter(it) }
+        .flatMap { it.allPos() }
+        .groupingBy { it }
+        .eachCount()
+        .filterValues { it >= 2 }
+        .size
 
     data class Vent(val from: Pos, val to: Pos) {
         private val signX = sign((to.x - from.x).toDouble()).toInt()
@@ -38,7 +34,7 @@ object Day05 {
                 Pos(mr.component3().toInt(), mr.component4().toInt()),
             )
 
-        fun isDiagonal() = from.x != to.x && from.y != to.y
+        fun isLine() = from.x == to.x || from.y == to.y
 
         fun allPos(): List<Pos> {
             return generateSequence(from) { last -> if (last != to) last + delta else null }.toList()
