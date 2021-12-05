60323127f4a90a3e2986b9138c5c160b8a79717e
Fredrik Rodland <git@rodland.no>
2021-12-05 20:33:29 +0100

    2021 - Day 05 - part 1 & 2

--- src/main/kotlin/no/rodland/advent_2021/Day05.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day05.kt
@@ -1,10 +1,10 @@
 package no.rodland.advent_2021
 
 import no.rodland.advent.Pos
+import kotlin.math.sign
 
 val regex = """(-?\d+),(-?\d+) -> (-?\d+),(-?\d+)""".toRegex()
 
-@Suppress("UNUSED_PARAMETER")
 object Day05 {
     fun partOne(list: List<String>): Int {
         return list
@@ -28,24 +28,23 @@ object Day05 {
     }
 
     data class Vent(val from: Pos, val to: Pos) {
+        private val signX = sign((to.x - from.x).toDouble()).toInt()
+        private val signY = sign((to.y - from.y).toDouble()).toInt()
 
-        constructor(str: String, mr: MatchResult.Destructured = regex.find(str)!!.destructured) : this(
-            Pos(mr.component1().toInt(), mr.component2().toInt()),
-            Pos(mr.component3().toInt(), mr.component4().toInt()),
-        )
+        constructor(str: String, mr: MatchResult.Destructured = regex.find(str)!!.destructured) :
+            this(
+                Pos(mr.component1().toInt(), mr.component2().toInt()),
+                Pos(mr.component3().toInt(), mr.component4().toInt()),
+            )
 
         fun isDiagonal() = from.x != to.x && from.y != to.y
 
         fun allPos(): List<Pos> {
-            val signX = Math.signum((to.x - from.x).toDouble()).toInt()
-            val signy = Math.signum((to.y - from.y).toDouble()).toInt()
-            var lastPos = from - Pos(signX, signy)
-            return generateSequence {
-                if (lastPos == to) {
-                    null
+            return generateSequence(from) { last ->
+                if (last != to) {
+                    last + Pos(signX, signY)
                 } else {
-                    lastPos += Pos(signX, signy)
-                    lastPos
+                    null
                 }
             }.toList()
         }
