70abddcf50ebcb867c53f330360102229cfab132
Fredrik Rodland <git@rodland.no>
2021-12-06 09:32:32 +0100

    2021 - Day 06 - Part 2

--- src/main/kotlin/no/rodland/advent_2021/Day06.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day06.kt
@@ -4,23 +4,28 @@ package no.rodland.advent_2021
 object Day06 {
 
 
-    fun partOne(list: List<Int>): Int {
-        val fishes = list.groupingBy { it }.eachCount()
-        val ar = IntArray(9) { fishes[it] ?: 0 }
-        return (1..80).fold(ar) { acc, i -> acc.cycle() }.sum()
+    fun partOne(list: List<Long>): Long {
+        return countFishes(list, 80)
     }
 
-    fun IntArray.cycle(): IntArray {
-        return IntArray(this.size) { idx ->
+    fun partTwo(list: List<Long>): Long {
+        return countFishes(list, 256)
+    }
+
+    private fun countFishes(list: List<Long>, iterations: Int): Long {
+        val fishes = list.groupingBy { it }.eachCount()
+        val ar = LongArray(9) { fishes[it.toLong()]?.toLong() ?: 0L }
+        val fold = (1..iterations).fold(ar) { acc, i -> acc.cycle() }
+        return fold.reduce { acc, l -> acc + l }
+    }
+
+    private fun LongArray.cycle(): LongArray {
+        return LongArray(this.size) { idx ->
             when (idx) {
                 6 -> this[0] + this[7]
                 8 -> this[0]
                 else -> this[idx + 1]
             }
         }
-    }
-
-    fun partTwo(list: List<Int>): Int {
-        return 2
     }
 }
--- src/test/kotlin/no/rodland/advent_2021/Day06Test.kt
+++ src/test/kotlin/no/rodland/advent_2021/Day06Test.kt
@@ -9,15 +9,15 @@ import readFile
 @Suppress("ClassName")
 @DisableSlow
 internal class Day06Test {
-    val data06 = "2021/input_06.txt".readFile()[0].split(",").map { it.toInt() }
-    val test06 = listOf(3, 4, 3, 1, 2)
+    val data06 = "2021/input_06.txt".readFile()[0].split(",").map { it.toLong() }
+    val test06 = listOf(3, 4, 3, 1, 2).map { it.toLong() }
 
     @Nested
     inner class Init {
         @Test
         fun `06,1,live,init`() {
             report {
-                Day06.partOne(data06) to 352872
+                Day06.partOne(data06) to 352872L
             }
         }
     }
@@ -27,21 +27,21 @@ internal class Day06Test {
         @Test
         fun `06,1,test`() {
             report {
-                Day06.partOne(test06) to 5934
+                Day06.partOne(test06) to 5934L
             }
         }
 
         @Test
         fun `06,1,live,1`() {
             report {
-                Day06.partOne(data06) to 352872
+                Day06.partOne(data06) to 352872L
             }
         }
 
         @Test
         fun `06,1,live,2`() {
             report {
-                Day06.partOne(data06) to 352872
+                Day06.partOne(data06) to 352872L
             }
         }
     }
@@ -51,21 +51,21 @@ internal class Day06Test {
         @Test
         fun `06,2,test`() {
             report {
-                Day06.partTwo(test06) to 2
+                Day06.partTwo(test06) to 26984457539L
             }
         }
 
         @Test
         fun `06,2,live,1`() {
             report {
-                Day06.partTwo(data06) to 2
+                Day06.partTwo(data06) to 1604361182149L
             }
         }
 
         @Test
         fun `06,2,live,2`() {
             report {
-                Day06.partTwo(data06) to 2
+                Day06.partTwo(data06) to 1604361182149L
             }
         }
     }
