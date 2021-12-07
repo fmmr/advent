9bc1613e2b07531d9ae839f738360f12909e3e7f
Fredrik Rodland <git@rodland.no>
2021-12-07 12:11:37 +0100

    2021 - Day 07 changed to only calculate for median (part1) and mean (part2) after having read up.

--- src/main/kotlin/no/rodland/advent_2021/Day07.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day07.kt
@@ -2,18 +2,24 @@ package no.rodland.advent_2021
 
 import kotlin.math.abs
 import kotlin.math.absoluteValue
+import kotlin.math.ceil
+import kotlin.math.floor
 
-@Suppress("UNUSED_PARAMETER")
 object Day07 {
     fun partOne(list: List<Int>): Int {
-        return solve(list, ::costPart1)
+        // loop through list: return solve(list, ::costPart1)
+        val pos = list.sorted()[list.size / 2]
+        return list.sumOf { crab -> costPart1(crab, pos) }
     }
 
 
     fun partTwo(list: List<Int>): Int {
-        return solve(list, ::costPart2)
+        // loop through list: return solve(list, ::costPart2)
+        val potentialPositions = list.average().let { setOf(floor(it).toInt(), ceil(it).toInt()) }
+        return potentialPositions.minOfOrNull { list.sumOf { crab -> costPart1(crab, it) } }!!
     }
 
+    @Suppress("unused")
     private fun solve(list: List<Int>, costCalc: (Int, Int) -> Int): Int {
         val max = list.maxOrNull()!!
         val min = list.minOrNull()!!
@@ -24,8 +30,6 @@ object Day07 {
 
     private fun costPart1(crab: Int, pos: Int) = abs(pos - crab)
 
-    fun costPart2(crab: Int, pos: Int) = triangular(pos - crab)
-
-    private fun triangular(diff: Int) = diff.absoluteValue.let { it * (it + 1) / 2 }
+    fun costPart2(crab: Int, pos: Int) = (pos - crab).absoluteValue.let { it * (it + 1) / 2 }
 
 }
