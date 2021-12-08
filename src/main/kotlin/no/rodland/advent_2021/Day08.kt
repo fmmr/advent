748e4dcf72d1f06b1d362e9ade5d0389741cb391
Fredrik Rodland <git@rodland.no>
2021-12-08 12:41:17 +0100

    2021 - Day 08 - part 1 & 2 - tidy code a bit

--- src/main/kotlin/no/rodland/advent_2021/Day08.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day08.kt
@@ -1,30 +1,29 @@
 package no.rodland.advent_2021
 
 object Day08 {
+
+    fun partOne(list: List<String>): Int {
+        val sizes = setOf(2, 4, 3, 7)
+        return parseInput(list)
+            .flatMap { it.output }
+            .count { it.length in sizes }
+    }
+
     fun partTwo(list: List<String>): Int {
         return parseInput(list).sumOf { it.outputAsint() }
     }
 
-    fun partOne(list: List<String>): Int {
-        return parseInput(list)
-            .map { it.output }
-            .flatMap { output -> output.filter { it.length in setOf(2, 4, 3, 7) } }
-            .count()
-    }
-
-    private fun parseInput(list: List<String>) = list.map { it.split(" | ") }.map { Entry(it.first().split(" ").map { str -> str.trim() }, it.last().split(" ").map { str -> str.trim() }) }
-
     class Entry(signalPattern: List<String>, val output: List<String>) {
         private val normalizedPatterns = signalPattern.map { str -> str.normalize() }
         private val decoding = run {
-            val four = normalizedPatterns.decoding(length = 4)
             val one = normalizedPatterns.decoding(length = 2)
             val seven = normalizedPatterns.decoding(length = 3)
+            val four = normalizedPatterns.decoding(length = 4)
             val eight = normalizedPatterns.decoding(length = 7)
-            val three = normalizedPatterns.decoding(length = 5, containsAll = one)
             val nine = normalizedPatterns.decoding(length = 6, containsAll = four)
             val zero = normalizedPatterns.decoding(length = 6, others = setOf(nine), containsAll = one)
             val six = normalizedPatterns.decoding(length = 6, others = setOf(nine, zero))
+            val three = normalizedPatterns.decoding(length = 5, containsAll = one)
             val five = normalizedPatterns.decoding(length = 5, others = setOf(three), containedIn = nine)
             val two = normalizedPatterns.decoding(length = 5, others = setOf(five, three))
             mapOf(zero to 0, one to 1, two to 2, three to 3, four to 4, five to 5, six to 6, seven to 7, eight to 8, nine to 9)
@@ -43,4 +42,6 @@ object Day08 {
             .filterNot { it in others }
             .single()
     }
+
+    private fun parseInput(list: List<String>) = list.map { it.split(" | ") }.map { Entry(it.first().split(" ").map { str -> str.trim() }, it.last().split(" ").map { str -> str.trim() }) }
 }
