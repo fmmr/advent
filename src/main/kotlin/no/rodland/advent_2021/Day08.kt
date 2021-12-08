9dfbd731b60b65005dd834b92377490c809dee3f
Fredrik Rodland <git@rodland.no>
2021-12-08 13:02:36 +0100

    2021 - Day 08 - part 1 & 2 - converted to codes to Set<String> to avoid having to normalize.

--- src/main/kotlin/no/rodland/advent_2021/Day08.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day08.kt
@@ -1,48 +1,46 @@
 package no.rodland.advent_2021
 
-object Day08 {
+private typealias Code = Set<Char>
 
+object Day08 {
     fun partOne(list: List<String>): Int {
         val sizes = setOf(2, 4, 3, 7)
         return list
             .toEntries()
             .flatMap { it.output }
-            .count { it.length in sizes }
+            .count { it.size in sizes }
     }
 
     fun partTwo(list: List<String>): Int {
         return list.toEntries().sumOf { it.outputAsint() }
     }
 
-    class Entry(signalPattern: List<String>, val output: List<String>) {
-        private val normalizedPatterns = signalPattern.map { str -> str.normalize() }
+    class Entry(input: List<Code>, val output: List<Code>) {
         private val decoding = run {
-            val one = normalizedPatterns.decoding(length = 2)
-            val seven = normalizedPatterns.decoding(length = 3)
-            val four = normalizedPatterns.decoding(length = 4)
-            val eight = normalizedPatterns.decoding(length = 7)
-            val nine = normalizedPatterns.decoding(length = 6, containsAll = four)
-            val zero = normalizedPatterns.decoding(length = 6, others = setOf(nine), containsAll = one)
-            val six = normalizedPatterns.decoding(length = 6, others = setOf(nine, zero))
-            val three = normalizedPatterns.decoding(length = 5, containsAll = one)
-            val five = normalizedPatterns.decoding(length = 5, others = setOf(three), containedIn = nine)
-            val two = normalizedPatterns.decoding(length = 5, others = setOf(five, three))
+            val one = input.decoding(length = 2)
+            val seven = input.decoding(length = 3)
+            val four = input.decoding(length = 4)
+            val eight = input.decoding(length = 7)
+            val nine = input.decoding(length = 6, containsAll = four)
+            val zero = input.decoding(length = 6, others = setOf(nine), containsAll = one)
+            val six = input.decoding(length = 6, others = setOf(nine, zero))
+            val three = input.decoding(length = 5, containsAll = one)
+            val five = input.decoding(length = 5, others = setOf(three), containedIn = nine)
+            val two = input.decoding(length = 5, others = setOf(five, three))
             mapOf(zero to 0, one to 1, two to 2, three to 3, four to 4, five to 5, six to 6, seven to 7, eight to 8, nine to 9)
         }
 
         fun outputAsint(): Int {
-            return output.map { it.normalize() }.map { decoding[it]!! }.joinToString("").toInt()
+            return output.map { decoding[it]!! }.joinToString("").toInt()
         }
 
-        private fun String.normalize() = toList().sorted().joinToString("")
-
-        private fun List<String>.decoding(length: Int, others: Set<String> = emptySet(), containsAll: String = "", containedIn: String = "abcdefg") = asSequence()
-            .filter { it.length == length }
-            .filter { potential -> containsAll.all { it in potential } }
-            .filter { potential -> potential.all { it in containedIn } }
+        private fun List<Code>.decoding(length: Int, others: Set<Code> = emptySet(), containsAll: Code = emptySet(), containedIn: Code = setOf('a', 'b', 'c', 'd', 'e', 'f', 'g')) = asSequence()
+            .filter { it.size == length }
+            .filter { it.containsAll(containsAll) }
+            .filter { containedIn.containsAll(it) }
             .filterNot { it in others }
             .single()
     }
 
-    private fun List<String>.toEntries() = map { it.split(" | ") }.map { Entry(it.first().split(" ").map { str -> str.trim() }, it.last().split(" ").map { str -> str.trim() }) }
+    private fun List<String>.toEntries() = map { it.split(" | ") }.map { Entry(it.first().split(" ").map { str -> str.trim().toSet() }, it.last().split(" ").map { str -> str.trim().toSet() }) }
 }
