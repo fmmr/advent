7fb2b68cc8444f0420a1a7b5361504aa7c82f408
Fredrik Rodland <git@rodland.no>
2021-12-09 11:08:41 +0100

    2021 - Day 09 - part 2

--- src/main/kotlin/no/rodland/advent_2021/Day09.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day09.kt
@@ -13,32 +13,31 @@ object Day09 {
     fun partTwo(list: List<String>): Int {
         val grid = list.toGrid()
         return lowPoints(grid, list)
-            .map {
-                it to getRegion(it.first, grid).distinct()
-            }
-//            .onEach { println("n $it") }
-            .map { it.second.size }
+            .map { getRegion(it.first, grid).size }
             .sorted()
             .takeLast(3)
             .reduce { acc, i -> acc * i }
     }
 
-    private fun getRegion(pos: Pos, grid: Array<IntArray>, included: List<Pos> = emptyList()): List<Pos> {
-        val value = grid[pos.y][pos.x]
-        val newNeighboors = pos.neighboorCellsNDLR()
-            .filter { it.isInGrid(grid) }
-            .filter { it !in included }
-            .filter { grid[it.y][it.x] != 9 }
-            .filter { grid[it.y][it.x] > value }
-        return (included + pos + newNeighboors).let { list ->
-            list + newNeighboors.flatMap { n -> getRegion(n, grid, list) }
+    private fun getRegion(pos: Pos, grid: Array<IntArray>): Set<Pos> {
+        val frontier = ArrayDeque<Pos>()
+        val visited = mutableSetOf<Pos>()
+        frontier.add(pos)
+        while (!frontier.isEmpty()) {
+            frontier.removeFirst().let { potentialPos ->
+                if (grid[potentialPos.y][potentialPos.x] < 9 && visited.add(potentialPos)) {
+                    grid.neighboors(potentialPos).forEach { frontier.add(it) }
+                }
+            }
         }
+        return visited
     }
+
 
     private fun lowPoints(grid: Array<IntArray>, list: List<String>): List<Pair<Pos, Int>> {
         val lowPoints = grid.flatMapIndexed { y, line ->
             line.mapIndexed { x, value ->
-                val neighboors = Pos(x, y).neighboorCellsNDLR().filter { it.isInGrid(grid) }
+                val neighboors = grid.neighboors(Pos(x, y))
                 if (neighboors.all { grid[it.y][it.x] > value }) {
                     Pos(x, y) to value
                 } else {
@@ -48,6 +47,8 @@ object Day09 {
         }.filterNotNull()
         return lowPoints
     }
+
+    private fun Array<IntArray>.neighboors(p: Pos) = p.neighboorCellsNDLR().filter { it.isInGrid(this) }
 
     private fun List<String>.toGrid() = Array(size) { row ->
         val line = this[row].toCharArray().map { it.asInt() }
