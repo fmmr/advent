0c81f5163b47c17a1326b50b9852e6631e1b641f
Fredrik Rodland <git@rodland.no>
2021-12-09 10:17:08 +0100

    2021 - Day 09 - part 2 - needs more thinking - lots of duplicates in recursive call.

--- src/main/kotlin/no/rodland/advent_2021/Day09.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day09.kt
@@ -10,20 +10,50 @@ object Day09 {
             val line = list[row].toCharArray().map { it.asInt() }
             IntArray(line.size) { line[it] }
         }
-        return grid.flatMapIndexed { y, line ->
+        return lowPoints(grid, list).map { it.second }.sumOf { it + 1 }
+    }
+
+    private fun lowPoints(grid: Array<IntArray>, list: List<String>): List<Pair<Pos, Int>> {
+        val lowPoints = grid.flatMapIndexed { y, line ->
             line.mapIndexed { x, value ->
                 val neighboors = Pos(x, y).neighboorCellsNDLR().filter { it.isInGrid(grid) }
                 if (neighboors.all { grid[it.y][it.x] > value }) {
-                    value
+                    Pos(x, y) to value
                 } else {
                     null
                 }
             }
-        }.filterNotNull().sumOf { it + 1 }
+        }.filterNotNull()
+        return lowPoints
     }
 
     fun partTwo(list: List<String>): Int {
-        return 2
+        val grid = Array(list.size) { row ->
+            val line = list[row].toCharArray().map { it.asInt() }
+            IntArray(line.size) { line[it] }
+        }
+        return lowPoints(grid, list)
+            .map {
+                it to getRegion(it.first, grid).distinct()
+            }
+//            .onEach { println("n $it") }
+            .map { it.second.size }
+            .sorted()
+            .takeLast(3)
+            .reduce { acc, i -> acc * i }
+    }
+
+    private fun getRegion(pos: Pos, grid: Array<IntArray>, included: List<Pos> = emptyList()): List<Pos> {
+        val value = grid[pos.y][pos.x]
+        val newNeighboors = pos.neighboorCellsNDLR()
+            .filter { it.isInGrid(grid) }
+            .filter { it !in included }
+            .filter { grid[it.y][it.x] != 9 }
+            .filter { grid[it.y][it.x] > value }
+        return (included + pos + newNeighboors).let { list ->
+            list + newNeighboors.flatMap { n -> getRegion(n, grid, list) }
+        }
     }
 }
+
 
--- src/test/kotlin/no/rodland/advent_2021/Day09Test.kt
+++ src/test/kotlin/no/rodland/advent_2021/Day09Test.kt
@@ -57,21 +57,21 @@ internal class Day09Test {
         @Test
         fun `09,2,test`() {
             report {
-                Day09.partTwo(test09) to 2
+                Day09.partTwo(test09) to 1134
             }
         }
 
         @Test
         fun `09,2,live,1`() {
             report {
-                Day09.partTwo(data09) to 2
+                Day09.partTwo(data09) to 949905
             }
         }
 
         @Test
         fun `09,2,live,2`() {
             report {
-                Day09.partTwo(data09) to 2
+                Day09.partTwo(data09) to 949905
             }
         }
     }
