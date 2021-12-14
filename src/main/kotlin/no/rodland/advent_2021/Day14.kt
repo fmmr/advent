df8379ede9c67aadeacc99268fe877c73ab6cd27
Fredrik Rodland <git@rodland.no>
2021-12-14 14:11:42 +0100

    2021 - Day 14 part 1 and 2

--- src/main/kotlin/no/rodland/advent_2021/Day14.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day14.kt
@@ -1,12 +1,66 @@
 package no.rodland.advent_2021
 
-@Suppress("UNUSED_PARAMETER")
 object Day14 {
-    fun partOne(list: List<String>): Int {
-        return 2
+    fun partOneAsPartTwo(list: List<String>): Long {
+        return partTwo(list, 10)
     }
 
-    fun partTwo(list: List<String>): Int {
-        return 2
+    fun partOne(list: List<String>): Long {
+        val (template, rules) = parse(list)
+        val result = (0 until 10)
+            .fold(template) { acc, n ->
+                acc
+                    .windowed(2)
+                    .flatMap { (first, second) ->
+                        val middle = rules[first to second]!!
+                        listOf(first, middle)
+                    } + acc.last()
+            }
+            .groupingBy { it }
+            .eachCount().values.map { it.toLong() }
+        return result.maxOrNull()!! - result.minOrNull()!!
+    }
+
+    fun partTwo(list: List<String>): Long {
+        return partTwo(list, 40)
+    }
+
+    fun partTwo(list: List<String>, iterations: Int): Long {
+        val (template, rules) = parse(list)
+        val initialCounts = template
+            .windowed(2)
+            .groupingBy { it.first() to it.last() }
+            .eachCount()
+            .mapValues { it.value.toLong() }
+
+        val doubleCounts = (0 until iterations)
+            .fold(initialCounts) { acc, i ->
+                acc.flatMap { (pair, count) ->
+                    val (first, second) = pair
+                    val middle = rules[pair]!!
+                    listOf((first to middle) to count, (middle to second) to count)
+                }
+                    .groupingBy { it.first }
+                    .fold(0L) { a, e -> a + e.second }
+            }
+            .toList()
+            .flatMap { (pair, count) -> listOf(pair.first to count, pair.second to count) }
+            .groupingBy { it.first }
+            .fold(0L) { a, e -> a + e.second }
+            .toMutableMap()
+
+        // first and last element are not double-counted
+        doubleCounts[template.first()] = doubleCounts[template.first()]!! + 1
+        doubleCounts[template.last()] = doubleCounts[template.last()]!! + 1
+
+        // doubleCounts is... well... double counted.
+        val counts = doubleCounts.map { it.value / 2 }
+
+        return counts.maxOrNull()!! - counts.minOrNull()!!
+    }
+
+    private fun parse(list: List<String>): Pair<List<Char>, Map<Pair<Char, Char>, Char>> {
+        return Pair(list[0].toList(), list.drop(2).associate { str -> (str[0] to str[1]) to str.substringAfter("-> ")[0] })
     }
 }
+
--- src/test/kotlin/no/rodland/advent_2021/Day14Test.kt
+++ src/test/kotlin/no/rodland/advent_2021/Day14Test.kt
@@ -3,7 +3,6 @@ package no.rodland.advent_2021
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.defaultTestSuite
 import no.rodland.advent.report
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import readFile
 import kotlin.time.ExperimentalTime
@@ -14,21 +13,37 @@ import kotlin.time.ExperimentalTime
 internal class Day14Test {
     private val liveData = "2021/input_14.txt".readFile()
     private val testData = listOf(
-        "1",
-        "2"
+        "NNCB",
+        "",
+        "CH -> B",
+        "HH -> N",
+        "CB -> H",
+        "NH -> C",
+        "HB -> C",
+        "HC -> B",
+        "HN -> C",
+        "NN -> C",
+        "BH -> H",
+        "NC -> B",
+        "NB -> B",
+        "BN -> B",
+        "BB -> N",
+        "BC -> B",
+        "CC -> N",
+        "CN -> C",
     )
     val test = defaultTestSuite(
         14, Day14::partOne, Day14::partTwo, liveData, testData,
-        testPart1 = 2,
-        livePart1 = 2,
-        testPart2 = 2,
-        livePart2 = 2
+        testPart1 = 1588,
+        livePart1 = 2657,
+        testPart2 = 2188189693529L,
+        livePart2 = 2911561572630L
     )
 
-    @BeforeAll
-    fun `0_init`() {
-        test.livePart1.run { function(data) }
-    }
+//    @BeforeAll
+//    fun `0_init`() {
+//        test.livePart1.run { function(data) }
+//    }
 
     @Test
     fun `1_test`() {
@@ -36,8 +51,18 @@ internal class Day14Test {
     }
 
     @Test
+    fun `1_test_as_part2`() {
+        report(test.testPart1.copy(function = Day14::partOneAsPartTwo))
+    }
+
+    @Test
     fun `1_live`() {
         report(test.livePart1)
+    }
+
+    @Test
+    fun `1_live_as_part2`() {
+        report(test.livePart1.copy(function = Day14::partOneAsPartTwo))
     }
 
     @Test
