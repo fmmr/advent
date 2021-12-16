a559c2fc0c88d27010729a27d6cd19a66d2479b3
Fredrik Rodland <git@rodland.no>
2021-12-16 22:17:30 +0100

    2021 - Day 16 - part 2 - wrong answer

--- src/main/kotlin/no/rodland/advent_2021/Day16.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day16.kt
@@ -1,29 +1,29 @@
 package no.rodland.advent_2021
 
+import no.rodland.advent_2021.Day16.Packet.Companion.getOp
 import prePad
 
 
 @Suppress("UNUSED_PARAMETER")
 object Day16 {
-    fun partOne(str: String): Int {
+    fun partOne(str: String): Long {
         val parse = str.toListOfBits().parse()
         return parse.versionSum()
     }
 
-    fun partTwo(str: String): Int {
-        return 2
+    fun partTwo(str: String): Long {
+        val parse = str.toListOfBits().parse()
+        return parse.value()
     }
 
     fun List<Char>.parse(): Packet {
         val (version, rest1) = takeDrop(3)
         val (type, rest2) = rest1.takeDrop(3)
 
-        val versionId = version.toInt()
-        val typeId = type.toInt()
-//        println("Parsing ${versionId}_$typeId")
-        val packet = when (typeId) {
-            4 -> {
-                getLiteralPacket(version.toInt(), typeId, rest2)
+        val versionId = version.toLong()
+        val packet = when (val typeId = type.toLong()) {
+            4L -> {
+                getLiteralPacket(versionId, typeId, rest2)
             }
             else -> {
                 val (length, rest3) = rest2.takeDrop(1)
@@ -36,23 +36,21 @@ object Day16 {
         return packet
     }
 
-    private fun parseNumSub(versionId: Int, typeId: Int, rest3: List<Char>): Operation {
+    private fun parseNumSub(versionId: Long, typeId: Long, rest3: List<Char>): Operation {
         val (numberSubPackets, rest4) = rest3.takeDrop(11)
-        val subPackets = (0 until numberSubPackets.toInt()).fold(emptyList<Packet>() to rest4) { acc, _ ->
+        val subPackets = (0 until numberSubPackets.toLong()).fold(emptyList<Packet>() to rest4) { acc, _ ->
             val (list, rest) = acc
             val packet = rest.parse()
             list + packet to packet.rest
         }
-        return Operation(versionId, typeId, subPackets.first, subPackets.second)
+        return getOp(versionId, typeId, subPackets.first, subPackets.second)
     }
 
-    private fun parseLengthSub(versionId: Int, typeId: Int, seq: List<Char>): Operation {
-
-
+    private fun parseLengthSub(versionId: Long, typeId: Long, seq: List<Char>): Operation {
         val (lengthSubPackets, rest1) = seq.takeDrop(15)
-        val (subPacketCharList, rest2) = rest1.takeDrop(lengthSubPackets.toInt())
+        val (subPacketCharList, rest2) = rest1.takeDrop(lengthSubPackets.toLong())
         val subPackets = parseLengthSub(subPacketCharList)
-        return Operation(versionId, typeId, subPackets, rest2)
+        return getOp(versionId, typeId, subPackets, rest2)
     }
 
     private fun parseLengthSub(subPacketCharList: List<Char>): List<Packet> {
@@ -64,9 +62,10 @@ object Day16 {
         }
     }
 
-    private fun getLiteralPacket(versionId: Int, typeId: Int, seq: List<Char>): Packet {
+    private fun getLiteralPacket(versionId: Long, typeId: Long, seq: List<Char>): Packet {
         val (lit, rest) = getLiteral(seq)
-        return Literal(versionId, typeId, lit.joinToString("").toLong(2), rest)
+        val value = lit.joinToString("").toLong(2)
+        return Literal(versionId, typeId, value, rest)
     }
 
     private fun getLiteral(seq: List<Char>): Pair<List<Char>, List<Char>> {
@@ -79,18 +78,18 @@ object Day16 {
         }
     }
 
-    private fun String.toListOfBits() = map { it.toString().toInt(16) }
+    private fun String.toListOfBits() = map { it.toString().toLong(16) }
         .map { it.toString(2) }
         .map { it.prePad(4, "0") }
         .flatMap { it.toList() }
 
 
-    private fun List<Char>.toInt(): Int = toBits().toInt(2)
+    private fun List<Char>.toLong(): Long = toBits().toLong(2)
 
     private fun List<Char>.toBits() = toList().joinToString("")
 
-    private fun List<Char>.takeDrop(i: Int): Pair<List<Char>, List<Char>> {
-        return take(i) to drop(i)
+    private fun List<Char>.takeDrop(i: Long): Pair<List<Char>, List<Char>> {
+        return take(i.toInt()) to drop(i.toInt())
     }
 
 
@@ -99,23 +98,72 @@ object Day16 {
         return str.toListOfBits().parse().allLiterals().map { it.value }
     }
 
+    // Only used in tests to check parsing
+    fun eval(str: String): Long {
+        return str.toListOfBits().parse().value()
+    }
 
-    sealed class Packet(val version: Int, val typeId: Int, val rest: List<Char>) {
-        abstract fun versionSum(): Int
+
+    sealed class Packet(val version: Long, val typeId: Long, val rest: List<Char>) {
+        abstract fun versionSum(): Long
+        abstract fun value(): Long
         abstract fun allLiterals(): List<Literal>
 
         override fun toString(): String {
             return "Packet(version=$version, typeId=$typeId, rest=$rest)"
         }
+
+        companion object {
+            fun getOp(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) = when (typeId) {
+                0L -> Sum(version, typeId, subPackets, rest)
+                1L -> Product(version, typeId, subPackets, rest)
+                2L -> Min(version, typeId, subPackets, rest)
+                3L -> Max(version, typeId, subPackets, rest)
+                5L -> GT(version, typeId, subPackets, rest)
+                6L -> LT(version, typeId, subPackets, rest)
+                7L -> EQ(version, typeId, subPackets, rest)
+                else -> error("No Op for $typeId")
+            }
+
+        }
     }
 
-    class Literal(version: Int, typeId: Int, val value: Long, rest: List<Char>) : Packet(version, typeId, rest) {
-        override fun versionSum(): Int = version
+    class Literal(version: Long, typeId: Long, val value: Long, rest: List<Char>) : Packet(version, typeId, rest) {
+        override fun versionSum(): Long = version
+        override fun value(): Long = value
         override fun allLiterals(): List<Literal> = listOf(this)
     }
 
-    class Operation(version: Int, typeId: Int, val subPackets: List<Packet>, rest: List<Char>) : Packet(version, typeId, rest) {
-        override fun versionSum(): Int = version + subPackets.sumOf { it.versionSum() }
+    sealed class Operation(version: Long, typeId: Long, val subPackets: List<Packet>, rest: List<Char>) : Packet(version, typeId, rest) {
+        override fun versionSum(): Long = version + subPackets.sumOf { it.versionSum() }
         override fun allLiterals(): List<Literal> = subPackets.flatMap { it.allLiterals() }
+    }
+
+    class Sum(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value(): Long = subPackets.sumOf { it.value() }
+    }
+
+    class Product(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value() = subPackets.fold(1L) { acc, packet -> acc * packet.value() }
+    }
+
+    class Min(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value() = subPackets.fold(Long.MAX_VALUE) { acc, packet -> if (acc < packet.value()) acc else packet.value() }
+    }
+
+    class Max(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value() = subPackets.fold(Long.MAX_VALUE) { acc, packet -> if (acc > packet.value()) acc else packet.value() }
+    }
+
+    class GT(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value() = if (subPackets[0].value() > subPackets[1].value()) 1.toLong() else 0.toLong()
+    }
+
+    class LT(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value() = if (subPackets[0].value() < subPackets[1].value()) 1.toLong() else 0.toLong()
+    }
+
+    class EQ(version: Long, typeId: Long, subPackets: List<Packet>, rest: List<Char>) : Operation(version, 0, subPackets, rest) {
+        override fun value() = if (subPackets[0].value() == subPackets[1].value()) 1.toLong() else 0.toLong()
     }
 }
--- src/test/kotlin/no/rodland/advent_2021/Day16Test.kt
+++ src/test/kotlin/no/rodland/advent_2021/Day16Test.kt
@@ -4,7 +4,6 @@ import no.rodland.advent.AOCTest
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.defaultTestSuite
 import no.rodland.advent.report
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import readFile
@@ -15,20 +14,79 @@ import kotlin.time.ExperimentalTime
 @DisableSlow
 internal class Day16Test {
     private val liveData = "2021/input_16.txt".readFile().first()
-    private val testData = "D2FE28"
+//    private val liveDataChris = "2021/input_16_chris.txt".readFile().first()
+private val testData = "D2FE28"
 
     val test = defaultTestSuite(
         16, Day16::partOne, Day16::partTwo, liveData, testData,
         testPart1 = 6,
         livePart1 = 847,
-        testPart2 = 2,
-        livePart2 = 2
+        testPart2 = 2021,
+        livePart2 = 329836099306L
     )
 
-    @BeforeAll
-    fun `0_init`() {
-        test.livePart1.run { function(data) }
+//    @BeforeAll
+//    fun `0_init`() {
+//        test.livePart1.run { function(data) }
+//    }
+
+    @Nested
+    inner class Eval {
+        @Test
+        fun `2_eval_D2FE28`() {
+            report(AOCTest("test_0", Day16::eval, "D2FE28", 2021L))
+        }
+
+        @Test
+        fun `2_eval_C200B40A82`() {
+            report(AOCTest("test_0", Day16::eval, "C200B40A82", 3L))
+        }
+
+        @Test
+        fun `2_eval_04005AC33890`() {
+            report(AOCTest("test_0", Day16::eval, "04005AC33890", 54L))
+        }
+
+        @Test
+        fun `2_eval_880086C3E88112`() {
+            report(AOCTest("test_0", Day16::eval, "880086C3E88112", 7L))
+        }
+
+        @Test
+        fun `2_eval_D8005AC2A8F0`() {
+            report(AOCTest("test_0", Day16::eval, "D8005AC2A8F0", 1L))
+        }
+
+//        @Test
+//        fun `2_eval_699ac25fcc200`() {
+//            report(AOCTest("test_0", Day16::eval, "699ac25fcc200", 1L))
+//        }
+
+        @Test
+        fun `2_eval_F600BC2D8F`() {
+            report(AOCTest("test_0", Day16::eval, "F600BC2D8F", 0L))
+        }
+
+        @Test
+        fun `2_eval_9C005AC2F8F0`() {
+            report(AOCTest("test_0", Day16::eval, "9C005AC2F8F0", 0L))
+        }
+
+        @Test
+        fun `2_eval_9C0141080250320F1802104A08`() {
+            report(AOCTest("test_0", Day16::eval, "9C0141080250320F1802104A08", 1L))
+        }
+
     }
+//    @Nested
+//    inner class Chris{
+//        @Test
+//        fun `1_chris`() {
+//            report(AOCTest("chris", Day16::partOne, liveData, listOf(2021L), 1))
+//        }
+//
+//    }
+
 
     @Nested
     inner class Parsing {
@@ -46,6 +104,10 @@ internal class Day16Test {
         fun `1_parse_EE00D40C823060`() {
             report(AOCTest("test_0", Day16::allLiterals, "EE00D40C823060", listOf(1L, 2L, 3L)))
         }
+//        @Test
+//        fun `1_parse_699AC25FCC200`() {
+//            report(AOCTest("test_0", Day16::allLiterals, "699AC25FCC200", listOf(1L, 2L, 3L)))
+//        }
 
         @Test
         fun `1_parse_8A004A801A8002F478`() {
