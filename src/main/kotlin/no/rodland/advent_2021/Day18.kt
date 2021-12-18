d0e614f1c78bf275ba25c5e5ba94568b1887c2bc
Fredrik Rodland <git@rodland.no>
2021-12-18 11:15:42 +0100

    2021 - Day 18 skel

--- src/main/kotlin/no/rodland/advent_2021/Day18.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day18.kt
@@ -1,5 +1,7 @@
 package no.rodland.advent_2021
 
+import kotlin.math.round
+
 @Suppress("UNUSED_PARAMETER")
 object Day18 {
     fun partOne(list: List<String>): Int {
@@ -9,4 +11,7 @@ object Day18 {
     fun partTwo(list: List<String>): Int {
         return 2
     }
+
+    fun Int.split(): Pair<Int, Int> = floorDiv(2) to round(toDouble() / 2.0).toInt()
+    fun Pair<Int, Int>.magnitude(): Int = first * 3 + second * 2
 }
--- src/test/kotlin/no/rodland/advent_2021/Day18Test.kt
+++ src/test/kotlin/no/rodland/advent_2021/Day18Test.kt
@@ -1,8 +1,11 @@
 package no.rodland.advent_2021
 
+import no.rodland.advent.AOCTest
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.defaultTestSuite
 import no.rodland.advent.report
+import no.rodland.advent_2021.Day18.magnitude
+import no.rodland.advent_2021.Day18.split
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import readFile
@@ -14,8 +17,13 @@ import kotlin.time.ExperimentalTime
 internal class Day18Test {
     private val liveData = "2021/input_18.txt".readFile()
     private val testData = listOf(
-        "1",
-        "2"
+        "[1,2]",
+        "[[1,2],3]",
+        "[9,[8,7]]",
+        "[[1,9],[8,5]]",
+        "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
+        "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
+        "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
     )
     val test = defaultTestSuite(
         18, Day18::partOne, Day18::partTwo, liveData, testData,
@@ -55,8 +63,24 @@ internal class Day18Test {
     //     report { 2 to 2 }
     // }
 
-    // @Test
-    // fun `18,1,demo_2`() {
-    //     report(AOCTest("18.demo", { it }, 2, 2))
-    // }
+    @Test
+    fun `18,1,split_10`() {
+        report(AOCTest("18.demo", { it.split() }, 10, 5 to 5))
+    }
+
+    @Test
+    fun `18,1,split_11`() {
+        report(AOCTest("18.demo", { it.split() }, 11, 5 to 6))
+    }
+
+    @Test
+    fun `18,1,magnitude_9,1`() {
+        report(AOCTest("18.demo", { it.magnitude() }, 9 to 1, 29))
+    }
+
+    @Test
+    fun `18,1,magnitude_1,9`() {
+        report(AOCTest("18.demo", { it.magnitude() }, 1 to 9, 21))
+    }
 }
+
