cb82d64cde51631b105163e68b03e5ba6e6a4e21
Fredrik Rodland <git@rodland.no>
2021-12-18 23:50:59 +0100

    2021 - Day 18, part 1 and (the much much simpler) part 2

--- src/main/kotlin/no/rodland/advent_2021/Day18.kt
+++ src/main/kotlin/no/rodland/advent_2021/Day18.kt
@@ -1,43 +1,117 @@
 package no.rodland.advent_2021
 
 import kotlin.math.round
+import kotlin.math.roundToInt
 
+// heavily inspired by https://gitlab.com/Nohus/adventofcode2021/-/blob/master/src/main/kotlin/day18_2/Solution.kt
 @Suppress("UNUSED_PARAMETER")
 object Day18 {
     fun partOne(list: List<String>): Int {
-        return 2
+        return list.map { it.parse() }.reduce { acc, fish -> acc + fish }.magnitude()
     }
 
     fun partTwo(list: List<String>): Int {
-        return 2
+        return list.flatMap { l1 ->
+            list.mapNotNull { l2 ->
+                if (l1 != l2) {
+                    (l1.parse() + l2.parse()).magnitude()
+                } else {
+                    null
+                }
+            }
+        }.maxOrNull()!!
     }
 
+
+    fun String.parse(): Fish {
+        if (!startsWith('[')) return FishNumber(toInt())
+        val inside = removeSurrounding("[", "]")
+        var nestLevel = 0
+        val index = inside.indexOfFirst {
+            if (it == '[') nestLevel++
+            else if (it == ']') nestLevel--
+            else if (it == ',' && nestLevel == 0) return@indexOfFirst true
+            false
+        }
+        return FishPair(inside.substring(0, index).parse(), inside.substring(index + 1).parse())
+    }
 
     sealed class Fish {
-        abstract fun right(): Fish
-        abstract fun left(): Fish
         abstract fun magnitude(): Int
         abstract fun split(): Fish
-        abstract fun explode(): Fish
+        abstract fun shouldSplit(): FishNumber?
+        abstract fun shouldExplode(level: Int = 0): FishPair?
+        abstract fun flatten(): List<Fish>
+        abstract fun replace(replacement: Pair<Fish, Fish>): Fish
+
+        operator fun plus(fish: Fish): Fish {
+            val fishPair = FishPair(this, fish)
+            return fishPair.reduce()
+        }
+
+        fun reduce(): Fish {
+            var reduced = this
+            while (true) {
+                val exploding = reduced.shouldExplode()
+                if (exploding != null) {
+                    val flattened = reduced.flatten()
+                    val index = flattened.indexOf(exploding)
+                    reduced = reduced.replace(exploding to FishNumber(0))
+
+                    val numberToLeft = flattened.take(index).filterIsInstance<FishNumber>().lastOrNull()
+                    if (numberToLeft != null) {
+                        reduced = reduced.replace(numberToLeft to FishNumber(numberToLeft.value + (exploding.first as FishNumber).value))
+                    }
+
+                    val numberToRight = flattened.drop(index + 1).filterIsInstance<FishNumber>().drop(2).firstOrNull()
+                    if (numberToRight != null) {
+                        reduced = reduced.replace(numberToRight to FishNumber(numberToRight.value + (exploding.second as FishNumber).value))
+                    }
+                    continue
+                }
+                val splitting = reduced.shouldSplit()
+                if (splitting != null) {
+                    val pair = FishPair(FishNumber(splitting.value / 2), FishNumber(((splitting.value + 0.5) / 2.0).roundToInt()))
+                    reduced = reduced.replace(splitting to pair)
+                    continue
+                }
+                break
+            }
+            return reduced
+        }
+
     }
 
-    data class FishNumber(val value: Int) : Fish() {
-        override fun right(): Fish = this
-        override fun left(): Fish = this
+    class FishNumber(val value: Int) : Fish() {
         override fun magnitude(): Int = value
-        override fun explode(): Fish = TODO("implement explode")
-
         override fun split(): Fish = when {
             value > 9 -> FishPair(FishNumber(value.floorDiv(2)), FishNumber(round(value.toDouble() / 2.0).toInt()))
             else -> this
         }
+
+        override fun shouldSplit(): FishNumber? = if (value > 9) this else null
+        override fun shouldExplode(level: Int): FishPair? = null
+        override fun flatten(): List<Fish> = emptyList()
+        override fun replace(replacement: Pair<Fish, Fish>) = if (this === replacement.first) replacement.second else this
+        override fun toString(): String {
+            return "$value"
+        }
     }
 
-    data class FishPair(val first: Fish, val second: Fish) : Fish() {
-        override fun right(): Fish = second.right()
-        override fun left(): Fish = first.left()
+    class FishPair(val first: Fish, val second: Fish) : Fish() {
         override fun magnitude(): Int = first.magnitude() * 3 + second.magnitude() * 2
         override fun split(): Fish = FishPair(first.split(), second.split())
-        override fun explode(): Fish = TODO("implement explode")
+        override fun shouldSplit(): FishNumber? = first.shouldSplit() ?: second.shouldSplit()
+        override fun shouldExplode(level: Int) = when (level) {
+            4 -> this
+            else -> first.shouldExplode(level + 1) ?: second.shouldExplode(level + 1)
+        }
+
+        override fun flatten(): List<Fish> = listOf(listOf(this.first), this.first.flatten(), listOf(this.second), this.second.flatten()).flatten()
+        override fun replace(replacement: Pair<Fish, Fish>) = if (this === replacement.first) replacement.second else FishPair(first.replace(replacement), second.replace(replacement))
+        override fun toString(): String {
+            return "[$first,$second]"
+        }
+
     }
 }
--- src/test/kotlin/no/rodland/advent_2021/Day18Test.kt
+++ src/test/kotlin/no/rodland/advent_2021/Day18Test.kt
@@ -2,11 +2,12 @@ package no.rodland.advent_2021
 
 import no.rodland.advent.AOCTest
 import no.rodland.advent.DisableSlow
+import no.rodland.advent.Slow
 import no.rodland.advent.defaultTestSuite
 import no.rodland.advent.report
 import no.rodland.advent_2021.Day18.FishNumber
 import no.rodland.advent_2021.Day18.FishPair
-import org.junit.jupiter.api.BeforeAll
+import no.rodland.advent_2021.Day18.parse
 import org.junit.jupiter.api.Test
 import readFile
 import kotlin.time.ExperimentalTime
@@ -16,27 +17,58 @@ import kotlin.time.ExperimentalTime
 @DisableSlow
 internal class Day18Test {
     private val liveData = "2021/input_18.txt".readFile()
+    private val testData2 = listOf(
+        "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+        "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+        "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+        "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+        "[7,[5,[[3,8],[1,4]]]]",
+        "[[2,[2,2]],[8,[8,1]]]",
+        "[2,9]",
+        "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+        "[[[5,[7,4]],7],1]",
+        "[[[[4,2],2],6],[8,7]]",
+    )
     private val testData = listOf(
-        "[1,2]",
-        "[[1,2],3]",
-        "[9,[8,7]]",
-        "[[1,9],[8,5]]",
-        "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
-        "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
-        "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
+        "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+        "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+        "[[[[5,4],[7,7]],8],[[8,3],8]]",
+        "[[9,3],[[9,9],[6,[4,9]]]]",
+        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]",
+    )
+    private val testDataSimple = listOf(
+        "[1,1]",
+        "[2,2]",
+        "[3,3]",
+        "[4,4]",
+        "[5,5]",
+        "[6,6]",
+    )
+    private val testDataVerySimple = listOf(
+        "[1,1]",
+        "[2,2]",
+        "[3,3]",
+        "[4,4]",
     )
     val test = defaultTestSuite(
         18, Day18::partOne, Day18::partTwo, liveData, testData,
-        testPart1 = 2,
-        livePart1 = 2,
-        testPart2 = 2,
-        livePart2 = 2
+        testPart1 = 4140,
+        livePart1 = 4072,
+        testPart2 = 3993,
+        livePart2 = 4483,
+        numTestPart1 = 1,
+        numTestPart2 = 1
     )
 
-    @BeforeAll
-    fun `0_init`() {
-        test.livePart1.run { function(data) }
-    }
+//    @BeforeAll
+//    fun `0_init`() {
+//        test.livePart1.run { function(data) }
+//    }
 
     @Test
     fun `1_test`() {
@@ -44,6 +76,7 @@ internal class Day18Test {
     }
 
     @Test
+    @Slow(100)
     fun `1_live`() {
         report(test.livePart1)
     }
@@ -54,18 +87,104 @@ internal class Day18Test {
     }
 
     @Test
+    @Slow(700)
     fun `2_live`() {
         report(test.livePart2)
     }
 
     @Test
+    fun `18,1,test_very_simple`() {
+        report(test.testPart1.copy(data = testDataVerySimple, expected = 445))
+    }
+
+    @Test
+    fun `18,1,test_simple`() {
+        report(test.testPart1.copy(data = testDataSimple, expected = 1137))
+    }
+
+    @Test
+    fun `18,1,add_1`() {
+        report(AOCTest("18.demo", { ("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]".parse() + "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]".parse()).toString() }, 1, "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"))
+    }
+
+    @Test
+    fun `18,1,add_3`() {
+        report(AOCTest("18.demo", { ("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]".parse() + "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]".parse()).toString() }, 1, "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]"))
+    }
+
+    @Test
+    fun `18,1,add_4`() {
+        report(AOCTest("18.demo", { ("[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]".parse() + "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]".parse()).toString() }, 1, "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]"))
+    }
+
+    @Test
+    fun `18,1,testdata_reduce`() {
+        report(AOCTest("18.demo", { testData2.map { it.parse() }.reduce { acc, fish -> acc + fish }.toString() }, 1, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"))
+    }
+
+    @Test
+    fun `18,1,add_2`() {
+        report(AOCTest("18.demo", { ("[1,1]".parse() + "[2,2]".parse()).toString() }, 1, "[[1,1],[2,2]]"))
+    }
+
+    @Test
+    fun `18,1,reduce_1`() {
+        report(AOCTest("18.demo", { "[[[[[9,8],1],2],3],4]".parse().reduce().toString() }, 1, "[[[[0,9],2],3],4]"))
+    }
+
+    @Test
+    fun `18,1,magnitude_1`() {
+        report(AOCTest("18.demo", { "[[[[5,0],[7,4]],[5,5]],[6,6]]".parse().reduce().magnitude() }, 1, 1137))
+    }
+
+    @Test
+    fun `18,1,magnitude_2`() {
+        report(AOCTest("18.demo", { "[[1,2],[[3,4],5]]".parse().reduce().magnitude() }, 1, 143))
+    }
+
+    @Test
+    fun `18,1,magnitude_3`() {
+        report(AOCTest("18.demo", { "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]".parse().reduce().magnitude() }, 1, 1384))
+    }
+
+    @Test
+    fun `18,1,magnitude_4`() {
+        report(AOCTest("18.demo", { "[[[[1,1],[2,2]],[3,3]],[4,4]]".parse().reduce().magnitude() }, 1, 445))
+    }
+
+    @Test
+    fun `18,1,magnitude_5`() {
+        report(AOCTest("18.demo", { "[[[[3,0],[5,3]],[4,4]],[5,5]]".parse().reduce().magnitude() }, 1, 791))
+    }
+
+    @Test
+    fun `18,1,magnitude_7`() {
+        report(AOCTest("18.demo", { "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]".parse().reduce().magnitude() }, 1, 3488))
+    }
+
+    @Test
+    fun `18,1,magnitude_8`() {
+        report(AOCTest("18.demo", { "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]".parse().magnitude() }, 1, 3488))
+    }
+
+    @Test
+    fun `18,1,magnitude_9`() {
+        report(AOCTest("18.demo", { "[[[[1,1],[2,2]],[3,3]],[4,4]]".parse().magnitude() }, 1, 445))
+    }
+
+    @Test
+    fun `18,1,reduce_2`() {
+        report(AOCTest("18.demo", { "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".parse().reduce().toString() }, 1, "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"))
+    }
+
+    @Test
     fun `18,1,split_10`() {
-        report(AOCTest("18.demo", { it.split() }, FishNumber(10), FishPair(FishNumber(5), FishNumber(5))))
+        report(AOCTest("18.demo", { it.split().toString() }, FishNumber(10), "[5,5]"))
     }
 
     @Test
     fun `18,1,split_11`() {
-        report(AOCTest("18.demo", { it.split() }, FishNumber(11), FishPair(FishNumber(5), FishNumber(6))))
+        report(AOCTest("18.demo", { it.split().toString() }, FishNumber(11), "[5,6]"))
     }
 
     @Test
