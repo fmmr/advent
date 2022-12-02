7eeaa6c59fefbb2213ef6c7d74730cb3ffd1a39f
Fredrik Rødland <fmr@finn.no>
2022-12-02 11:58:40 +0100

    2022 - Day 02 - renamed Game => Hand

--- src/main/kotlin/no/rodland/advent_2022/Day02.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day02.kt
@@ -1,8 +1,8 @@
 package no.rodland.advent_2022
 
-import no.rodland.advent_2022.Day02.Game.PAPER
-import no.rodland.advent_2022.Day02.Game.ROCK
-import no.rodland.advent_2022.Day02.Game.SCISSOR
+import no.rodland.advent_2022.Day02.Hand.PAPER
+import no.rodland.advent_2022.Day02.Hand.ROCK
+import no.rodland.advent_2022.Day02.Hand.SCISSOR
 
 // template generated: 28/11/2022
 // Fredrik Rødland 2022
@@ -16,7 +16,7 @@ object Day02 {
         return list.playAllGames { c, elf -> c.fromOutcome(elf) }
     }
 
-    private fun List<String>.playAllGames(resolveXYZ: (Char, Game) -> Game) =
+    private fun List<String>.playAllGames(resolveXYZ: (Char, Hand) -> Hand) =
         map { str ->
             val line = str.split(" ")
             val elfGame = line.first().first().toHand()
@@ -25,37 +25,37 @@ object Day02 {
 
         }.sumOf { round(it.first, it.second) + it.second.points }
 
-    private fun Char.fromOutcome(elf: Game) = when (this) {
+    private fun Char.fromOutcome(elf: Hand) = when (this) {
         'X' -> elf.win()
         'Y' -> elf
         'Z' -> elf.lose()
         else -> error("unknown char ${this}")
     }
 
-    private fun Char.toHand(): Game = when (this) {
+    private fun Char.toHand(): Hand = when (this) {
         'A', 'X' -> ROCK
         'B', 'Y' -> PAPER
         'C', 'Z' -> SCISSOR
         else -> error("unknown char ${this@Day02}")
     }
 
-    private fun round(elf: Game, player: Game): Int = when (elf) {
+    private fun round(elf: Hand, player: Hand): Int = when (elf) {
         player -> 3
         player.lose() -> 0
         player.win() -> 6
         else -> error("should not happen $elf $player")
     }
 
-    enum class Game(val points: Int) {
+    enum class Hand(val points: Int) {
         ROCK(1), PAPER(2), SCISSOR(3);
 
-        fun win(): Game = when (this) {
+        fun win(): Hand = when (this) {
             ROCK -> SCISSOR
             PAPER -> ROCK
             SCISSOR -> PAPER
         }
 
-        fun lose(): Game = when (this) {
+        fun lose(): Hand = when (this) {
             ROCK -> PAPER
             PAPER -> SCISSOR
             SCISSOR -> ROCK
