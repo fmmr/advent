7de0e9916d7b03435d20706d061b92d01e80e3fa
Fredrik Rødland <fmr@finn.no>
2022-12-03 11:12:56 +0100

    2022 - Day 03 - part 1

--- src/main/kotlin/no/rodland/advent_2022/Day03.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day03.kt
@@ -4,11 +4,30 @@ package no.rodland.advent_2022
 // Fredrik Rødland 2022
 
 object Day03 {
-    fun partOne(list: List<String>): Long {
-        return 2
+    fun partOne(list: List<String>): Int {
+        return list.map { Bag(it) }.map { it.common() }.sumOf { it.priority() }
     }
 
     fun partTwo(list: List<String>): Long {
         return 2
     }
+
+    fun Char.priority():Int {
+        return when {
+            this >= 'a' -> code - 96
+            else -> code - 38
+        }
+    }
+
+
+    class Bag(val input: String) {
+        val compartment1 = component1()
+        val compartment2 = component2()
+
+        operator fun component1(): List<Char> = input.toCharArray().toList().subList(0, input.length / 2)
+        operator fun component2(): List<Char> = input.toCharArray().toList().subList(input.length / 2, input.length)
+
+        fun common() = compartment1.intersect(compartment2).distinct().first()
+    }
 }
+
--- src/test/kotlin/no/rodland/advent_2022/Day03Test.kt
+++ src/test/kotlin/no/rodland/advent_2022/Day03Test.kt
@@ -2,6 +2,7 @@ package no.rodland.advent_2022
 
 import no.rodland.advent.DisableSlow
 import no.rodland.advent.report
+import no.rodland.advent_2022.Day03.priority
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import readFile
@@ -10,10 +11,17 @@ import readFile
 @DisableSlow
 internal class Day03Test {
     val data03 = "2022/input_03.txt".readFile()
-    val test03 = listOf("1", "2")
+    val test03 = listOf(
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+    )
 
-    val resultTestOne = 2L
-    val resultOne = 2L
+    val resultTestOne = 157
+    val resultOne = 7795
 
     val resultTestTwo = 2L
     val resultTwo = 2L
@@ -49,6 +57,45 @@ internal class Day03Test {
             report {
                 Day03.partOne(data03) to resultOne
             }
+        }
+    }
+
+    @Nested
+    inner class Misc {
+        @Test
+        fun `03,1,bag`() {
+            report {
+                val (sub1, sub2) = Day03.Bag("ttHH")
+                (sub1 to sub2) to (listOf('t', 't') to listOf('H', 'H'))
+            }
+        }
+
+        @Test
+        fun `03,1,common`() {
+            report {
+                val bag = Day03.Bag("ttyTswHHyMMl")
+                (bag.common()) to ('y')
+            }
+        }
+
+        @Test
+        fun `03,1,priority,1`() {
+            report { ('a'.priority()) to (1) }
+        }
+
+        @Test
+        fun `03,1,priority,2`() {
+            report { ('z'.priority()) to (26) }
+        }
+
+        @Test
+        fun `03,1,priority,3`() {
+            report { ('A'.priority()) to (27) }
+        }
+
+        @Test
+        fun `03,1,priority,4`() {
+            report { ('Z'.priority()) to (52) }
         }
     }
 
