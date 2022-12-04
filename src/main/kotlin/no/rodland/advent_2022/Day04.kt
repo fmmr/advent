184983defe152bf82654da6ec85284b4d8bf050b
Fredrik Rødland <fmr@finn.no>
2022-12-04 11:00:37 +0100

    2022 - Day 04 - part 1 & 2 - renamed functions

--- src/main/kotlin/no/rodland/advent_2022/Day04.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day04.kt
@@ -5,19 +5,18 @@ package no.rodland.advent_2022
 
 object Day04 {
     fun partOne(list: List<String>): Int {
-        return list.map { CleaningPair(it) }.count { it.isContained }
-
+        return list.map { CleaningPair(it) }.count { it.fullyContained }
     }
 
     fun partTwo(list: List<String>): Int {
-        return list.map { CleaningPair(it) }.count { it.anyContained }
+        return list.map { CleaningPair(it) }.count { it.overlap }
     }
 
     data class CleaningPair(val input: String) {
         val first = input.split(",").first().let { firstStr -> firstStr.split("-").let { it.first().toInt()..it.last().toInt() } }
         val second = input.split(",").last().let { secondStr -> secondStr.split("-").let { it.first().toInt()..it.last().toInt() } }
 
-        val isContained = first.all { it in second } || second.all { it in first }
-        val anyContained = first.any { it in second } || second.any { it in first }
+        val fullyContained = first.all { it in second } || second.all { it in first }
+        val overlap = first.any { it in second } || second.any { it in first }
     }
 }
