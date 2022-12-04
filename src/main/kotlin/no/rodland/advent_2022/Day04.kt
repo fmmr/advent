f909da8b2154f360da1e68ce6c4a180b9190e2ea
Fredrik Rødland <fmr@finn.no>
2022-12-04 13:13:51 +0100

    2022 - Day 04 - extracted diff between 1 & 2 as function.

--- src/main/kotlin/no/rodland/advent_2022/Day04.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day04.kt
@@ -5,20 +5,19 @@ package no.rodland.advent_2022
 
 object Day04 {
     fun partOne(list: List<String>): Int {
-        return list.map { CleaningPair(it) }.count { it.second fullyContained it.first || it.first fullyContained it.second }
+        return count(list, ::fullyContained)
     }
 
     fun partTwo(list: List<String>): Int {
-        return list.map { CleaningPair(it) }.count { it.first partlyContained it.second || it.second partlyContained it.first }
+        return count(list, ::partlyContained)
     }
 
-    data class CleaningPair(val input: String) {
-        val first = input.split(",").first().toRange()
-        val second = input.split(",").last().toRange()
-    }
+    private fun count(list: List<String>, func: (IntRange, IntRange) -> Boolean) = list
+        .map { input -> input.split(",").let { it.first().toRange() to it.last().toRange() } }
+        .count { func(it.first, it.second) || func(it.second, it.first) }
 
     private fun String.toRange() = split("-").let { it.first().toInt()..it.last().toInt() }
 
-    private infix fun IntRange.fullyContained(other: IntRange) = first in other && last in other
-    private infix fun IntRange.partlyContained(other: IntRange) = first in other || last in other
+    private fun fullyContained(first: IntRange, second: IntRange) = first.first in second && first.last in second
+    private fun partlyContained(first: IntRange, second: IntRange) = first.first in second || first.last in second
 }
