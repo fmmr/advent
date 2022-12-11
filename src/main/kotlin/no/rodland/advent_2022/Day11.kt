96edc549cff60cf05e36603e6326b64d383d03d1
Fredrik Rødland <fmr@finn.no>
2022-12-11 11:51:37 +0100

    2022 - Day 11 - part 1

--- src/main/kotlin/no/rodland/advent_2022/Day11.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day11.kt
@@ -5,8 +5,53 @@ package no.rodland.advent_2022
 
 object Day11 {
     @Suppress("UNUSED_PARAMETER")
-    fun partOne(list: List<String>): Long {
-        return 2
+    fun partOne(list: List<String>): Int {
+        val monkeys = list.joinToString("\n").split("\n\n").map { Monkey.fromList(it) }.associateBy { it.id }
+        repeat(20) { monkeys.round() }
+        return monkeys.values.map { it.count }.sortedDescending().take(2).reduce(Int::times)
+    }
+
+    private fun Map<Int, Monkey>.round() {
+        forEach { (_, monkey) ->
+            monkey.removeAll { item ->
+                val newValue = monkey.inspectAndCount(item) / 3
+                val newMonkey = monkey.next(newValue)
+                get(newMonkey)!!.add(newValue)
+            }
+        }
+    }
+
+    class Monkey(val id: Int, private val items: ArrayDeque<Int>, val inspect: (Int) -> Int, val next: (Int) -> Int) : MutableList<Int> by items {
+
+        var count = 0
+
+        fun inspectAndCount(old: Int): Int {
+            count++
+            return inspect(old)
+        }
+
+        companion object {
+            fun fromList(input: String): Monkey {
+                val id = input.substringAfter(" ").substringBefore(":").toInt()
+                val items = input.substringAfter("Starting items: ").substringBefore("\n").split(", ").map { it.toInt() }
+                val operationText = input.substringAfter("Operation: new = old ").substringBefore("\n")
+                val op = operationText.substringBefore(" ")
+                val opValue = operationText.substringAfter(" ")
+                val test = input.substringAfter("Test: divisible by ").substringBefore("\n").toInt()
+                val trueMonkey = input.substringAfter("If true: throw to monkey ").substringBefore("\n").toInt()
+                val falseMonkey = input.substringAfter("If false: throw to monkey ").substringBefore("\n").toInt()
+                val operation = { newValue: Int ->
+                    val v = if (opValue == "old") newValue else opValue.toInt()
+                    when (op) {
+                        "*" -> newValue * v
+                        "+" -> newValue + v
+                        else -> error("only + and * allowed")
+                    }
+                }
+                val action = { newValue: Int -> if (newValue % test == 0) trueMonkey else falseMonkey }
+                return Monkey(id, ArrayDeque(items), operation, action)
+            }
+        }
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -14,3 +59,5 @@ object Day11 {
         return 2
     }
 }
+
+
--- src/test/kotlin/no/rodland/advent_2022/Day11Test.kt
+++ src/test/kotlin/no/rodland/advent_2022/Day11Test.kt
@@ -11,11 +11,37 @@ import readFile
 internal class Day11Test {
     val data11 = "2022/input_11.txt".readFile()
     val test11 = listOf(
-        "1"
+        "Monkey 0:",
+        "  Starting items: 79, 98",
+        "  Operation: new = old * 19",
+        "  Test: divisible by 23",
+        "    If true: throw to monkey 2",
+        "    If false: throw to monkey 3",
+        "",
+        "Monkey 1:",
+        "  Starting items: 54, 65, 75, 74",
+        "  Operation: new = old + 6",
+        "  Test: divisible by 19",
+        "    If true: throw to monkey 2",
+        "    If false: throw to monkey 0",
+        "",
+        "Monkey 2:",
+        "  Starting items: 79, 60, 97",
+        "  Operation: new = old * old",
+        "  Test: divisible by 13",
+        "    If true: throw to monkey 1",
+        "    If false: throw to monkey 3",
+        "",
+        "Monkey 3:",
+        "  Starting items: 74",
+        "  Operation: new = old + 3",
+        "  Test: divisible by 17",
+        "    If true: throw to monkey 0",
+        "    If false: throw to monkey 1",
     )
 
-    val resultTestOne = 2L
-    val resultOne = 2L
+    val resultTestOne = 10605
+    val resultOne = 58056
 
     val resultTestTwo = 2L
     val resultTwo = 2L
