d9dc71ee69f6245a9c179390fb0656fb05b3c470
Fredrik Rødland <fmr@finn.no>
2022-12-12 11:00:37 +0100

    2022 - Day 12 - end condition is now a set.  Also added walkRestriction as function.

--- src/main/kotlin/no/rodland/advent_2022/Day12.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day12.kt
@@ -12,29 +12,29 @@ object Day12 {
         val all = grid.all()
         val start = all.first { it.second == 'S'.code }.first
         val end = all.first { it.second == 'E'.code }.first
-        val shortestPath = bfs(grid, start, end)
+        val shortestPath = bfs(grid, start, setOf(end)) { p1, p2 -> grid[p1] < (grid[p2] + 2) }
         return shortestPath.size - 1
     }
 
     fun partTwo(list: List<String>): Int {
         val grid = IntGrid.fromChar(list)
         val all = grid.all()
-        val start = all.filter { it.second == 'a'.code || it.second == 'S'.code }.map { it.first }
-        val end = all.first { it.second == 'E'.code }.first
-        val sorted = start.map { bfs(grid, it, end).size }.filterNot { it == 0 }.sorted()
-        return sorted.min() - 1
+        val start = all.first { it.second == 'E'.code }.first
+        val end = all.filter { it.second == 'a'.code || it.second == 'S'.code }.map { it.first }.toSet()
+        val shortestPath = bfs(grid, start, end) { p1, p2 -> grid[p1] > (grid[p2] - 2) }
+        return shortestPath.size - 1
     }
 
-    private fun bfs(grid: IntGrid, start: Pos, end: Pos): List<Pos?> {
+    private fun bfs(grid: IntGrid, start: Pos, end: Set<Pos>, walkRestriction: (Pos, Pos) -> Boolean): List<Pos?> {
         val queue = ArrayDeque(listOf(start))
         val parents = mutableMapOf<Pos, Pos?>().apply { put(start, null) }
         while (queue.isNotEmpty()) {
             val current = queue.removeFirst()
-            if (current == end) {
-                return generateSequence(end) { parents[it] }.toList()
+            if (current in end) {
+                return generateSequence(current) { parents[it] }.toList()
             }
             grid.neighbours(current)
-                .filter { grid[it] < (grid[current] + 2) }
+                .filter { walkRestriction(it, current) }
                 .filterNot { it in parents.keys }
                 .forEach { neighbor ->
                     queue.add(neighbor)
