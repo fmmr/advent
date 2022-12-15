5f948351def9c9e0ae15f1f7ebadcf30e3eb631f
Fredrik Rødland <fmr@finn.no>
2022-12-15 16:47:01 +0100

    2022 - Day 15 - part 1

--- src/main/kotlin/no/rodland/advent_2022/Day15.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day15.kt
@@ -1,16 +1,78 @@
 package no.rodland.advent_2022
 
+import no.rodland.advent.Pos
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
 // template generated: 28/11/2022
 // Fredrik Rødland 2022
 
 object Day15 {
+    fun partOne(input: List<String>, row: Int = 10): Int {
+        val areas = input.parse()
+        val beacons = areas.map { it.beacon }
+        val ranges = areas
+            .map { it.rangeOnRow(row) }
+            .filterNot { it.isEmpty() }
+        val mergedRanges = ranges.merge().merge().merge().merge().merge().merge().merge().merge()
+        return mergedRanges.sumOf { it.last - it.first + 1 } - beacons.filter { it.y == row }.distinct().size
+    }
     @Suppress("UNUSED_PARAMETER")
-    fun partOne(list: List<String>): Long {
+    fun partTwo(input: List<String>): Int {
         return 2
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    fun partTwo(list: List<String>): Long {
-        return 2
+    private fun Collection<IntRange>.merge(): MutableSet<IntRange> {
+        return fold(mutableSetOf()) { acc, intRange ->
+            val overlap = acc.firstOrNull { it.overlap(intRange) }
+            if (overlap == null) {
+                acc += intRange
+            } else {
+                acc -= overlap
+                acc += overlap.combine(intRange)
+            }
+            acc
+        }
+    }
+
+    private fun IntRange.combine(other: IntRange): IntRange {
+        return min(first, other.first)..max(last, other.last)
+    }
+
+    private fun IntRange.overlap(other: IntRange): Boolean = first <= other.last && last >= other.first
+
+    data class Area(val sensor: Pos, val beacon: Pos) {
+        private val manhattan = sensor.distanceTo(beacon)
+
+        private val colRange = (sensor.x - manhattan)..(sensor.x + manhattan)
+        private val rowRange = ((sensor.y - manhattan)..(sensor.y + manhattan))
+
+        private fun affectsRow(row: Int): Boolean = row in rowRange
+        fun rangeOnRow(row: Int): IntRange {
+            return when {
+                !affectsRow(row) -> IntRange.EMPTY
+                sensor.y == row -> colRange
+                else -> {
+                    val diff = manhattan - abs(sensor.y - row)
+                    (sensor.x - diff)..(sensor.x + diff)
+                }
+            }
+        }
+    }
+
+
+
+
+    // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    fun List<String>.parse(): List<Area> {
+        return map {
+            val sensorX = it.substringAfter("x=").substringBefore(", y=").toInt()
+            val beaconX = it.substringAfterLast("x=").substringBefore(", y=").toInt()
+            val sensorY = it.substringAfter("y=").substringBefore(": cl").toInt()
+            val beaconY = it.substringAfterLast("y=").toInt()
+            Area(Pos(sensorX, sensorY), Pos(beaconX, beaconY))
+        }
     }
 }
+
--- src/test/kotlin/no/rodland/advent_2022/Day15Test.kt
+++ src/test/kotlin/no/rodland/advent_2022/Day15Test.kt
@@ -11,21 +11,34 @@ import readFile
 internal class Day15Test {
     val data15 = "2022/input_15.txt".readFile()
     val test15 = listOf(
-        "1"
+        "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+        "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+        "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+        "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+        "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+        "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+        "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+        "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+        "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+        "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+        "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+        "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+        "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+        "Sensor at x=20, y=1: closest beacon is at x=15, y=3",
     )
 
-    val resultTestOne = 2L
-    val resultOne = 2L
+    val resultTestOne = 26
+    val resultOne = 4748135
 
-    val resultTestTwo = 2L
-    val resultTwo = 2L
+    val resultTestTwo = 2 //56000011
+    val resultTwo = 2
 
     @Nested
     inner class Init {
         @Test
         fun `15,1,live,init`() {
             report {
-                Day15.partOne(data15) to resultOne
+                Day15.partOne(data15, 2000000) to resultOne
             }
         }
 
@@ -42,14 +55,14 @@ internal class Day15Test {
         @Test
         fun `15,1,test`() {
             report {
-                Day15.partOne(test15) to resultTestOne
+                Day15.partOne(test15, 10) to resultTestOne
             }
         }
 
         @Test
         fun `15,1,live,1`() {
             report {
-                Day15.partOne(data15) to resultOne
+                Day15.partOne(data15, 2000000) to resultOne
             }
         }
     }
