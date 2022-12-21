48b16f9e4db901293fea727d29c6c6d7b74aa9c3
Fredrik Rødland <fmr@finn.no>
2022-12-21 13:24:31 +0100

    2022 - Day 21 - part 2 (not calculated, printing equation)

--- src/main/kotlin/no/rodland/advent_2022/Day21.kt
+++ src/main/kotlin/no/rodland/advent_2022/Day21.kt
@@ -4,73 +4,77 @@ package no.rodland.advent_2022
 // Fredrik Rødland 2022
 
 object Day21 {
-    @Suppress("UNUSED_PARAMETER")
     fun partOne(list: List<String>): Long {
         val monkeys = list.map { it.toMonkey() }.associateBy { it.id }
-        val numMonkeys = monkeys.values.map { toNumMonkey(monkeys, it) }.associateBy { it.id }
+        val numMonkeys = monkeys.values.map { toNumMonkey(monkeys, it) }
+        return numMonkeys.first { it.id == "root" }.value
+    }
 
-        return numMonkeys["root"]!!.value
+    @Suppress("UNUSED_VARIABLE")
+    fun partTwo(list: List<String>): Long {
+        val monkeys = list.map { it.toMonkey() }.associateBy { it.id }
+        val root = monkeys["root"] as OperationMonkey
+        val humanLeft = isHumanInTree(monkeys, root.left)
+        val (num, eq) = if (humanLeft) {
+            toNumMonkey(monkeys, toNumMonkey(monkeys, monkeys[root.right]!!)).value to toEq(monkeys, monkeys[root.left]!!)
+        } else {
+            toNumMonkey(monkeys, toNumMonkey(monkeys, monkeys[root.left]!!)).value to toEq(monkeys, monkeys[root.right]!!)
+        }
+        // https://www.mathpapa.com/equation-solver/
+        // solves the output from this printlln:
+        // println("$num=$eq")
+        return 2L
+    }
+
+    private fun toEq(map: Map<String, Monkey>, monkey: Monkey): String {
+        return when {
+            monkey.id == "humn" -> "x"
+            monkey is NumMonkey -> monkey.value.toString()
+            monkey is OperationMonkey -> "(" + toEq(map, map[monkey.left]!!) + monkey.operation.op + toEq(map, map[monkey.right]!!) + ")"
+            else -> error("nope")
+        }
+    }
+
+    private fun isHumanInTree(monkeys: Map<String, Monkey>, start: String): Boolean {
+        val monkey = monkeys[start]!!
+        return when {
+            monkey.id == "humn" -> true
+            monkey is NumMonkey -> false
+            monkey is OperationMonkey -> isHumanInTree(monkeys, monkey.left) || isHumanInTree(monkeys, monkey.right)
+            else -> error("nope")
+        }
     }
 
     private fun toNumMonkey(map: Map<String, Monkey>, monkey: Monkey): NumMonkey {
         return when (monkey) {
             is NumMonkey -> monkey
-            is OperationMonkey -> NumMonkey(monkey.id, monkey.operation.calc(monkey.left.value, monkey.right.value))
-            is StringOperationMonkey -> toNumMonkey(map, OperationMonkey(monkey.id, toNumMonkey(map, map[monkey.left]!!), toNumMonkey(map, map[monkey.right]!!), monkey.operation))
+            is OperationMonkey -> NumMonkey(monkey.id, monkey.operation.calc(toNumMonkey(map, map[monkey.left]!!).value, toNumMonkey(map, map[monkey.right]!!).value))
         }
     }
 
+    enum class Operation(val op: String, val operation: (Long, Long) -> Long) {
+        MINUS("-", { n1, n2 -> n1 - n2 }), PLUS("+", { n1, n2 -> n1 + n2 }), MULT("*", { n1, n2 -> n1 * n2 }), DIV("/", { n1, n2 -> n1 / n2 });
 
-    @Suppress("UNUSED_PARAMETER")
-    fun partTwo(list: List<String>): Long {
-        return 2
-    }
-
-    enum class Operation {
-        MINUS, PLUS, MULT, DIV;
-
-        fun calc(left: Long, right: Long): Long {
-            return when (this) {
-                MINUS -> left - right
-                PLUS -> left + right
-                MULT -> left * right
-                DIV -> left / right
-            }
-        }
-
+        fun calc(left: Long, right: Long) = operation(left, right)
     }
 
     sealed class Monkey(open val id: String)
-
-    data class OperationMonkey(override val id: String, var left: NumMonkey, var right: NumMonkey, val operation: Operation) : Monkey(id)
-    data class StringOperationMonkey(override val id: String, var left: String, var right: String, val operation: Operation) : Monkey(id)
+    data class OperationMonkey(override val id: String, var left: String, var right: String, val operation: Operation) : Monkey(id)
     data class NumMonkey(override val id: String, val value: Long) : Monkey(id)
 
-    //lgvd: ljgn * ptdq
-//drzm: hmdt - zczc
-//hmdt: 32
     private fun String.toMonkey(): Monkey {
         val (id, rest) = split(": ")
         return if (rest.toLongOrNull() != null) {
             NumMonkey(id, rest.toLongOrNull()!!)
         } else {
             val (l, o, r) = rest.split(" ")
-            val op = o.toOp()
-            StringOperationMonkey(id, l, r, op)
+            OperationMonkey(id, l, r, o.toOp())
         }
     }
 
-    fun String.toOp(): Operation {
-        return when (trim()) {
-            "+" -> Operation.PLUS
-            "-" -> Operation.MINUS
-            "*" -> Operation.MULT
-            "/" -> Operation.DIV
-            else -> error("unsupported: ${this}")
-        }
-    }
-
+    private fun String.toOp(): Operation = Operation.values().first { it.op == this }
 }
+
 
 
 
