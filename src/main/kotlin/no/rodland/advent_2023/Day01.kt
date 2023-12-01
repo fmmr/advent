2c39715666e5912f6e1de3923819ef05df06dd7c
Fredrik Rødland <fmr@finn.no>
2023-12-01 11:50:28 +0100

    2023 - Day01 - using indexOfAny

--- src/main/kotlin/no/rodland/advent_2023/Day01.kt
+++ src/main/kotlin/no/rodland/advent_2023/Day01.kt
@@ -9,37 +9,40 @@ import kotlin.Int.Companion.MAX_VALUE
 class Day01(val input: List<String>) {
 
     fun partOne(): Long {
-        return input.sumOf { line -> findNumbers(line) { digit -> listOf(digit) } }.toLong()
+        return input.sumOf { line -> calibrationValue(line) { digit -> listOf(digit.toString()) } }.toLong()
     }
 
     fun partTwo(): Long {
-        return input.sumOf { line -> findNumbers(line) { digit -> listOf(digit, DIGIT_MAPPING[digit]!!) } }.toLong()
+        return input.sumOf { line -> calibrationValue(line) { digit -> listOf(digit.toString(), DIGIT_MAPPING[digit]!!) } }.toLong()
     }
 
-    fun findNumbers(line: String, listFunc: (String) -> List<String>): Int {
-        val map = DIGIT_MAPPING
-            .keys
-            .map { d -> DigitPos(d, listFunc(d).minOf { line.indexOfMaxDefault(it) }, listFunc(d).maxOf { line.lastIndexOf(it) }) }
+    fun calibrationValue(line: String, listFunc: (Int) -> List<String>): Int {
+        val map = DIGIT_MAPPING.keys.map { d -> DigitPos(d, line.indexOfMaxDefault(listFunc(d)), line.lastIndexOfAny(listFunc(d))) }
         val first = map.minBy { it.firstOcc }.digit
         val last = map.maxBy { it.lastOcc }.digit
-        return "$first$last".toInt()
+        return first * 10 + last
     }
 
-    data class DigitPos(val digit: String, val firstOcc: Int, val lastOcc: Int)
+    data class DigitPos(val digit: Int, val firstOcc: Int, val lastOcc: Int)
 
-    fun String.indexOfMaxDefault(sub: String) = if (contains(sub)) indexOf(sub) else MAX_VALUE
+    fun String.indexOfMaxDefault(sub: List<String>) = indexOfAny(sub).let {
+        when (it) {
+            -1 -> MAX_VALUE
+            else -> it
+        }
+    }
 
     companion object {
         private val DIGIT_MAPPING = mapOf(
-            "1" to "one",
-            "2" to "two",
-            "3" to "three",
-            "4" to "four",
-            "5" to "five",
-            "6" to "six",
-            "7" to "seven",
-            "8" to "eight",
-            "9" to "nine",
+            1 to "one",
+            2 to "two",
+            3 to "three",
+            4 to "four",
+            5 to "five",
+            6 to "six",
+            7 to "seven",
+            8 to "eight",
+            9 to "nine",
         )
     }
 }
