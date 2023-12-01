bd705be6480d9ca49b41253937cbccb0c37606b0
Fredrik Rødland <fmr@finn.no>
2023-12-01 01:05:26 +0100

    2023 - create some template files

---
+++ src/main/kotlin/no/rodland/advent_2023/Day01.kt
@@ -0,0 +1,22 @@
+package no.rodland.advent_2023
+
+// template generated: 01/12/2023
+// Fredrik Rødland 2023
+
+@Suppress("unused")
+class Day01(val input: List<String>) {
+
+    val parsed = input.parse()
+
+    fun partOne(): Long {
+        return 2
+    }
+
+    fun partTwo(): Long {
+        return 2
+    }
+
+    fun List<String>.parse(): List<String> {
+        return this
+    }
+}
---
+++ src/test/kotlin/no/rodland/advent_2023/Day01Test.kt
@@ -0,0 +1,57 @@
+package no.rodland.advent_2023
+
+import no.rodland.advent.DisableSlow
+import no.rodland.advent.report
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import readFile
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day01Test {
+    private val data25 = "2022/input_25.txt".readFile()
+    private val test25 = listOf("1", "2")
+
+    private val day25 = Day01(data25)
+    private val day25Test = Day01(test25)
+
+    private val resultTestOne = 2L
+    private val resultTestTwo = 2L
+    private val resultOne = 2L
+    private val resultTwo = 2L
+
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `25,1,test`() {
+            report {
+                day25Test.partOne() to resultTestOne
+            }
+        }
+
+        @Test
+        fun `25,1,live,1`() {
+            report {
+                day25.partOne() to resultOne
+            }
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `25,2,test`() {
+            report {
+                day25Test.partTwo() to resultTestTwo
+            }
+        }
+
+        @Test
+        fun `25,2,live,1`() {
+            report {
+                day25.partTwo() to resultTwo
+            }
+        }
+    }
+}
