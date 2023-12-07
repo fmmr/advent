47fc9540876ba30810cdfb2959c0177e8eab0da5
Fredrik Rødland <fmr@finn.no>
2023-12-07 10:53:46 +0100

    2023 - Day07 - refactored - differences are now in part1 & part2 - not throughout the code.

--- src/main/kotlin/no/rodland/advent_2023/Day07.kt
+++ src/main/kotlin/no/rodland/advent_2023/Day07.kt
@@ -5,85 +5,88 @@ import no.rodland.advent.Day
 // template generated: 07/12/2023
 // Fredrik Rødland 2023
 
-class Day07(val input: List<String>) : Day<Long, Long, List<Day07.Hand>> {
+class Day07(val input: List<String>) : Day<Long, Long, List<Pair<List<Char>, Int>>> {
 
     private val parsed = input.parse()
 
     override fun partOne(): Long {
-        val selectors = listOf(Hand::getBestValue1) + (0..4).map { idx -> { it: Hand -> it.cardValues1[idx] } }
-        return score(selectors)
+        val findGroups: (List<Char>) -> List<Int> = { hand ->
+            hand.groupBy { it }.map { it.value.size }.sortedByDescending { it }
+        }
+        val cardValue: (Char) -> Int = { LABELS[it]!! }
+        return score(findGroups, cardValue)
     }
 
     override fun partTwo(): Long {
-        val selectors = listOf(Hand::getBestValue2) + (0..4).map { idx -> { it: Hand -> it.cardValues2[idx] } }
-        return score(selectors)
+        val findGroups: (List<Char>) -> List<Int> = { hand ->
+            val groupsWithoutJoker = hand.filterNot { it == 'J' }.groupBy { it }.map { it.value.size }.sortedByDescending { it }
+            val numJokers = hand.count { it == 'J' }
+            val groups = if (numJokers == 5) {
+                listOf(5)
+            } else {
+                listOf(groupsWithoutJoker[0] + numJokers) + groupsWithoutJoker.drop(1)
+            }
+            groups
+        }
+        val cardValue: (Char) -> Int = { if (it == 'J') 1 else LABELS[it]!! }
+        return score(findGroups, cardValue)
     }
 
-    private fun score(selectors: List<(Hand) -> Int>): Long {
+    private fun score(findGroups: (List<Char>) -> List<Int>, cardValue: (Char) -> Int): Long {
+        val selectors = listOf(Hand::getBestValue) + (0..4).map { idx -> { it: Hand -> it.cards[idx] } }
         return parsed
-            .sortedWith(compareBy(*selectors.toTypedArray()))
+            .map { (cards, bid) ->
+                val groups = findGroups(cards)
+                Hand(cards.map { cardValue(it) }, groups, bid)
+            }
+            .sortedWith(compareBy(*selectors.toTypedArray<(Hand) -> Int>()))
             .mapIndexed { index: Int, hand: Hand -> (index + 1) * hand.bid.toLong() }
             .sum()
     }
 
-    data class Hand(val cards: List<Char>, val bid: Int) {
-        val groups = cards.groupBy { it }.map { it.value.size }.sortedByDescending { it }
-        val groupsWithoutJoker = cards.filterNot { it == 'J' }.groupBy { it }.map { it.value.size }.sortedByDescending { it }
-        val numJokers = cards.count { it == 'J' }
-        val groupsPart2 = if (numJokers == 5) {
-            listOf(5)
-        } else {
-            listOf(groupsWithoutJoker[0] + numJokers) + groupsWithoutJoker.drop(1)
-        }
-        val cardValues1 = cards.map { LABELS[it]!! }
-        val cardValues2 = cards.map { if (it == 'J') 1 else LABELS[it]!! }
 
-        fun getBestValue2(): Int = getBestValue(groupsPart2)
-
-        fun getBestValue1(): Int = getBestValue(groups)
-
-        private fun getBestValue(checkGroups: List<Int>): Int = when {
-            checkGroups.size == 1 -> 6
-            checkGroups[0] == 4 -> 5
-            checkGroups[0] == 3 && checkGroups[1] == 2 -> 4
-            checkGroups[0] == 3 && checkGroups[1] == 1 -> 3
-            checkGroups[0] == 2 && checkGroups[1] == 2 -> 2
-            checkGroups[0] == 2 && checkGroups[1] == 1 -> 1
+    data class Hand(val cards: List<Int>, val groups: List<Int>, val bid: Int) {
+        fun getBestValue(): Int = when {
+            groups.size == 1 -> 6
+            groups[0] == 4 -> 5
+            groups[0] == 3 && groups[1] == 2 -> 4
+            groups[0] == 3 && groups[1] == 1 -> 3
+            groups[0] == 2 && groups[1] == 2 -> 2
+            groups[0] == 2 && groups[1] == 1 -> 1
             else -> 0
         }
 
-        private fun getBestString(groupValue: Int): String = when (groupValue) {
-            6 -> "five"
-            5 -> "four"
-            4 -> "house"
-            3 -> "three"
-            2 -> "twopair"
-            1 -> "pair"
-            else -> "high"
-        }
-
-        override fun toString(): String {
-            return cards.joinToString("") + " (1: " + getBestString(getBestValue(groups)) + ", 2: " + getBestString(getBestValue(groupsPart2)) + ") " + groups
-        }
-
-        companion object {
-            val LABELS = mapOf(
-                'A' to 14,
-                'K' to 13,
-                'Q' to 12,
-                'J' to 11,
-                'T' to 10,
-            ) + (2..9).map { it.digitToChar() to it }
-        }
+//        private fun getBestString(groupValue: Int): String = when (groupValue) {
+//            6 -> "five"
+//            5 -> "four"
+//            4 -> "house"
+//            3 -> "three"
+//            2 -> "twopair"
+//            1 -> "pair"
+//            else -> "high"
+//        }
+//
+//        override fun toString(): String {
+//            return cards.joinToString("") + " (" + getBestString(getBestValue()) + ") " + groups
+//        }
     }
 
-    override fun List<String>.parse(): List<Hand> {
+    override fun List<String>.parse(): List<Pair<List<Char>, Int>> {
         return map { line ->
             val (cards, bid) = line.split(' ')
-            Hand(cards.toList(), bid.toInt())
+            cards.toList() to bid.toInt()
         }
     }
 
+    companion object {
+        val LABELS = mapOf(
+            'A' to 14,
+            'K' to 13,
+            'Q' to 12,
+            'J' to 11,
+            'T' to 10,
+        ) + (2..9).map { it.digitToChar() to it }
+    }
 
     override val day = "07".toInt()
 }
