cc9f6efcc21dcd36fe606a00b574dab143b24f2b
Fredrik Rødland <fmr@finn.no>
2023-12-09 17:43:08 +0100

    2023 - Day09 - denser it is

--- src/main/kotlin/no/rodland/advent_2023/Day09.kt
+++ src/main/kotlin/no/rodland/advent_2023/Day09.kt
@@ -5,38 +5,23 @@ import no.rodland.advent.Day
 // template generated: 09/12/2023
 // Fredrik Rødland 2023
 
-
-// can probably be calculated directly (think something along the lines of Pascal's Triangle)
-// instead of looping through everything (twice for part 2)
 class Day09(val input: List<String>) : Day<Int, Int, List<List<Int>>> {
 
     private val parsed = input.parse()
 
     override fun partOne(): Int {
-        return parsed.sumOf { list -> allDiffs(list).sumOf { it.last() } }
+        return parsed.sumOf { it.next() }
     }
 
     override fun partTwo(): Int {
-        return parsed.sumOf { list ->
-            val append2 = mutableListOf<List<Int>>()
-            allDiffs(list).reversed().map { next ->
-                append2.add(diffFirst(append2.lastOrNull(), next))
-            }
-            append2.last().first()
-        }
+        return parsed.sumOf { it.previous() }
     }
 
-    private fun allDiffs(list: List<Int>): List<List<Int>> {
-        val append = mutableListOf(list)
-        while (append.last().any { it != 0 }) {
-            append.add(append.last().diffs())
-        }
-        return append
-    }
+    private fun List<Int>.next(): Int = if (all { it == 0 }) 0
+    else last() + diffs().next()
 
-    private fun diffFirst(last: List<Int>?, next: List<Int>): List<Int> {
-        return listOf(next.first() - (last?.first() ?: 0)) + next
-    }
+    private fun List<Int>.previous(): Int = if (all { it == 0 }) 0
+    else first() - diffs().previous()
 
     private fun List<Int>.diffs(): List<Int> = zipWithNext { a, b -> b - a }
 
