996b246c6a1770208898d261aeb5e74c59a60f75
Fredrik Rødland <fmr@finn.no>
2023-12-12 22:07:14 +0100

    2023 - Day12 - part 1 - tweaked

--- src/main/kotlin/no/rodland/advent_2023/Day12.kt
+++ src/main/kotlin/no/rodland/advent_2023/Day12.kt
@@ -25,18 +25,16 @@ class Day12(val input: List<String>) : Day<Int, Long, List<Pair<String, List<Int
 
 
     fun expand(input: String): List<String> {
-        fun String.replaceBoolean(): List<Char> = map { char ->
-            if (char == '0') '.'
-            else '#'
-        }
-
         val count = input.count { it == '?' }
         val numChars = 1 shl count  // 2^count
-        return (0..<numChars).map { boolean ->
-            boolean.toString(2).padStart(count, '0').replaceBoolean()
-        }.map { mask ->
-            mask.fold(input) { acc: String, c: Char -> acc.replaceFirst("?", c.toString()) }
-        }
+        fun Int.toMapString(): String = toString(2)
+            .padStart(count, '0')
+            .replace("0", ".")
+            .replace("1", "#")
+
+        return (0..<numChars)
+            .map { boolean -> boolean.toMapString() }
+            .map { mask -> mask.fold(input) { acc: String, c: Char -> acc.replaceFirst("?", c.toString()) } }
     }
 
     override fun List<String>.parse(): List<Pair<String, List<Int>>> {
