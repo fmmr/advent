d3ea6d41cbb69c1c4ccb974a24fc4c63c8af8b2c
Fredrik Rødland <fmr@finn.no>
2023-12-13 08:11:35 +0100

    2023 - Day 13 - Init

---
+++ src/test/kotlin/no/rodland/advent_2023/Day13Test.kt
@@ -0,0 +1,82 @@
+package no.rodland.advent_2023
+
+import no.rodland.advent.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import readFile
+
+//
+//  run: download_aoc_input.sh to download input
+//
+
+@Suppress("ClassName")
+@DisableSlow
+internal class Day13Test {
+    private val data13 = "2023/input_13.txt".readFile()
+    private val test13 = "2023/input_13_test.txt".readFile()
+
+    private val resultTestOne = 2L
+    private val resultTestTwo = 2L
+    private val resultOne = 2L
+    private val resultTwo = 2L
+
+    val test = defaultTestSuiteParseOnInit(
+        Day13(data13),
+        Day13(test13),
+        resultTestOne,
+        resultOne,
+        resultTestTwo,
+        resultTwo,
+        { Day13(data13) },
+        { Day13(test13) },
+    )
+
+    @Nested
+    inner class Init {
+        @Test
+        fun `13,-,example,1`() {
+            report(AOCTest({ "123".toInt() }, Unit, 123, 5, "13".toInt(), Part.TWO, false, "example"))
+        }
+
+        @Test
+        fun `13,-,example,2`() {
+            report(test.initTest.copy())
+        }
+
+        @Test
+        fun `13,-,test,init`() {
+            report(test.initTest)
+        }
+
+        @Test
+        fun `13,-,live,init`() {
+            report(test.initLive)
+        }
+    }
+
+    @Nested
+    inner class `Part 1` {
+        @Test
+        fun `13,1,test`() {
+            report(test.testPart1)
+        }
+
+        @Test
+        fun `13,1,live,1`() {
+            report(test.livePart1)
+        }
+    }
+
+    @Nested
+    inner class `Part 2` {
+        @Test
+        fun `13,2,test`() {
+            report(test.testPart2)
+        }
+
+        @Test
+        fun `13,2,live,1`() {
+            report(test.livePart2)
+        }
+    }
+}
