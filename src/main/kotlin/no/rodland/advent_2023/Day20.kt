c788771c0d2ddb80f2b252ed70883662479f83e2
Fredrik Rødland <fmr@finn.no>
2023-12-20 12:11:22 +0100

    2023 - Day 20 - input

--- src/main/kotlin/no/rodland/advent_2023/Day20.kt
+++ src/main/kotlin/no/rodland/advent_2023/Day20.kt
@@ -5,7 +5,7 @@ import no.rodland.advent.Day
 // template generated: 20/12/2023
 // Fredrik Rødland 2023
 
-class Day20(val input: List<String>) : Day<Long, Long, List<String>> {
+class Day20(val input: List<String>) : Day<Long, Long, Map<String, Day20.Mod>> {
 
     private val parsed = input.parse()
 
@@ -17,9 +17,23 @@ class Day20(val input: List<String>) : Day<Long, Long, List<String>> {
         return 2
     }
 
-    override fun List<String>.parse(): List<String> {
-        return map { line ->
-            line
+    sealed class Mod(val id: String, val destinations: List<String>)
+    class FlipFlop(id: String, destinations: List<String>) : Mod(id, destinations)
+    class Broadcast(destinations: List<String>) : Mod("broadcaster", destinations)
+    class Conjunction(id: String, destinations: List<String>) : Mod(id, destinations)
+
+    override fun List<String>.parse(): Map<String, Mod> {
+        return associate { line ->
+            println(line)
+            val (id, list) = line.split(" -> ")
+            val destinations = list.split(";", " ").filterNot { it.isEmpty() }
+            val mod = when {
+                id.startsWith("&") -> Conjunction(id.drop(1), destinations)
+                id.startsWith("%") -> FlipFlop(id.drop(1), destinations)
+                id == "broadcaster" -> Broadcast(destinations)
+                else -> error("unknown mod: $id")
+            }
+            mod.id to mod
         }
     }
 
