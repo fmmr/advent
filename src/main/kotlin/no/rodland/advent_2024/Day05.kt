c15de4a2cf573ca8c0f6b73a886aab3ed6bdd3d2
Fredrik Rødland <fmr@finn.no>
2024-12-05 13:03:36 +0100

    2024 Day 05 Part2 - rules should be a set

--- src/main/kotlin/no/rodland/advent_2024/Day05.kt
+++ src/main/kotlin/no/rodland/advent_2024/Day05.kt
@@ -5,7 +5,7 @@ import no.rodland.advent.Day
 // template generated: 05/12/2024
 // Fredrik Rødland 2024
 
-class Day05(val input: List<String>) : Day<Int, Int, Pair<List<Pair<Int, Int>>, List<List<Int>>>> {
+class Day05(val input: List<String>) : Day<Int, Int, Pair<Set<Pair<Int, Int>>, List<List<Int>>>> {
 
     private val parsed = input.parse()
     private val rules = parsed.first
@@ -59,7 +59,7 @@ class Day05(val input: List<String>) : Day<Int, Int, Pair<List<Pair<Int, Int>>, 
         }
     }
 
-    override fun List<String>.parse(): Pair<List<Pair<Int, Int>>, List<List<Int>>> {
+    override fun List<String>.parse(): Pair<Set<Pair<Int, Int>>, List<List<Int>>> {
         val (rulesStr, pagesStr) = this.joinToString("\n").split("\n\n").map { it.split("\n") }
 
         val pages = pagesStr.map { line ->
@@ -67,7 +67,7 @@ class Day05(val input: List<String>) : Day<Int, Int, Pair<List<Pair<Int, Int>>, 
         }
         val rules = rulesStr.map { line ->
             line.split("|").map { it.toInt() }.let { it[0] to it[1] }
-        }
+        }.toSet()
         return rules to pages
     }
 
