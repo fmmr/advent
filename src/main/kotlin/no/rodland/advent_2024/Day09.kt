80aa55921ae8c3401ab00031b2d59f10a59f74de
Fredrik Rødland <fmr@finn.no>
2024-12-09 07:50:49 +0100

    2024 - Day 09 - parse

--- src/main/kotlin/no/rodland/advent_2024/Day09.kt
+++ src/main/kotlin/no/rodland/advent_2024/Day09.kt
@@ -5,11 +5,14 @@ import no.rodland.advent.Day
 // template generated: 09/12/2024
 // Fredrik Rødland 2024
 
-class Day09(val input: List<String>) : Day<Long, Long, List<String>> {
+class Day09(val input: List<String>) : Day<Long, Long, Pair<List<Int>, List<Int>>> {
 
     private val parsed = input.parse()
+    private val files = parsed.first
+    private val space = parsed.second
 
     override fun partOne(): Long {
+        parsed
         return 2
     }
 
@@ -17,10 +20,11 @@ class Day09(val input: List<String>) : Day<Long, Long, List<String>> {
         return 2
     }
 
-    override fun List<String>.parse(): List<String> {
-        return map { line ->
-            line
-        }
+    override fun List<String>.parse(): Pair<List<Int>, List<Int>> {
+        return first()
+            .map { c -> c.digitToInt() }
+            .chunked(2) { it.first() to it.last() }
+            .unzip()
     }
 
     override val day = "09".toInt()
