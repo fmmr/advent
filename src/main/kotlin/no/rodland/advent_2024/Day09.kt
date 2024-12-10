f6d5b174329da9785725bd7f06252f69398d67f6
Fredrik Rødland <fmr@finn.no>
2024-12-10 09:19:57 +0100

    2024 - Day 10 - input/init

--- src/main/kotlin/no/rodland/advent_2024/Day09.kt
+++ src/main/kotlin/no/rodland/advent_2024/Day09.kt
@@ -8,20 +8,45 @@ import no.rodland.advent.Day
 class Day09(val input: List<String>) : Day<Long, Long, Pair<Int, List<Day09.Num?>>> {
 
     private val parsed = input.parse()
-    val size = parsed.first
-    val parsedList = parsed.second
+    private val sizePart1 = parsed.first
+    private val parsedList = parsed.second
 
     override fun partOne(): Long {
         val list = parsedList.toMutableList()
-        val memory = (0..<size).map { idx ->
-            list[idx] ?: list.removeLastNonNull()
+        val memory = (0..<sizePart1).map { idx ->
+            val num = list[idx]
+            if (num.isFile) {
+                num
+            } else {
+                list.removeLastNonNull()
+            }
         }
         return memory.map { it.id }.checksum()
     }
 
-    private fun MutableList<Num?>.removeLastNonNull(): Num {
+    override fun partTwo(): Long {
+//        val list = parsedList.toMutableList()
+//        val map = mutableMapOf<Num, Int>()
+//
+//
+//        parsedList.forEachIndexed { idx, id ->
+//            map.putIfAbsent(id, idx)
+//        }
+//        val sizePart2 = map.map { (k, v) -> k.size }.sum()
+//        val posMap = map.map { (k, v) -> v!! to k }.toMap()
+//        val maxId = map.keys.last().id
+//
+//        map.keys.reversed().forEach {
+//
+//        }
+
+
+        return 2
+    }
+
+    private fun MutableList<Num>.removeLastNonNull(): Num {
         var last = removeLast()
-        while (last == null) {
+        while (last.isSpace) {
             last = removeLast()
         }
         return last
@@ -29,31 +54,33 @@ class Day09(val input: List<String>) : Day<Long, Long, Pair<Int, List<Day09.Num?
 
     private fun Iterable<Int>.checksum(): Long = foldIndexed(0L) { i, acc, id -> acc + i * id }
 
-    override fun partTwo(): Long {
-        return 2
-    }
 
-    override fun List<String>.parse(): Pair<Int, List<Num?>> {
+    override fun List<String>.parse(): Pair<Int, List<Num>> {
         var id = 0
+        var spaceId = -1
         var size = 0
         val chunked = first()
             .map { c -> c.digitToInt() }
             .chunked(2) {
                 val num = Num(id++, it.first())
-                size += it.first()
-                if (it.size == 2) {
-                    num to it.last()
+                val space = if (it.size == 2) {
+                    Num(spaceId--, it.last())
                 } else {
-                    num to null
+                    Num(spaceId--, 0)
                 }
+                size += it.first()
+                num to space
             }
         val flatMap = chunked.flatMap { (num, space) ->
-            (0..<num.size).map { num } + (space?.let { (0..<space).map { null } } ?: emptyList())
+            (0..<num.size).map { num } + (0..<space.size).map { space }
         }
         return size to flatMap
     }
 
-    data class Num(val id: Int, val size: Int)
+    data class Num(val id: Int, val size: Int) {
+        val isSpace = id < 0
+        val isFile = !isSpace
+    }
 
     override val day = "09".toInt()
 }
