9e91dabe6e1079020a7a0462e08248054999d8b8
Fredrik Rødland <fmr@finn.no>
2024-12-13 09:37:25 +0100

    2024 - Day 13 - parse machines

--- src/main/kotlin/no/rodland/advent_2024/Day13.kt
+++ src/main/kotlin/no/rodland/advent_2024/Day13.kt
@@ -1,13 +1,14 @@
 package no.rodland.advent_2024
 
 import no.rodland.advent.Day
+import no.rodland.advent.Pos
 
 // template generated: 13/12/2024
 // Fredrik Rødland 2024
 
-class Day13(val input: List<String>) : Day<Long, Long, List<String>> {
+class Day13(val input: List<String>) : Day<Long, Long, List<Day13.Machine>> {
 
-    private val parsed = input.parse()
+    private val machines = input.parse()
 
     override fun partOne(): Long {
         return 2
@@ -17,11 +18,18 @@ class Day13(val input: List<String>) : Day<Long, Long, List<String>> {
         return 2
     }
 
-    override fun List<String>.parse(): List<String> {
-        return map { line ->
-            line
+    override fun List<String>.parse(): List<Machine> {
+        return chunked(4).map { (a, b, p) ->
+            val ax = a.substringAfter("X+").substringBefore(",").toInt()
+            val ay = a.substringAfter("Y+").toInt()
+            val bx = b.substringAfter("X+").substringBefore(",").toInt()
+            val by = b.substringAfter("Y+").toInt()
+            val px = p.substringAfter("X=").substringBefore(",").toInt()
+            val py = p.substringAfter("Y=").toInt()
+            Machine(Pos(ax, ay), Pos(bx, by), Pos(px, py))
         }
     }
 
+    data class Machine(val a: Pos, val b: Pos, val prize: Pos)
     override val day = "13".toInt()
 }
