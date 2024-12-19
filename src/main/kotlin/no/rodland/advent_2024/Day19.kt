e46a227db5ae59b3d0372b84ba26899a714fc112
Fredrik Rødland <fmr@finn.no>
2024-12-19 08:04:16 +0100

    2024 - Day 19 - parse

--- src/main/kotlin/no/rodland/advent_2024/Day19.kt
+++ src/main/kotlin/no/rodland/advent_2024/Day19.kt
@@ -5,9 +5,11 @@ import no.rodland.advent.Day
 // template generated: 19/12/2024
 // Fredrik Rødland 2024
 
-class Day19(val input: List<String>) : Day<Long, Long, List<String>> {
+class Day19(val input: List<String>) : Day<Long, Long, Pair<List<String>, List<String>>> {
 
     private val parsed = input.parse()
+    private val towels = parsed.first
+    private val designs = parsed.second
 
     override fun partOne(): Long {
         return 2
@@ -17,10 +19,11 @@ class Day19(val input: List<String>) : Day<Long, Long, List<String>> {
         return 2
     }
 
-    override fun List<String>.parse(): List<String> {
-        return map { line ->
-            line
-        }
+    override fun List<String>.parse(): Pair<List<String>, List<String>> {
+        val (towel, design) = joinToString("\n").split("\n\n")
+        val towels = towel.split(", ")
+        val designs = design.split("\n")
+        return towels to designs
     }
 
     override val day = "19".toInt()
