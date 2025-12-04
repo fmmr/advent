f83d9c3b05507b7740355b6cdf63efed970a0ad7
Fredrik Rødland <fmr@finn.no>
2025-12-04 14:27:21 +0100

    2025 - Day 04 - input/init

--- src/main/kotlin/no/rodland/advent_2025/Day03.kt
+++ src/main/kotlin/no/rodland/advent_2025/Day03.kt
@@ -2,37 +2,31 @@ package no.rodland.advent_2025
 
 import no.rodland.advent.Day
 
-// template generated: 03/12/2025
-// Fredrik Rødland 2025
-
-class Day03(val input: List<String>) : Day<Long, Long, List<String>> {
+// https://todd.ginsberg.com/post/advent-of-code/2025/day3/
+class Day03(val input: List<String>) : Day<Long, Long, List<List<IndexedValue<Int>>>> {
 
     private val parsed = input.parse()
 
     override fun partOne(): Long {
-        return parsed
-            .asSequence()
-            .map { it.getMax() }
-            .sum()
-            .toLong()
-    }
-
-    private fun String.getMax(): Int {
-        (9 downTo 1).forEach { first ->
-            val idx = indexOf(first.toString())
-            if (idx > -1 && idx != lastIndex) {
-                val second = substring(idx + 1).maxBy { it.digitToInt() }
-                return (first.toString() + second.digitToInt()).toInt()
-            }
-        }
-        return -1
+        return parsed.sumOf { joltage(it, 2) }
     }
 
     override fun partTwo(): Long {
-        return 2
+        return parsed.sumOf { joltage(it, 12) }
     }
 
-    override fun List<String>.parse(): List<String> = this
+    private fun joltage(bank: List<IndexedValue<Int>>, batteries: Int): Long =
+        (1..batteries).fold(0L to 0) { (total, leftIndex), offset ->
+            bank.subList(leftIndex, bank.size - batteries + offset)
+                .maxBy { it.value }
+                .let { (total * 10) + it.value to it.index + 1 }
+        }.first
+
+
+    override fun List<String>.parse(): List<List<IndexedValue<Int>>> = map {
+        it.map { d -> d.digitToInt() }.withIndex().toList()
+    }
+
 
     override val day = "03".toInt()
 }
